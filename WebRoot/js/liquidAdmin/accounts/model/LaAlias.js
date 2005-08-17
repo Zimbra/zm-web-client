6675bb52e0229f9179c45e30860ca69f98bfa7ad
Unknown Perforce User <aclark>
2005-08-16 20:22:39 -0500

    http://bugzilla.liquidsys.com/show_bug.cgi?id=3581 http://bugzilla.liquidsys.com/show_bug.cgi?id=1 Bug 3581: Phase 1 - directory rename
    Copied from Perforce
     Change: 10931

--- WebRoot/js/liquidAdmin/accounts/model/LaAlias.js
+++
@@ -1,9 +0,0 @@
-function LaAlias() {
-	LaItem.call(this, LaEvent.S_ACCOUNT);
-	this.attrs = new Object();
-	this.id = "";
-	this.name="";
-}
-
-LaAlias.prototype = new LaItem;
-LaAlias.prototype.constructor = LaAlias;
--- WebRoot/js/liquidAdmin/accounts/model/LaForwardingAddress.js
+++
@@ -1,9 +0,0 @@
-function LaForwardingAddress() {
-	LaItem.call(this, LaEvent.S_ACCOUNT);
-	this.attrs = new Object();
-	this.id = "";
-	this.name="";
-}
-
-LaForwardingAddress.prototype = new LaItem;
-LaForwardingAddress.prototype.constructor = LaForwardingAddress;
