7c6624123267d776062476eed1af39bfadd71c27
Conrad Damon <cdamon@zimbra.com>
2005-08-22 16:56:45 -0500

    Add license block to Ajax and ZimbraWebClient source files.
    Copied from Perforce
     Change: 11337

--- WebRoot/js/zimbraMail/share/view/ZmCurrentAppToolBar.js
+++ WebRoot/js/zimbraMail/share/view/ZmCurrentAppToolBar.js
@@ -1,3 +1,25 @@
+/*
+***** BEGIN LICENSE BLOCK *****
+Version: ZPL 1.1
+
+The contents of this file are subject to the Zimbra Public License Version 1.1 ("License");
+You may not use this file except in compliance with the License. You may obtain a copy of
+the License at http://www.zimbra.com/license
+
+Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY
+OF ANY KIND, either express or implied. See the License for the specific language governing
+rights and limitations under the License.
+
+The Original Code is: Zimbra Collaboration Suite.
+
+The Initial Developer of the Original Code is Zimbra, Inc.
+Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+All Rights Reserved.
+Contributor(s): ______________________________________.
+
+***** END LICENSE BLOCK *****
+*/
+
 /**
 * This toolbar sits above the overview and represents the current app. It has a label
 * that tells the user what the current app is, and an optional View button/menu for
