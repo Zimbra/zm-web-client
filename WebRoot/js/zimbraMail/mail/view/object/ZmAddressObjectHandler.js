5502993678f2bd902132781898e965a3a478a350
Unknown Perforce User <khenrikson>
2005-08-31 19:34:30 -0500

    - Add caching of GPoint which results in caching the geocode call.   Additional hovers are now instant, even if you go to a different message and return
    Copied from Perforce
     Change: 12010

--- WebRoot/js/zimbraMail/mail/view/object/ZmAddressObjectHandler.js
+++ WebRoot/js/zimbraMail/mail/view/object/ZmAddressObjectHandler.js
@@ -23,7 +23,6 @@
  * ***** END LICENSE BLOCK *****
  */
 
-
 function ZmAddressObjectHandler(appCtxt) {
 
 	ZmObjectHandler.call(this, appCtxt, "address", null);
@@ -36,44 +35,58 @@ ZmAddressObjectHandler.prototype.constructor = ZmAddressObjectHandler;
 // 1234 Main St City CA 99999
 ZmAddressObjectHandler.ADDRESS_RE = /[\w]{3,}([A-Za-z]\.)?([ \w]*\#\d+)?(\r\n| )[ \w]{3,}\x20[A-Za-z]{2}\x20\d{5}(-\d{4})?/ig;
 
+ZmAddressObjectHandler.CACHE = new Array();
+
 ZmAddressObjectHandler.prototype.match =
 function(line, startIndex) {
 	ZmAddressObjectHandler.ADDRESS_RE.lastIndex = startIndex;
 	return ZmAddressObjectHandler.ADDRESS_RE.exec(line);
-}
+};
 
 ZmAddressObjectHandler.prototype._getHtmlContent =
 function(html, idx, address, context) {
 	var	url = "http://maps.google.com/maps?q="+AjxStringUtil.htmlEncode(address);
     html[idx++] = '<a target="_blank" href="'+url+'">'+AjxStringUtil.htmlEncode(address)+'</a>';	
 	return idx;
-}
+};
 
 ZmAddressObjectHandler.prototype.getToolTipText =
 function(obj, context) {
-	return '<div id="map" style="width: 350px; height: 350px"></div>';
-}
+	return '<div id="gMap" style="width: 350px; height: 350px"></div>';
+};
 
 ZmAddressObjectHandler.prototype.populateToolTip =
 function(obj, context) {
-		var request = GXmlHttp.create();
-		var url = "/zimbra/zimlets/geocode.jsp?address="+AjxStringUtil.urlEncode(obj);
-		request.open("GET", url, true);
-		request.onreadystatechange = function() {
-	    	if (request.readyState == 4) {
-	    		var resp = request.responseText;
-				// This is a bit cheap but quicker than adding/writing an RDF parser for now.
-				var lg_s = resp.indexOf("<geo:long>");
-				var lg_e = resp.indexOf("</geo:long>");
-				var lt_s = resp.indexOf("<geo:lat>");
-				var lt_e = resp.indexOf("</geo:lat>");
-			    var map = new GMap(document.getElementById("map"));
-			    var point = new GPoint(resp.substring(lg_s+10,lg_e), resp.substring(lt_s+9,lt_e));
-			    map.centerAndZoom(point, 4);
-				var marker = new GMarker(point);
-				map.addOverlay(marker);
-				marker.openInfoWindowHtml('<br/>'+obj);
-		  	}
+		if(ZmAddressObjectHandler.CACHE[obj]) {
+			ZmAddressObjectHandler.displayMap(ZmAddressObjectHandler.CACHE[obj], obj);
+		} else {
+			var request = GXmlHttp.create();
+			var url = "/zimbra/zimlets/geocode.jsp?address="+AjxStringUtil.urlEncode(obj);
+			request.open("GET", url, true);
+			request.onreadystatechange = function() {
+		    	if (request.readyState == 4) {
+		    		var resp = request.responseText;
+					// Quick RDF parser
+					var lg_s = resp.indexOf("<geo:long>");
+					var lg_e = resp.indexOf("</geo:long>");
+					var lt_s = resp.indexOf("<geo:lat>");
+					var lt_e = resp.indexOf("</geo:lat>");
+				    var point = new GPoint(resp.substring(lg_s+10,lg_e), resp.substring(lt_s+9,lt_e));
+				    ZmAddressObjectHandler.displayMap(point, obj);
+			  	}
+			};
+			request.send(null);
 		}
-		request.send(null);
-}
+};
+
+ZmAddressObjectHandler.displayMap =
+function(point, obj) {
+    var map = new GMap(document.getElementById("gMap"));
+    map.centerAndZoom(point, 4);
+	var marker = new GMarker(point);
+	map.addOverlay(marker);
+	marker.openInfoWindowHtml('<br/>'+obj);
+	if(!ZmAddressObjectHandler.CACHE[obj]) {
+		ZmAddressObjectHandler.CACHE[obj] = point;
+	}
+};
