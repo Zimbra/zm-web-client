db230ba7f8c15bc8d7a20253eeb113c62c78c7d8
Unknown Perforce User <schemers>
2005-09-07 15:38:16 -0500

    remove typo from license
    Copied from Perforce
     Change: 12257

--- WebRoot/js/zimbraMail/calendar/view/ZmCalDayView.js
+++ WebRoot/js/zimbraMail/calendar/view/ZmCalDayView.js
@@ -5,7 +5,7 @@
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.1 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
- * http://www.zimbraga.com/license
+ * http://www.zimbra.com/license
  * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
