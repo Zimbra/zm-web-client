3213d6edf157aba20130ac8956fa3aebb137ef12
Unknown Perforce User <schemers>
2005-11-01 02:04:56 -0600

    so is new history
    Copied from Perforce
     Change: 15407

--- WebRoot/js/zimbraMail/share/view/dialog/ZmStatusHistoryDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmStatusHistoryDialog.js
@@ -62,10 +62,10 @@ function(historyVector) {
 	var history = historyVector.getArray();
 	for (var i=0; i < history.length; i++) {
 		var work = history[i];
-		html.append("<div class='", ZmStatusView.getClass(work)+"'>");
+		html.append("<div class='", ZmStatusView.getClass(work)+"'", ((i > 0) ? " style='border-top:none;'": ""), ">");
 		html.append("<table><tr><td rowspan=2 align='center'>", ZmStatusView.getImageHtml32(work),"</td>");
 		html.append("<td>", ZmMsg.date, ": ", AjxDateUtil.getTimeStr(work.date,"%n/%d %H:%m:%s %P"), "</td></tr>");
-		html.append("<tr><td>", work.msg, "</td></tr></table>");
+		html.append("<tr><td><b>", AjxStringUtil.htmlEncode(work.msg), "</b></td></tr></table>");
 		html.append("</div>");
 	}
 	var div = document.getElementById(this._listId);
