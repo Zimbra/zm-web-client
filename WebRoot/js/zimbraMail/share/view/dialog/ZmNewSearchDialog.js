b34c8faa9f30dded65d5294826b0b19f4af6ec78
Unknown Perforce User <anandp>
2005-11-09 15:42:33 -0600

    More licensing.
    Original Code must be identified as "ZCS Web Client" (instead of
    just "ZCS").
    
    Convert the Russian translation file with mac2unix.
    
    Some code had been cut pasted from Ajax and had 
    Original Code and so was identified wrong - fix to identify all
    code inside ZimbraWebClient project correctly.
    
    Copied from Perforce
     Change: 16136

--- WebRoot/js/zimbraMail/share/view/dialog/ZmNewSearchDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmNewSearchDialog.js
@@ -12,7 +12,7 @@
  * the License for the specific language governing rights and limitations
  * under the License.
  * 
- * The Original Code is: Zimbra Collaboration Suite.
+ * The Original Code is: Zimbra Collaboration Suite Web Client
  * 
  * The Initial Developer of the Original Code is Zimbra, Inc.
  * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
