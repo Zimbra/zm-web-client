0c5aecc46f88a5a7ae457387d51ba3c4b53ea50b
Unknown Perforce User <schemers>
2005-11-13 04:17:14 -0600

    nuke CsvServlet and point web client to /service/user/~/contacts.csvUse ItemId in UserServlet and (hopefully) handle all proxying correctly.
    Copied from Perforce
     Change: 16560

--- WebRoot/js/zimbraMail/share/model/ZmSettings.js
+++ WebRoot/js/zimbraMail/share/model/ZmSettings.js
@@ -296,7 +296,7 @@ function() {
 	this._settings[ZmSetting.CSFE_UPLOAD_URI].setValue(value);
 	
 	// CSFE EXPORT URI
-	value = (noPort) ? "/service/csv/contacts.csv" : ":" + location.port + "/service/csv/contacts.csv";
+	value = (noPort) ? "/service/user/~/contacts.csv" : ":" + location.port + "/service/user/~/contacts.csv";	
 	this._settings[ZmSetting.CSFE_EXPORT_URI].setValue(value);
 	
 	// default sorting preferences
