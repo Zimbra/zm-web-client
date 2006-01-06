da65a006fcaf926ac1dc2d1bbc537f8721378c6f
Conrad Damon <cdamon@zimbra.com>
2006-01-05 18:19:55 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=786 Bug: 786
    Prompt user when emptying Trash (or
    deleting a folder in the Trash)
    
    Copied from Perforce
     Change: 19158

--- WebRoot/js/zimbraMail/share/controller/ZmFolderTreeController.js
+++ WebRoot/js/zimbraMail/share/controller/ZmFolderTreeController.js
@@ -180,16 +180,15 @@ function(parent, name, search, url) {
 ZmFolderTreeController.prototype._deleteListener = 
 function(ev) {
 	var organizer = this._getActionedOrganizer(ev);
-	if (organizer.isInTrash()) {
-		this._doDelete(organizer);
-	} else if (organizer.id == ZmFolder.ID_SPAM) {
+	if (organizer.id == ZmFolder.ID_SPAM || organizer.isInTrash()) {
 		this._pendingActionData = organizer;
 		if (!this._deleteShield) {
 			this._deleteShield = new DwtMessageDialog(this._shell, null, [DwtDialog.YES_BUTTON, DwtDialog.NO_BUTTON]);
 			this._deleteShield.registerCallback(DwtDialog.YES_BUTTON, this._deleteShieldYesCallback, this, organizer);
 			this._deleteShield.registerCallback(DwtDialog.NO_BUTTON, this._clearDialog, this, this._deleteShield);
 		}
-		this._deleteShield.setMessage(ZmMsg.confirmEmptyJunk, DwtMessageDialog.WARNING_STYLE);
+		var msg = AjxMessageFormat.format(ZmMsg.confirmEmptyFolder, organizer.getName());
+		this._deleteShield.setMessage(msg, DwtMessageDialog.WARNING_STYLE);
 		this._deleteShield.popup();
     } else {
 		this._doMove(organizer, this._appCtxt.getTree(ZmOrganizer.FOLDER).getById(ZmFolder.ID_TRASH));
