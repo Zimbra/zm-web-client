6717efd06e0377d46755d66a7453e0b1e7776e2f
Unknown Perforce User <aclark>
2006-01-17 17:23:56 -0600

    integrate 19450
    Copied from Perforce
     Change: 19777

--- WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
@@ -810,8 +810,47 @@ function(el, bodyPart, callback, result) {
 	// if no text part, check if theres a calendar part and generate some canned
 	// text, otherwise, get the html part if one exists
 	if (content == null) {
-		if (bodyPart.ct == ZmMimeTable.TEXT_CAL)
-			content = this._msg.isInvite() ? this._msg.getInvite().getCannedText() : null;
+		if (bodyPart.ct == ZmMimeTable.TEXT_CAL) {
+			// NOTE: If there's only a text/calendar part, then fall 
+			//       back to the description line(s) in the vcal content.
+			/***
+			var regex = /DESCRIPTION:(.*(?:\r\n\s+.*)*)/;
+			var results = regex.exec(bodyPart.content);
+			if (results && results.length > 1) {
+				content = results[1];
+				content = content.replace(/\r\n\s+/g," ");
+				content = content.replace(/\\t/g, "\t");
+				content = content.replace(/\\n/g, "\n");
+				content = content.replace(/\\(.)/g, "$1");
+			}
+			/***/
+			// NOTE: IE doesn't match my multi-line regex, even when
+			//       explicitly specifying the "m" attribute.
+			var lines = bodyPart.content.split(/\r\n/);
+			var desc = [];
+			for (var i = 0; i < lines.length; i++) {
+				var line = lines[i];
+				if (line.match(/^DESCRIPTION:/)) {
+					desc.push(line.substr(12));
+					for (var j = i + 1; j < lines.length; j++) {
+						var line = lines[j];
+						if (line.match(/^\s+/)) {
+							desc.push(line.replace(/^\s+/, " "));
+							continue;
+						}
+						break;
+					}
+					break;
+				}
+			}
+			if (desc.length > 0) {
+				content = desc.join("");
+				content = content.replace(/\\t/g, "\t");
+				content = content.replace(/\\n/g, "\n");
+				content = content.replace(/\\(.)/g, "$1");
+			}
+			/***/
+		}
 		else if (bodyPart.ct == ZmMimeTable.TEXT_HTML)
 			content = bodyPart.content;
 	}
