564a61acbec9cf92c2a40a5f943648e1d03e1f17
Unknown Perforce User <aclark>
2006-01-24 20:10:48 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=4718 Bug 4718: Server now returns the expanded username (if not fully-qualified on input) so that is used to send out the email.
    Copied from Perforce
     Change: 20310

--- WebRoot/js/zimbraMail/share/view/dialog/ZmSharePropsDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmSharePropsDialog.js
@@ -138,12 +138,13 @@ ZmSharePropsDialog.prototype._handleOkButton = function(event) {
 	if (this._dialogType == ZmSharePropsDialog.NEW) {
 		share.grantee.name = this._inputEl.value;
 	}
-	share.grantee.email = share.grantee.name;
 	share.link.perm = this._getSelectedRole();
 
 	// execute grant operation
 	try {
-		share.grantee.id = this._executeGrantAction(folder, share);
+		var action = this._executeGrantAction(folder, share);
+		share.grantee.id = action.zid;
+		share.grantee.email = action.d;
 	}
 	catch (ex) {
 		var message = ZmMsg.unknownError;
@@ -224,12 +225,12 @@ ZmSharePropsDialog.prototype._executeGrantAction = function(folder, share) {
 	
 	var shareNode = soapDoc.set("grant", null, actionNode);
 	shareNode.setAttribute("gt", "usr");
-	shareNode.setAttribute("d", share.grantee.email);
+	shareNode.setAttribute("d", share.grantee.name);
 	shareNode.setAttribute("perm", share.link.perm);
 	
 	var resp = this._appCtxt.getAppController().sendRequest({soapDoc: soapDoc});
 	
-	return resp.FolderActionResponse.action.zid;
+	return resp.FolderActionResponse.action;
 }
 
 ZmSharePropsDialog.prototype._handleCompletionData = 
