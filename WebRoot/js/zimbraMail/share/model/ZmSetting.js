66747c0c7b39f7cfbf54630fb834fdfc5f3eadfc
Unknown Perforce User <schemers>
2006-03-18 14:51:43 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=2799 http://bugzilla.zimbra.com/show_bug.cgi?id=2799 bug: 2799
           set default value (if not set in COS/account) to 0, which means never pop up
    
    Copied from Perforce
     Change: 22866

--- WebRoot/js/zimbraMail/share/model/ZmSetting.js
+++ WebRoot/js/zimbraMail/share/model/ZmSetting.js
@@ -344,7 +344,7 @@ ZmSetting.INIT[ZmSetting.CAL_USE_QUICK_ADD] 			= ["zimbraPrefCalendarUseQuickAdd
 ZmSetting.INIT[ZmSetting.CAL_ALWAYS_SHOW_MINI_CAL]		= ["zimbraPrefCalendarAlwaysShowMiniCal", ZmSetting.T_PREF, ZmSetting.D_BOOLEAN, false];
 ZmSetting.INIT[ZmSetting.CALENDAR_INITIAL_VIEW]			= ["zimbraPrefCalendarInitialView", ZmSetting.T_PREF, ZmSetting.D_STRING, ZmSetting.CAL_DAY];
 ZmSetting.INIT[ZmSetting.DEFAULT_CALENDAR_TIMEZONE]		= ["zimbraPrefTimeZoneId", ZmSetting.T_PREF, ZmSetting.D_STRING];
-ZmSetting.INIT[ZmSetting.CAL_REMINDER_WARNING_TIME]		= ["zimbraPrefCalendarApptReminderWarningTime", ZmSetting.T_PREF, ZmSetting.D_INT, 30];
+ZmSetting.INIT[ZmSetting.CAL_REMINDER_WARNING_TIME]		= ["zimbraPrefCalendarApptReminderWarningTime", ZmSetting.T_PREF, ZmSetting.D_INT, 0];
 
 // general preferences
 ZmSetting.INIT[ZmSetting.PASSWORD]						= [null, ZmSetting.T_PREF];
