5541ab012a5473a7aab6f30f6ce34e3ce0e7ce4e
Unknown Perforce User <pshah>
2006-04-06 14:36:16 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=6514 bug: 6514 - Safari: Calendar cannot be selected (checked/unchecked) using mouse click
    Copied from Perforce
     Change: 23917

--- WebRoot/js/zimbraMail/share/controller/ZmCalendarTreeController.js
+++ WebRoot/js/zimbraMail/share/controller/ZmCalendarTreeController.js
@@ -208,6 +208,12 @@ function(ev) {
 		var overviewId = ev.item.getData(ZmTreeView.KEY_ID);
 		var calendar = ev.item.getData(Dwt.KEY_OBJECT);
 
+		// bug fix #6514 - Explicitly set checkbox for Safari
+		// NOTE: this bug fix should be removed once Safari gets new version!
+		if (AjxEnv.isSafari) {
+			ev.item._checkBox.checked = !ev.item._checkBox.checked;
+		}
+
 		// notify listeners of selection
 		if (this._eventMgrs[overviewId]) {
 			this._eventMgrs[overviewId].notifyListeners(DwtEvent.SELECTION, ev);
