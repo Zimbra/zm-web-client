569a562c5bde2c6a8d6ca241d5bde3a7611c8ca2
Unknown Perforce User <pshah>
2006-07-12 13:58:48 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=8622 bug: 8622 - Safari: Cannot create Meetings because of DOM Exception Also removed code that prevented pre-caching in Safari which was causing other JS errors.
    Copied from Perforce
     Change: 28285

--- WebRoot/js/zimbraMail/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/ZmZimbraMail.js
@@ -761,9 +761,6 @@ function() {
 ZmZimbraMail.prototype._preloadViews =
 function() {
 
-	// safari is slow on preloading so dont do it
-	if (AjxEnv.isSafari) return;
-
 	// preload the compose view
 	var cc = this.getApp(ZmZimbraMail.MAIL_APP).getComposeController();
 	if (cc) {
