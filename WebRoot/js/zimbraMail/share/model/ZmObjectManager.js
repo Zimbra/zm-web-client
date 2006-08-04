9fdc93bdb38d34ab5473281143933b7b2d52f109
Conrad Damon <cdamon@zimbra.com>
2006-08-04 18:27:08 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=9603 Bug: 9603
    	IE was throwing a JS error every other
    	time an appt chooser tab was shown, when it
    	tried to set focus to an input. Putting the
    	controller's call to _restoreFocus() in a timed
    	action seems to fix it. Ours is not to wonder
    	why...
    
    Copied from Perforce
     Change: 29478

--- WebRoot/js/zimbraMail/share/model/ZmObjectManager.js
+++ WebRoot/js/zimbraMail/share/model/ZmObjectManager.js
@@ -65,6 +65,8 @@ function ZmObjectManager(view, appCtxt, selectCallback, skipHandlers) {
 	this.sortHandlers();
 	this.reset();
 
+	DBG.println(AjxDebug.DBG3, "ZmObjectManager " + zimlets.length + " Zimlets loaded");
+
 	// install handlers
 	if (view != null) {
 	    view.addListener(DwtEvent.ONMOUSEOVER, new AjxListener(this, this._mouseOverListener));
