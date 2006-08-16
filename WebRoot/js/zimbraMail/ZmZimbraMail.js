5b2bd15894139bfbd9c11a321d23f11b777d897e
Unknown Perforce User <schemers>
2006-08-16 18:47:45 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=9968 bug: 9968 (add back view=compose support)
    Copied from Perforce
     Change: 30313

--- WebRoot/js/zimbraMail/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/ZmZimbraMail.js
@@ -449,6 +449,18 @@ function() {
 		dlg.setMessage(ZmMsg.licenseExpired, DwtMessageDialog.WARNING_STYLE);
 		dlg.popup();
 	}
+
+	// check for jump to compose page
+	if (location.search && (location.search.match(/\bview=compose\b/))) {
+		var cc = this.getApp(ZmZimbraMail.MAIL_APP).getComposeController();
+		var match = location.search.match(/\bsubject=([^&]+)/);
+		var subject = match ? decodeURIComponent(match[1]) : null;
+		match = location.search.match(/\bto=([^&]+)/);
+		var to = match ? decodeURIComponent(match[1]) : null;
+		match = location.search.match(/\bbody=([^&]+)/);
+		var body = match ? decodeURIComponent(match[1]) : null;
+		cc.doAction(ZmOperation.NEW_MESSAGE, false, null, to, subject, body);
+	}
 	// Setup an async load of the views we precreate
 	var ta = new AjxTimedAction(this, ZmZimbraMail.prototype._preloadViews);
 	AjxTimedAction.scheduleAction(ta, 500);
