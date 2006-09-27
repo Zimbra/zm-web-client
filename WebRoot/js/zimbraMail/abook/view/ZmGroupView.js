096c8556dde1aef078a6808f08213768588d19c4
Unknown Perforce User <pshah>
2006-09-27 17:18:56 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=6010 bug: 6010 - checkpoint
    Copied from Perforce
     Change: 32431

--- WebRoot/js/zimbraMail/abook/view/ZmGroupView.js
+++ WebRoot/js/zimbraMail/abook/view/ZmGroupView.js
@@ -65,6 +65,7 @@ function(contact) {
 
 	if (!this._htmlInitialized) {
 		this._createHtml(contact);
+		this._installOnKeyupHandler();
 	}
 
 	if (this._contact) {
@@ -74,7 +75,6 @@ function(contact) {
 	this._contact = contact;
 
 	this._setFields();
-	this._isDirty = false;
 };
 
 ZmGroupView.prototype.getModifiedAttrs =
@@ -84,7 +84,8 @@ function() {
 
 ZmGroupView.prototype.enableInputs =
 function(bEnable) {
-	// TODO
+	document.getElementById(this._groupNameId).disabled = !bEnable;
+	document.getElementById(this._groupMembersId).disabled = !bEnable;
 };
 
 // Following two overrides are a hack to allow this view to pretend it's a list view
@@ -100,7 +101,8 @@ function() {
 
 ZmGroupView.prototype.isDirty =
 function() {
-	return this._isDirty;
+	// TODO - check group name and group members for dirtyness
+	return true;
 };
 
 ZmGroupView.prototype.getTitle =
@@ -108,41 +110,46 @@ function() {
 	return [ZmMsg.zimbraTitle, ZmMsg.group].join(": ");
 };
 
+ZmGroupView.prototype.setBounds =
+function(x, y, width, height) {
+	DwtComposite.prototype.setBounds.call(this, x, y, width, height);
+	var groupTable = document.getElementById(this._groupMembersId);
+	if (groupTable) {
+		Dwt.setSize(groupTable, Dwt.DEFAULT, height-90);
+	}
+};
+
+
 
 // Private methods
 
-ZmGroupView.prototype._setFields =
+// Consistent spot to locate various dialogs
+ZmGroupView.prototype._getDialogXY =
 function() {
-	this._setHeaderColor();
-	this._setTitle();
-	this._setTags();
+	var loc = Dwt.toWindow(this.getHtmlElement(), 0, 0);
+	return new DwtPoint(loc.x + 50, loc.y + 100);
 };
 
 ZmGroupView.prototype._setHeaderColor =
 function() {
-/*
 	// set the appropriate header color
 	var folderId = this._contact.folderId;
 	var folder = folderId ? this._appCtxt.getTree(ZmOrganizer.ADDRBOOK).getById(folderId) : null;
 	var color = folder ? folder.color : ZmAddrBook.DEFAULT_COLOR;
 	var bkgdColor = ZmOrganizer.COLOR_TEXT[color] + "Bg";
-	var contactHdrRow = document.getElementById(this._groupHeaderRowId);
+	var contactHdrRow = document.getElementById(this._contactHeaderRowId);
 	contactHdrRow.className = "contactHeaderRow " + bkgdColor;
-*/
 };
 
 ZmGroupView.prototype._setTitle =
 function(title) {
-/*
-	var div = document.getElementById(this._groupTitleId);
+	var div = document.getElementById(this._titleId);
 	var fileAs = title || this._contact.getFileAs();
 	div.innerHTML = fileAs || (this._contact.id ? "&nbsp;" : ZmMsg.newGroup);
-*/
 };
 
 ZmGroupView.prototype._setTags =
 function() {
-/*
 	// get sorted list of tags for this msg
 	var ta = new Array();
 	for (var i = 0; i < this._contact.tags.length; i++)
@@ -159,33 +166,93 @@ function() {
 		html[i++] = "&nbsp;";
 	}
 
-	var tagCell = document.getElementById(this._groupTagId);
+	var tagCell = document.getElementById(this._tagsId);
 	tagCell.innerHTML = html.join("");
-*/
+};
+
+ZmGroupView.prototype._setFields =
+function() {
+
+	// TODO - set fields from this._contact
+
+	this._setHeaderColor();
+	this._setTitle();
+	this._setTags();
 };
 
 ZmGroupView.prototype._createHtml =
 function(contact) {
+	this._titleId = Dwt.getNextId();
+	this._tagsId = Dwt.getNextId();
+	this._contactHeaderId = Dwt.getNextId();
+	this._contactHeaderRowId = Dwt.getNextId();
+	this._groupNameId = Dwt.getNextId();
+	this._groupMembersId = Dwt.getNextId();
+
 	var idx = 0;
-	var html = new Array(50);
+	var html = [];
+
+	// Title bar
+	html[idx++] = "<table id='";
+	html[idx++] = this._contactHeaderId;
+	html[idx++] = "' cellspacing=0 cellpadding=0 width=100%><tr class='contactHeaderRow' id='";
+	html[idx++] = this._contactHeaderRowId;
+	html[idx++] = "'><td width=20><center>";
+	html[idx++] = AjxImg.getImageHtml("Group");
+	html[idx++] = "</center></td><td><div id='";
+	html[idx++] = this._titleId;
+	html[idx++] = "' class='contactHeader'></div></td><td align='right' id='";
+	html[idx++] = this._tagsId;
+	html[idx++] = "'></td></tr></table>";
+	// content
+	html[idx++] = "<table border=0 width=100% cellpadding=1 cellspacing=1><td width=50% valign=top>";
+	html[idx++] = "<table border=0 width=100% cellpadding=2 cellspacing=2><tr><td colspan=2>";
+	html[idx++] = ZmMsg.groupName;
+	html[idx++] = ": <input type='text' id='";
+	html[idx++] = this._groupNameId;
+	html[idx++] = "'></td></tr><tr><td>";
+	html[idx++] = ZmMsg.groupMembers;
+	html[idx++] = ":</td><td class='editLabel' style='color:#666666'>";
+	html[idx++] = ZmMsg.enterAddresses;
+	html[idx++] = "</td></tr></table>";
+	html[idx++] = "<textarea class='groupMembers' id='";
+	html[idx++] = this._groupMembersId;
+	html[idx++] = "'></textarea></td><td valign=top>";
 
 	// TODO
+	html[idx++] = "...list view goes here...";
+
+	html[idx++] = "</td>";
+	html[idx++] = "</tr></table>";
 
 	this.getHtmlElement().innerHTML = html.join("");
 
 	this._htmlInitialized = true;
 };
 
+ZmGroupView.prototype._installOnKeyupHandler =
+function() {
+	var groupName = document.getElementById(this._groupNameId);
+	Dwt.setHandler(groupName, DwtEvent.ONKEYUP, ZmGroupView._onKeyUp);
+	Dwt.associateElementWithObject(groupName, this);
+	groupName._field = field;
+};
+
 ZmGroupView.prototype._getTabGroupMembers =
 function() {
-	// TODO
-	return [];
+	var fields = [];
+
+	fields.push(document.getElementById(this._groupNameId));
+	fields.push(document.getElementById(this._groupMembersId));
+
+	return fields;
 };
 
 ZmGroupView.prototype._getDefaultFocusItem =
 function() {
-	// TODO
+	return document.getElementById(this._groupNameId);
 };
+
 
 // Listeners
 
@@ -215,6 +282,22 @@ function(ev) {
 
 
 // Static methods
+
+ZmGroupView._onKeyUp =
+function(ev) {
+	ev = DwtUiEvent.getEvent(ev);
+	if (ev.metaKey || ev.altKey || ev.ctrlKey)
+		return;
+
+	var e = DwtUiEvent.getTarget(ev);
+	var view = e ? Dwt.getObjectFromElement(e) : null;
+	if (view) {
+		view._setTitle(e.value);
+	}
+
+	return true;
+};
+
 ZmGroupView.getPrintHtml =
 function(contact, abridged, appCtxt) {
 	DBG.println("TODO");
