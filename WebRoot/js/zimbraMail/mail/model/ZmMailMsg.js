50d99f0ce1589b0879746a0d18111542b36fc071
Unknown Perforce User <pshah>
2006-12-11 20:08:22 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=6163 bug: 6163 - Add an "Add to Address Book" link next to .vcf (vcard) attachments
    Copied from Perforce
     Change: 37690

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -902,7 +902,7 @@ function(findHits) {
 					else if (attach.ct == ZmMimeTable.TEXT_VCARD)
 					{
 						var onclickStr = "ZmMailMsgView.vcardCallback(" + this.getId() + "," + attach.part + ");";
-						props.vcardLink = "<a style='text-decoration:underline' class='AttLink' href='javascript:;' onclick='" + onclickStr + "'>";
+						props.vcardLink = "<a style='text-decoration:underline' class='AttLink' href='javascript:;' onclick='ZmMailMsgView.unloadHackCallback(); " + onclickStr + "'>";
 					}
 					else
 					{
