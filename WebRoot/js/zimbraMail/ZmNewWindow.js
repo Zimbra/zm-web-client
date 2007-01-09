bbc513ba5f26be33f41f347661c88b3545f3c42d
Conrad Damon <cdamon@zimbra.com>
2007-01-09 15:29:01 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=13312 Bug: 13312
    Make sure to enable keyboard manager
    in new window.
    
    Copied from Perforce
     Change: 40533

--- WebRoot/js/zimbraMail/ZmNewWindow.js
+++ WebRoot/js/zimbraMail/ZmNewWindow.js
@@ -48,8 +48,11 @@ function ZmNewWindow(appCtxt, domain) {
 
 	// Register keymap and global key action handler w/ shell's keyboard manager
 	this._kbMgr = appCtxt.getKeyboardMgr();
-	this._kbMgr.registerKeyMap(new ZmKeyMap(appCtxt));
-	this._kbMgr.pushDefaultHandler(this);
+	if (this._appCtxt.get(ZmSetting.USE_KEYBOARD_SHORTCUTS)) {
+		this._kbMgr.enable(true);
+		this._kbMgr.registerKeyMap(new ZmKeyMap(appCtxt));
+		this._kbMgr.pushDefaultHandler(this);
+	}
 
 	this._apps = {};
 	this.startup();
