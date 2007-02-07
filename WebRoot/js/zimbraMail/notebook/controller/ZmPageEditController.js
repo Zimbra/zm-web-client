2acaadfee6871ce0913e1c35af6e0f64335cfb96
Unknown Perforce User <dcomfort>
2007-02-07 12:54:04 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=11472 Bug: 11472 Untitled New Page gets closed before saving it
    Copied from Perforce
     Change: 43659

--- WebRoot/js/zimbraMail/notebook/controller/ZmPageEditController.js
+++ WebRoot/js/zimbraMail/notebook/controller/ZmPageEditController.js
@@ -456,8 +456,6 @@ ZmPageEditController.prototype._popShieldYesCallback =
 function() {
 	this._popShield.popdown();
 	this._doSave(true);
-	this._app.popView(true);
-	this._appCtxt.getAppViewMgr().showPendingView(true);
 };
 
 ZmPageEditController.prototype._popShieldNoCallback =
