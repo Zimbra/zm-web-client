f5894a35375324aa8af47b392c76a18376b17b89
Unknown Perforce User <aclark>
2007-02-26 20:55:36 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=15003 bug: 15003 Renaming the skin files to more reasonable names. This will make it easier to understand and easier to copy -- you won't have to rename all of the files anymore anytime that you take an existing skin and copy it.
    Copied from Perforce
     Change: 45737

---
+++ WebRoot/skins/steel/skin.js
@@ -0,0 +1,9 @@
+skin = new BaseSkin({
+    // info
+    version:    { major: 2, minor: 0, patch: 0 },
+
+    // specific components
+    app_chooser		: {	style:"chiclet", 	direction:"TB"	},
+	help_button		: {	style:"link", 		container:"app_chooser"	},
+	logout_button	: { style:"link", 		container:"app_chooser"	}
+});
