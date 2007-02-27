472252f9d98779eabe99e8688ff8efdd6f9c86c3
Unknown Perforce User <aclark>
2007-02-26 21:12:12 -0600

    http://bugzilla.zimbra.com/show_bug.cgi?id=15003 bug: 15003 Finished rename of skin files and updated SkinResources servlet to pull information from manifest.xml instead of {skin}.xml.
    Copied from Perforce
     Change: 45741

--- WebRoot/skins/steel/steel.js
+++
@@ -1,9 +0,0 @@
-skin = new BaseSkin({
-    // info
-    version:    { major: 2, minor: 0, patch: 0 },
-
-    // specific components
-    app_chooser		: {	style:"chiclet", 	direction:"TB"	},
-	help_button		: {	style:"link", 		container:"app_chooser"	},
-	logout_button	: { style:"link", 		container:"app_chooser"	}
-});
