94953eaa71d20300465df96c93d8160342f6a48e
Unknown Perforce User <pshah>
2007-04-12 12:34:56 -0500

    http://bugzilla.zimbra.com/show_bug.cgi?id=15958 bug: 15958 - Zimlet context menu contains tag/delete/print options This was a regression due to changelist #45355 (bug 13764).
    Copied from Perforce
     Change: 49523

--- WebRoot/js/zimbraMail/share/model/ZmZimletContext.js
+++ WebRoot/js/zimbraMail/share/model/ZmZimletContext.js
@@ -324,7 +324,7 @@ ZmZimletContext.prototype.getPanelActionMenu = function() {
 };
 
 ZmZimletContext.prototype._makeMenu = function(obj) {
-	var menu = new ZmActionMenu({parent:DwtShell.getShell(window), standardMenuItems:ZmOperation.NONE});
+	var menu = new ZmActionMenu({parent:DwtShell.getShell(window), menuItems:ZmOperation.NONE});
 	for (var i = 0; i < obj.length; ++i) {
 		var data = obj[i];
 		if (!data.id) {
