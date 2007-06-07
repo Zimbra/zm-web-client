d18e37ab2801a6a7ffcef13140c53f7f72390739
Unknown Perforce User <mihai>
2007-06-07 05:31:34 -0500

    Bug: 14156
    Cleaned up ZmNewRosterItemDialog (moved to AjxTemplate, using
    DwtInputField-s rather than plain <input> elements, fixed tab order).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=14156
    
    Copied from Perforce
     Change: 55098

--- WebRoot/js/zimbraMail/im/view/ZmNewRosterItemDialog.js
+++ WebRoot/js/zimbraMail/im/view/ZmNewRosterItemDialog.js
@@ -26,8 +26,27 @@
 ZmNewRosterItemDialog = function(parent, appCtxt) {
 	ZmQuickAddDialog.call(this, parent, null, null);
 	this._appCtxt = appCtxt;
-	this.setContent(this._contentHtml());
 
+	this._init();
+};
+
+ZmNewRosterItemDialog._OVERVIEW_ID = "ZmNewRosterItemDialog";
+
+ZmNewRosterItemDialog.prototype = new ZmQuickAddDialog;
+ZmNewRosterItemDialog.prototype.constructor = ZmNewRosterItemDialog;
+
+ZmNewRosterItemDialog.prototype._init = function() {
+	this.setContent(this._contentHtml());
+	var id = this._baseId;
+
+	this._addrEntry = new DwtInputField({ parent: this, size: 30, required: true });
+	this._addrEntry.reparentHtmlElement(id + "_address");
+
+	this._nameEntry = new DwtInputField({ parent: this, size: 30 });
+	this._nameEntry.reparentHtmlElement(id + "_name");
+
+	this._groupsEntry = new DwtInputField({ parent: this, size: 30, required: true });
+	this._groupsEntry.reparentHtmlElement(id + "_groups");
 
 	var options = [];
 	var roster = AjxDispatcher.run("GetRoster");
@@ -36,167 +55,117 @@ ZmNewRosterItemDialog = function(parent, appCtxt) {
 		var gw = gws[i];
 		options.push(new DwtSelectOption(gw.type, i == 0, gw.type));
 	}
-	var sel = new DwtSelect(this, options);
-	sel.reparentHtmlElement(this._serviceTypeId);
-	this._serviceTypeSelect = sel;
+	this._serviceTypeSelect = new DwtSelect(this, options);
+	this._serviceTypeSelect.reparentHtmlElement(id + "_serviceType");
 
 	this.setTitle(ZmMsg.createNewRosterItem);
-//	this.setTabOrder([this._addressFieldId, this._nameFieldId, this._groupsFieldId]);
 	this._initAddressAutocomplete();
 	this._initGroupAutocomplete();
     	this.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._okButtonListener));
-}
 
-ZmNewRosterItemDialog._OVERVIEW_ID = "ZmNewRosterItemDialog";
-
-ZmNewRosterItemDialog.prototype = new ZmQuickAddDialog;
-ZmNewRosterItemDialog.prototype.constructor = ZmNewRosterItemDialog;
-
-ZmNewRosterItemDialog.prototype.toString =
-function() {
-	return "ZmNewRosterItemDialog";
+	this._tabGroup.addMember(this._serviceTypeSelect);
+	this._tabGroup.addMember(this._addrEntry);
+	this._tabGroup.addMember(this._nameEntry);
+	this._tabGroup.addMember(this._groupsEntry);
 };
 
-ZmNewRosterItemDialog.prototype._contentHtml =
-function() {
-	this._addressFieldId = Dwt.getNextId();
-	this._nameFieldId = Dwt.getNextId();
-	this._groupsFieldId = Dwt.getNextId();
-
-	var html = new AjxBuffer();
-	//	html.append("<table cellpadding='0' cellspacing='5' border='0'>");
-	html.append("<table border='0' width=325>");
-
-	this._serviceTypeId = Dwt.getNextId();
-
-	html.append("<tr valign='center'><td class='ZmChatDialogField'>", ZmMsg.imGateway, "</td>");
-	html.append("<td id='" + this._serviceTypeId + "'>");
-	html.append("</td></tr>");
-
-	html.append("<tr valign='center'><td class='ZmChatDialogField'>", ZmMsg.imAddressLabel, "</td>");
-	html.append("<td>");
-	html.append(Dwt.CARET_HACK_BEGIN);
-	html.append("<input autocomplete=OFF type='text' style='width:100%; height:22px' id='", this._addressFieldId, "' />");
-	html.append(Dwt.CARET_HACK_END);
-	html.append("</td></tr>");
-
-	html.append("<tr valign='center'><td class='ZmChatDialogField'>", ZmMsg.imNameLabel, "</td>");
-	html.append("<td>");
-	html.append(Dwt.CARET_HACK_BEGIN);
-	html.append("<input autocomplete=OFF type='text' style='width:100%; height:22px' id='", this._nameFieldId, "' />");
-	html.append(Dwt.CARET_HACK_END);
-	html.append("</td></tr>");
-
-	html.append("<tr valign='center'><td class='ZmChatDialogField'>", ZmMsg.imGroupsLabel, "</td>");
-	html.append("<td>");
-	html.append(Dwt.CARET_HACK_BEGIN);
-	html.append("<input autocomplete=OFF type='text' style='width:100%; height:22px' id='", this._groupsFieldId, "' />");
-	html.append(Dwt.CARET_HACK_END);
-	html.append("</td></tr>");
-	html.append("</table>");
-
-	return html.toString();
+ZmNewRosterItemDialog.prototype._contentHtml = function() {
+	var id = this._baseId = Dwt.getNextId();
+	return AjxTemplate.expand("zimbraMail.im.templates.Chat#NewRosterItemDlg",
+				  { id : id });
 };
 
-ZmNewRosterItemDialog.prototype.popup =
-function(loc) {
+ZmNewRosterItemDialog.prototype.popup = function(loc) {
 	DwtDialog.prototype.popup.call(this, loc);
-	document.getElementById(this._addressFieldId).focus();
+	this._serviceTypeSelect.focus();
 };
 
-
-ZmNewRosterItemDialog.prototype._okButtonListener =
-function(ev) {
+ZmNewRosterItemDialog.prototype._okButtonListener = function(ev) {
 	var results = this._getRosterItemData();
 	if (results)
 		DwtDialog.prototype._buttonListener.call(this, ev, results);
 };
 
-ZmNewRosterItemDialog.prototype._getRosterItemData =
-function() {
-	var name = AjxStringUtil.trim(document.getElementById(this._nameFieldId).value);
+ZmNewRosterItemDialog.prototype._getRosterItemData = function() {
+	var name = AjxStringUtil.trim(this._nameEntry.getValue());
 	var msg = ZmRosterItem.checkName(name);
 
-	var address = AjxStringUtil.trim(document.getElementById(this._addressFieldId).value);
+	var address = AjxStringUtil.trim(this._addrEntry.getValue());
 	if (address) address = address.replace(/;$/, "");
 	address = AjxDispatcher.run("GetRoster").makeServerAddress(address, this._serviceTypeSelect.getValue());
 	if (!msg) msg = ZmRosterItem.checkAddress(address);
 
-	var groups = AjxStringUtil.trim(document.getElementById(this._groupsFieldId).value);
+	var groups = AjxStringUtil.trim(this._groupsEntry.getValue());
 	if (groups) groups = groups.replace(/,$/, "");
 	if (!msg) msg = ZmRosterItem.checkGroups(groups);
 
 	return (msg ? this._showError(msg) : [address, name, groups]);
 };
 
-ZmNewRosterItemDialog.prototype.reset =
-function() {
+ZmNewRosterItemDialog.prototype.reset = function() {
 	ZmDialog.prototype.reset.call(this);
-	var field = document.getElementById(this._addressFieldId);
-	field.value = "";
-	//field.readOnly = false;
-	field.disabled = false;
+	this._addrEntry.setValue("");
+	this._addrEntry.setEnabled(true);
 	this._serviceTypeSelect.setEnabled(true);
-	document.getElementById(this._nameFieldId).value = "";
-	document.getElementById(this._groupsFieldId).value = "";
+	this._nameEntry.setValue("");
+	this._groupsEntry.setValue("");
 };
 
-ZmNewRosterItemDialog.prototype.setGroups =
-function(newGroups) {
-	document.getElementById(this._groupsFieldId).value = newGroups || "";
+ZmNewRosterItemDialog.prototype.setGroups = function(newGroups) {
+	this._groupsEntry.setValue(newGroups || "");
 };
 
-ZmNewRosterItemDialog.prototype.setName =
-function(newName) {
-	document.getElementById(this._nameFieldId).value = newName || "";
+ZmNewRosterItemDialog.prototype.setName = function(newName) {
+	this._nameEntry.setValue(newName || "");
 };
 
-ZmNewRosterItemDialog.prototype.setAddress =
-function(newAddress, readonly) {
-	var field = document.getElementById(this._addressFieldId);
+ZmNewRosterItemDialog.prototype.setAddress = function(newAddress, readonly) {
 	var a = AjxDispatcher.run("GetRoster").breakDownAddress(newAddress);
 	if (a.type) {
 		this._serviceTypeSelect.setSelectedValue(a.type);
 		newAddress = a.addr;
 	}
-	field.value = newAddress;
-	//if (readonly) field.readOnly = true;
+	this._addrEntry.setValue(newAddress);
 	if (readonly) {
-		field.disabled = true;
+		this._addrEntry.setEnabled(false);
 		this._serviceTypeSelect.setEnabled(false);
 	}
 };
 
 
-ZmNewRosterItemDialog.prototype._initAddressAutocomplete =
-function() {
+ZmNewRosterItemDialog.prototype._initAddressAutocomplete = function() {
 	if (this._addressAutocomplete || !this._appCtxt.get(ZmSetting.CONTACTS_ENABLED))
 		return;
 
 	var shell = this._appCtxt.getShell();
 	var contactsApp = shell ? shell.getData(ZmAppCtxt.LABEL).getApp(ZmApp.CONTACTS) : null;
 	var contactsList = contactsApp ? contactsApp.getContactList : null;
-	var params = {parent: shell, dataClass: contactsApp, dataLoader: contactsList,
-				  matchValue: ZmContactsApp.AC_VALUE_EMAIL};
+	var params = { parent	  : shell,
+		       dataClass  : contactsApp,
+		       dataLoader : contactsList,
+		       matchValue : ZmContactsApp.AC_VALUE_EMAIL
+		     };
 	this._addressAutocomplete = new ZmAutocompleteListView(params);
-	this._addressAutocomplete.handle(document.getElementById(this._addressFieldId));
+	this._addressAutocomplete.handle(this._addrEntry.getInputElement());
 };
 
-ZmNewRosterItemDialog.prototype._initGroupAutocomplete =
-function() {
+ZmNewRosterItemDialog.prototype._initGroupAutocomplete = function() {
 	if (this._groupAutocomplete) return;
 
 	var shell = this._appCtxt.getShell();
 	var imApp = shell ? shell.getData(ZmAppCtxt.LABEL).getApp(ZmApp.IM) : null;
 	var groupList = imApp ? imApp.getAutoCompleteGroups : null;
-	var params = {parent: shell, dataClass: imApp, dataLoader: groupList,
-				  matchValue: "text", separator: ','};
+	var params = { parent	  : shell,
+		       dataClass  : imApp,
+		       dataLoader : groupList,
+		       matchValue : "text",
+		       separator  : ','
+		     };
 	this._groupAutocomplete = new ZmAutocompleteListView(params);
-	this._groupAutocomplete.handle(document.getElementById(this._groupsFieldId));
+	this._groupAutocomplete.handle(this._groupsEntry.getInputElement());
 };
 
-ZmNewRosterItemDialog.prototype._showError =
-function(msg, loc) {
+ZmNewRosterItemDialog.prototype._showError = function(msg, loc) {
 	var msgDialog = this._appCtxt.getMsgDialog();
 	msgDialog.reset();
 	loc = loc ? loc : new DwtPoint(this.getLocation().x + 50, this.getLocation().y + 100);
@@ -204,4 +173,3 @@ function(msg, loc) {
 	msgDialog.popup(loc);
 	return null;
 };
-
