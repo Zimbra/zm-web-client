1b12d64898bb2a765a00df1bfd904691e9896ae6
Unknown Perforce User <mihai>
2007-06-17 07:43:27 -0500

    Bug: 14156
    Ugh, typo (posStyle is the 4th argument)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=14156
    
    Copied from Perforce
     Change: 56215

--- WebRoot/js/zimbraMail/im/view/ZmImOverview.js
+++ WebRoot/js/zimbraMail/im/view/ZmImOverview.js
@@ -1,5 +1,5 @@
 ZmImOverview = function(appCtxt, parent) {
-	DwtComposite.call(this, parent, Dwt.ABSOLUTE_STYLE);
+	DwtComposite.call(this, parent, null, Dwt.ABSOLUTE_STYLE);
 
 	this.setScrollStyle(DwtControl.SCROLL);
 
