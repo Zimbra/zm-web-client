c89d22b2b31051948ded934c4be2cacb7bc00049
Unknown Perforce User <dcomfort>
2007-08-02 16:29:22 -0500

    Bug: 19068 Integrate Change 61622  from franklin_d3 License
    http://bugzilla.zimbra.com/show_bug.cgi?id=19068
    
    Copied from Perforce
     Change: 61627

--- WebRoot/js/zimbraMail/voicemail/view/ZmSoundPlayer.js
+++ WebRoot/js/zimbraMail/voicemail/view/ZmSoundPlayer.js
@@ -1,17 +1,26 @@
 /*
- * Copyright (C) 2006, The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: ZPL 1.2
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.2 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Web Client
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
  */
 
 
