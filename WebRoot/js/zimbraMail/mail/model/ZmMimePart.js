d07619b38f79c2d42ccb1996acf493f188203018
Unknown Perforce User <pshah>
2007-08-24 13:20:48 -0500

    bug: 17649 - Integrate change 57084 from main to FRANK
    http://bugzilla.zimbra.com/show_bug.cgi?id=17649
    
    Copied from Perforce
     Change: 64437

--- WebRoot/js/zimbraMail/mail/model/ZmMimePart.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMimePart.js
@@ -116,6 +116,9 @@ function(partNode, attachments, bodyParts) {
 		if (this.node.body &&
 			(this.node.ct == ZmMimeTable.TEXT_HTML || this.node.ct == ZmMimeTable.TEXT_PLAIN))
 		{
+			// add subsequent body parts as attachments if already found
+			if (ZmMimePart._contentTypeFound(bodyParts, this.node))
+				attachments.push(this.node);
 			bodyParts.push(this.node);
 		}
 
@@ -125,4 +128,13 @@ function(partNode, attachments, bodyParts) {
 			this.children.add(ZmMimePart.createFromDom(this.node.mp, params));
 		}
 	}
+};
+
+ZmMimePart._contentTypeFound =
+function(bodyParts, node) {
+	for (var i = 0; i < bodyParts.length; i++) {
+		if (bodyParts[i].ct == node.ct)
+			return true;
+	}
+	return false;
 };
