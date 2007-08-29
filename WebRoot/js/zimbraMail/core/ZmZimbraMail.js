bd708b892ae22be06311a781a07f31f83fc5adc9
Unknown Perforce User <dcomfort>
2007-08-29 11:53:30 -0500

    Bug: 19444 Make upsells full screen
    http://bugzilla.zimbra.com/show_bug.cgi?id=19444
    
    Copied from Perforce
     Change: 65085

--- WebRoot/js/zimbraMail/core/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/core/ZmZimbraMail.js
@@ -1514,7 +1514,7 @@ function(appName) {
 	htmlArr[idx++] = "'>";
 	el.innerHTML = htmlArr.join("");
 	var elements = {};
-	elements[ZmAppViewMgr.C_APP_CONTENT] = upsellView;
+	elements[ZmAppViewMgr.C_APP_CONTENT_FULL] = upsellView;
 	var viewName = [appName, "upsell"].join("_");
 	this._appViewMgr.createView(viewName, appName, elements, null, true);
 	this._appViewMgr.pushView(viewName);
