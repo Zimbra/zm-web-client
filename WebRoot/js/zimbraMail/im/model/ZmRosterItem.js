22a6d504227038495f80030b0ca2b9c7ab2b9170
Unknown Perforce User <mihai>
2007-10-17 10:19:28 -0500

    Bug: 14156
    Convert buddy address (XMPP -> service://buddy) before we lookup the
    corresponding contact.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=14156
    
    Copied from Perforce
     Change: 70795

--- WebRoot/js/zimbraMail/im/model/ZmRosterItem.js
+++ WebRoot/js/zimbraMail/im/model/ZmRosterItem.js
@@ -1,17 +1,17 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2005, 2006, 2007 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */
 ZmRosterItem = function(id, list, name, presence, groupNames) {
@@ -162,7 +162,11 @@ ZmRosterItem.prototype.getGroupNames = function() { return this.groupNames; };
 ZmRosterItem.prototype.getName = function() {	return this.name; }
 
 ZmRosterItem.prototype.getContact = function() {
-	return AjxDispatcher.run("GetContacts").getContactByIMAddress(this.id);
+        var roster = AjxDispatcher.run("GetRoster");
+        var addr = roster.makeGenericAddress(this.id);
+        if (addr)
+	        return AjxDispatcher.run("GetContacts").getContactByIMAddress(addr);
+        // undef if not found
 };
 
 ZmRosterItem.prototype.getDisplayName = function() {
