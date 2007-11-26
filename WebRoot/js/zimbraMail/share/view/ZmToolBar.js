8123160c96a97661f1a37286ecc2fd06a1343772
Conrad Damon <cdamon@zimbra.com>
2007-11-26 12:46:40 -0600

    Bug: none
    Add safety check
    
    Copied from Perforce
     Change: 74764

--- WebRoot/js/zimbraMail/share/view/ZmToolBar.js
+++ WebRoot/js/zimbraMail/share/view/ZmToolBar.js
@@ -145,19 +145,20 @@ function(button) {
 
 ZmToolBar.prototype.autoAdjustWidth =
 function(refElement) {
+	if (!refElement) { return; }
     var el = this.getHtmlElement();
-	if(!el) return;
-    for(var i in this._buttons) {
+	if (!el) { return; }
+    for (var i in this._buttons) {
 		var offset1 = refElement.offsetWidth;
 		var offset2 = el.firstChild ? el.firstChild.offsetWidth : offset1;
-		if(this._buttons[i].getImage()!=null ) {	
-			if(offset2 > offset1){
-				this._buttons[i]._toggleText = (this._buttons[i]._toggleText!=null && this._buttons[i]._toggleText!="") ? this._buttons[i]._toggleText : this._buttons[i].getText();
+		if (this._buttons[i].getImage() != null ) {	
+			if (offset2 > offset1){
+				this._buttons[i]._toggleText = (this._buttons[i]._toggleText != null && this._buttons[i]._toggleText != "") ? this._buttons[i]._toggleText : this._buttons[i].getText();
 				this._buttons[i].setText("");
-			}else if(this._buttons[i]._toggleText) {
+			} else if(this._buttons[i]._toggleText) {
 				this._buttons[i].setText(this._buttons[i]._toggleText);
 				this._buttons[i]._toggleText = null;
 			}
-		 }
+		}
 	}
 };
