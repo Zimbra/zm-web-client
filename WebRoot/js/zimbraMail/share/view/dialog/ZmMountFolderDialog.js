6033699d2c267e277cc427f75744c9ca5bad4834
Unknown Perforce User <pshah>
2007-12-03 13:29:01 -0600

    bug: 22282 - checkpoint -- templatize ZmMountFolderDialog
    http://bugzilla.zimbra.com/show_bug.cgi?id=22282
    
    Copied from Perforce
     Change: 75705

--- WebRoot/js/zimbraMail/share/view/dialog/ZmMountFolderDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmMountFolderDialog.js
@@ -19,18 +19,21 @@ ZmMountFolderDialog = function(shell, className) {
 	className = className || "ZmMountFolderDialog";
 	var title = ZmMsg[ZmOrganizer.MOUNT_KEY[ZmOrganizer.FOLDER]];
 	DwtDialog.call(this, shell, className, title);
+
 	this.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._handleOkButton));
 
-	// create auto-completer
+	// create auto-complete
 	if (appCtxt.get(ZmSetting.CONTACTS_ENABLED)) {
 		var dataClass = appCtxt.getApp(ZmApp.CONTACTS);
-		var dataLoader = dataClass.getContactList;
-		var locCallback = new AjxCallback(this, this._getNewAutocompleteLocation, [this]);
-		var compCallback = new AjxCallback(this, this._handleCompletionData, [this]);
-		var params = {parent: this, dataClass: dataClass, dataLoader: dataLoader,
-					  matchValue: ZmContactsApp.AC_VALUE_EMAIL, locCallback: locCallback,
-					  compCallback: compCallback,
-					  keyUpCallback: new AjxCallback(this, this._acKeyUpListener) };
+		var params = {
+			parent: this,
+			dataClass: dataClass,
+			dataLoader: dataClass.getContactList,
+			matchValue: ZmContactsApp.AC_VALUE_EMAIL,
+			locCallback: (new AjxCallback(this, this._getNewAutocompleteLocation)),
+			compCallback: (new AjxCallback(this, this._handleCompletionData)),
+			keyUpCallback: (new AjxCallback(this, this._acKeyUpListener))
+		};
 		this._acAddrSelectList = new ZmAutocompleteListView(params);
 	}
 
@@ -69,18 +72,19 @@ function(organizerType, folderId, user, path) {
 
 	// show
 	DwtDialog.prototype.popup.call(this);
-	ZmMountFolderDialog._enableFieldsOnEdit(this);
+
+	this._enableFieldsOnEdit();
 };
 
 // Auto-complete methods
 
 ZmMountFolderDialog.prototype._acKeyUpListener =
-function(event, aclv, result) {
-	ZmMountFolderDialog._handleOtherKeyUp(event);
+function(ev, aclv, result) {
+	this._handleOtherKeyUp(ev);
 };
 
 ZmMountFolderDialog.prototype._handleCompletionData =
-function (control, text, element) {
+function(text, element, match) {
 	element.value = text.replace(/;\s*/g,"");
 	try {
 		if (element.fireEvent) {
@@ -97,99 +101,82 @@ function (control, text, element) {
 };
 
 ZmMountFolderDialog.prototype._getNewAutocompleteLocation =
-function(cv, ev) {
+function(ev) {
 	var element = ev.element;
-	var id = element.id;
-
-	var viewEl = this.getHtmlElement();
-	var location = Dwt.toWindow(element, 0, 0, viewEl);
+	var location = Dwt.toWindow(element, 0, 0, this.getHtmlElement());
 	var size = Dwt.getSize(element);
-	return new DwtPoint((location.x), (location.y + size.y) );
+	return (new DwtPoint((location.x), (location.y + size.y)));
 };
 
 // Protected functions
 
-ZmMountFolderDialog._handleOtherKeyUp =
-function(event) {
-	var value = ZmMountFolderDialog._handleKeyUp(event);
+ZmMountFolderDialog.prototype._handleOtherKeyUp =
+function(ev) {
+	var value = this._handleKeyUp(ev);
 
-	var target = DwtUiEvent.getTarget(event);
-	var inputField = Dwt.getObjectFromElement(target);
-	var dialog = inputField.parent.parent;
-
-	if (!dialog._nameInputDirty) {
-		var user = dialog._userInput.getValue();
-		var path = dialog._pathInput.getValue();
+	if (!this._nameInputDirty) {
+		var user = this._userInput.getValue();
+		var path = this._pathInput.getValue();
 
 		if (user != "" && path != "") {
-			if (!dialog._nameFormatter) {
-				dialog._nameFormatter = new AjxMessageFormat(ZmMsg.shareNameDefault);
+			if (!this._nameFormatter) {
+				this._nameFormatter = new AjxMessageFormat(ZmMsg.shareNameDefault);
 			}
 
 			user = user.replace(/@.*/,"");
 			user = user.substr(0,1).toUpperCase() + user.substr(1);
-
 			path = path.replace(/\/$/,"");
 			path = path.replace(/^.*\//,"");
 
 			var args = [user, path];
-			dialog._nameInput.setValue(dialog._nameFormatter.format(args));
+			this._nameInput.setValue(this._nameFormatter.format(args));
 		}
 	}
 
 	return value;
 };
 
-ZmMountFolderDialog._handleNameKeyUp =
-function(event) {
-	var target = DwtUiEvent.getTarget(event);
-	var inputField = Dwt.getObjectFromElement(target);
-	var dialog = inputField.parent.parent;
-
-	dialog._nameInputDirty = true;
-
-	return ZmMountFolderDialog._handleKeyUp(event);
+ZmMountFolderDialog.prototype._handleNameKeyUp =
+function(ev) {
+	this._nameInputDirty = true;
+	return this._handleKeyUp(ev);
 };
 
-ZmMountFolderDialog._handleKeyUp =
-function(event) {
-	if (DwtInputField._keyUpHdlr(event)) {
-		var target = DwtUiEvent.getTarget(event);
-		var inputField = Dwt.getObjectFromElement(target);
-		var dialog = inputField.parent.parent;
-		return ZmMountFolderDialog._enableFieldsOnEdit(dialog);
-	}
-	return false;
+ZmMountFolderDialog.prototype._handleKeyUp =
+function(ev) {
+	return (DwtInputField._keyUpHdlr(ev)) ? this._enableFieldsOnEdit() : false;
 };
 
-ZmMountFolderDialog._enableFieldsOnEdit =
-function(dialog) {
-	var user = dialog._userInput.getValue();
-	var path = dialog._pathInput.getValue();
-	var name = dialog._nameInput.getValue();
+ZmMountFolderDialog.prototype._enableFieldsOnEdit =
+function() {
+	var user = this._userInput.getValue();
+	var path = this._pathInput.getValue();
+	var name = this._nameInput.getValue();
 	var enabled = user.length > 0 && user.match(/\S/) &&
 				  path.length > 0 && path.match(/\S/) && !path.match(/^\/+$/) &&
 				  name.length > 0 && name.match(/\S/);
-	dialog.setButtonEnabled(DwtDialog.OK_BUTTON, enabled);
+	this.setButtonEnabled(DwtDialog.OK_BUTTON, enabled);
 };
 
 // Protected methods
 
 ZmMountFolderDialog.prototype._handleOkButton =
-function(event) {
+function(ev) {
 	var params = {
 		"l": this._folderId,
 		"name": this._nameInput.getValue(),
 		"owner": this._userInput.getValue(),
 		"path": this._pathInput.getValue(),
-		"view": ZmOrganizer.VIEWS[this._organizerType][0] || ZmOrganizer.VIEWS[ZmOrganizer.FOLDER][0],
+		"view": (ZmOrganizer.VIEWS[this._organizerType][0] || ZmOrganizer.VIEWS[ZmOrganizer.FOLDER][0]),
 		"color": this._colorSelect.getValue()
 	};
-	if (appCtxt.get(ZmSetting.CALENDAR_ENABLED)) {
-		if (this._organizerType == ZmOrganizer.CALENDAR) {
-			params.f = ZmOrganizer.FLAG_CHECKED;
-		}
+
+	if (appCtxt.get(ZmSetting.CALENDAR_ENABLED) &&
+		this._organizerType == ZmOrganizer.CALENDAR)
+	{
+		params.f = ZmOrganizer.FLAG_CHECKED;
 	}
+
 	var callback = new AjxCallback(this, this.popdown);
 	var errorCallback = new AjxCallback(this, this._handleCreateError);
 
@@ -200,81 +187,48 @@ ZmMountFolderDialog.prototype._handleCreateError =
 function(response) {
 	var code = response.code;
 	if (code == ZmCsfeException.SVC_PERM_DENIED ||
-		code == ZmCsfeException.MAIL_NO_SUCH_FOLDER) {
+		code == ZmCsfeException.MAIL_NO_SUCH_FOLDER)
+	{
 		var msg = ZmCsfeException.getErrorMsg(code);
-
-		var controller = appCtxt.getAppController();
-		controller.popupErrorDialog(msg, null, null, true);
-
+		appCtxt.getAppController().popupErrorDialog(msg, null, null, true);
 		return true;
 	}
 };
 
 ZmMountFolderDialog.prototype._createMountHtml =
 function() {
-	// create instructional elements
-	var instructEl1 = document.createElement("DIV");
-	instructEl1.innerHTML = ZmMsg.mountInstructions1;
-	instructEl1.style.width = "30em";
-	instructEl1.style.marginBottom = "0.5em";
 
-	var instructEl2 = document.createElement("DIV");
-	instructEl2.innerHTML = ZmMsg.mountInstructions2;
-	instructEl2.style.marginTop = "0.5em";
-	instructEl2.style.marginBottom = "0.5em";
+	this._getContentDiv().innerHTML = AjxTemplate.expand("share.Dialogs#ZmMountFolderDialog", {id: this._htmlElId})
 
-	// create components
-	var props1 = new DwtPropertySheet(this);
-	var props2 = new DwtPropertySheet(this);
-
-	var params = { parent: props1, required: true };
+	var params = {parent:this, required:true};
 	this._userInput = new DwtInputField(params);
+	this._userInput.reparentHtmlElement(this._htmlElId + "_email");
 	this._pathInput = new DwtInputField(params);
-
-	var params = { parent: props2, required: true };
+	this._pathInput.reparentHtmlElement(this._htmlElId + "_path");
 	this._nameInput = new DwtInputField(params);
+	this._nameInput.reparentHtmlElement(this._htmlElId + "_name");
 
-	this._colorSelect = new DwtSelect(props1);
+	this._colorSelect = new DwtSelect(this);
 	for (var i = 0; i < ZmOrganizer.COLOR_CHOICES.length; i++) {
 		var choice = ZmOrganizer.COLOR_CHOICES[i];
 		this._colorSelect.addOption(choice.label, i == 0, choice.value);
 	}
-
-	// setup property sheets
-	props1.addProperty(ZmMsg.emailLabel, this._userInput);
-	props1.addProperty(ZmMsg.pathLabel, this._pathInput);
-
-	props2.addProperty(ZmMsg.nameLabel, this._nameInput);
-	props2.addProperty(ZmMsg.colorLabel, this._colorSelect);
-
-	var propsEl1 = props1.getHtmlElement();
-	var propsEl2 = props2.getHtmlElement();
-
-	propsEl1.style.marginLeft = "1em";
-	propsEl2.style.marginLeft = "1em";
+	this._colorSelect.reparentHtmlElement(this._htmlElId + "_color");
 
 	// setup input fields
 	var inputEl = this._userInput.getInputElement();
 	inputEl.style.width = "25em";
 	if (this._acAddrSelectList) {
 		this._acAddrSelectList.handle(inputEl);
-	}
-	else {
-		Dwt.setHandler(inputEl, DwtEvent.ONKEYUP, ZmMountFolderDialog._handleOtherKeyUp);
+	} else {
+		inputEl.onkeyup = AjxCallback.simpleClosure(this._handleOtherKeyUp, this);
 	}
 
-	var inputEl = this._pathInput.getInputElement();
+	inputEl = this._pathInput.getInputElement();
 	inputEl.style.width = "25em";
-	Dwt.setHandler(inputEl, DwtEvent.ONKEYUP, ZmMountFolderDialog._handleOtherKeyUp);
+	inputEl.onkeyup = AjxCallback.simpleClosure(this._handleOtherKeyUp, this);
 
-	var inputEl = this._nameInput.getInputElement();
-	inputEl.style.width = "20em";
-	Dwt.setHandler(inputEl, DwtEvent.ONKEYUP, ZmMountFolderDialog._handleNameKeyUp);
-
-	// add to dialog
-	var element = this._getContentDiv();
-	element.appendChild(instructEl1);
-	element.appendChild(propsEl1);
-	element.appendChild(instructEl2);
-	element.appendChild(propsEl2);
+	inputEl = this._nameInput.getInputElement();
+	inputEl.style.width = "25em";
+	inputEl.onkeyup = AjxCallback.simpleClosure(this._handleNameKeyUp, this);
 };
