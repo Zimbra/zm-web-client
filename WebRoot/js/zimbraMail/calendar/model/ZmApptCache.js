2bf5f2e8fbb848528bbcf83fbab4b055b877e230
Unknown Perforce User <pshah>
2008-01-18 13:17:14 -0600

    bug: 23213 - minor code cleanup
    http://bugzilla.zimbra.com/show_bug.cgi?id=23213
    
    Copied from Perforce
     Change: 80557

--- WebRoot/js/zimbraMail/calendar/model/ZmApptCache.js
+++ WebRoot/js/zimbraMail/calendar/model/ZmApptCache.js
@@ -225,16 +225,16 @@ function(params) {
 	}
 	params.queryHint = query;
 	params.folderIdMapper = folderIdMapper;
+	params.offset = 0;
 
 	// this array will hold a list of appts as we collect them from the server
 	this._rawAppts = [];
 
-    params.offset = 0;
 	this._search(params);
 };
 
 ZmApptCache.prototype._search =
-function(params, offset) {
+function(params) {
 	var soapDoc = AjxSoapDoc.create("SearchRequest", "urn:zimbraMail");
 
 	var method = soapDoc.getMethod();
@@ -288,8 +288,8 @@ function(params, result) {
 		if (searchResp.more) {
 			var lastAppt = searchResp.appt[searchResp.appt.length-1];
 			if (lastAppt) {
-                params.offset+=500;
-				this._search(params, lastAppt.id, lastAppt.sf);
+                params.offset += 500;
+				this._search(params);
 				return;
 			}
 		}
