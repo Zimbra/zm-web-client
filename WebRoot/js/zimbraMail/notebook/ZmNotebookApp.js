ff446de7adad57d1aae481cf9db01bf2f0bcb860
Unknown Perforce User <ssugumaran>
2008-01-17 19:34:29 -0600

    bug: 22881, 23244 integrated to franklin
    http://bugzilla.zimbra.com/show_bug.cgi?id=22881
    http://bugzilla.zimbra.com/show_bug.cgi?id=23244
    
    Copied from Perforce
     Change: 80429

--- WebRoot/js/zimbraMail/notebook/ZmNotebookApp.js
+++ WebRoot/js/zimbraMail/notebook/ZmNotebookApp.js
@@ -303,6 +303,7 @@ function(creates, force) {
 
 ZmNotebookApp.prototype.modifyNotify =
 function(modifies, force) {
+	if(!modifies) return;
 	if (!modifies["w"] && !modifies["doc"]) { return; }
 	if (!force && !this._noDefer && this._deferNotifications("modify", list)) { return; }
 	
