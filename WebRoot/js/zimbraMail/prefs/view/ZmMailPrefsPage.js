41b237ea67d9109ae6def65654ee14a12adba3d3
Unknown Perforce User <rpatil>
2008-02-12 14:04:17 -0600

    bug: 10071 Added UI support for Start and End date for auto reply (Vacation Message).
    http://bugzilla.zimbra.com/show_bug.cgi?id=10071
    
    Copied from Perforce
     Change: 83454

--- WebRoot/js/zimbraMail/prefs/view/ZmMailPrefsPage.js
+++ WebRoot/js/zimbraMail/prefs/view/ZmMailPrefsPage.js
@@ -39,11 +39,91 @@ ZmMailPrefsPage.prototype.reset = function(useDefaults) {
 
 ZmMailPrefsPage.prototype._createControls = function() {
 	ZmPreferencesPage.prototype._createControls.apply(this, arguments);
-	var cbox = this.getFormObject(ZmSetting.VACATION_MSG_ENABLED);
+
+    this._sId = this._htmlElId + "_startMiniCal";
+    this._eId = this._htmlElId + "_endMiniCal";
+    this._startDateField 	= document.getElementById(this._htmlElId + "_VACATION_FROM1");
+    this._endDateField 		= document.getElementById(this._htmlElId + "_VACATION_UNTIL1");
+
+    this._startDateVal 	= document.getElementById(this._htmlElId + "_VACATION_FROM");
+    this._endDateVal 		= document.getElementById(this._htmlElId + "_VACATION_UNTIL");
+    this._formatter = new AjxDateFormat("yyyyMMddHHmmss'Z'");
+
+    
+    this._startDateField.value = AjxDateUtil.simpleComputeDateStr(this._formatter.parse(this._startDateVal.value));
+    this._endDateField.value = AjxDateUtil.simpleComputeDateStr(this._formatter.parse(this._endDateVal.value));
+
+
+    /*this._startDateVal.value =  this._formatter.format(AjxDateUtil.simpleParseDateStr(this._startDateField.value) || new Date());
+    this._endDateVal.value =  this._formatter.format(AjxDateUtil.simpleParseDateStr(this._endDateField.value) || new Date());//this._formatter.format(this._endDateField.value);*/
+
+    /*var today =  new Date();
+    this._startDateField.value = AjxDateUtil.simpleComputeDateStr(today);
+    today.setDate(today.getDate()+1);
+    var nextDay = AjxDateUtil.getDateForNextDay(today,AjxDateUtil.FRIDAY,1)
+    this._endDateField.value = AjxDateUtil.simpleComputeDateStr(nextDay);*/
+    
+    var dateButtonListener = new AjxListener(this, this._dateButtonListener);
+    var dateCalSelectionListener = new AjxListener(this, this._dateCalSelectionListener);
+
+    this._startDateButton = ZmCalendarApp.createMiniCalButton(this, this._sId, dateButtonListener, dateCalSelectionListener, true);
+    this._endDateButton = ZmCalendarApp.createMiniCalButton(this, this._eId, dateButtonListener, dateCalSelectionListener, true);
+
+    var cbox = this.getFormObject(ZmSetting.VACATION_MSG_ENABLED);
 	if (cbox) {
 		this._handleEnableVacationMsg(cbox);
 	}
 };
+
+ZmMailPrefsPage.prototype._dateButtonListener =
+function(ev) {
+	var calDate = ev.item == this._startDateButton
+		? AjxDateUtil.simpleParseDateStr(this._startDateField.value)
+		: AjxDateUtil.simpleParseDateStr(this._endDateField.value);
+
+	// if date was input by user and its foobar, reset to today's date
+	if (isNaN(calDate)) {
+		calDate = new Date();
+		var field = ev.item == this._startDateButton
+			? this._startDateField : this._endDateField;
+		field.value = AjxDateUtil.simpleComputeDateStr(calDate);
+	}
+
+	// always reset the date to current field's date
+	var menu = ev.item.getMenu();
+    
+    var cal = menu.getItem(0);
+	cal.setDate(calDate, true);
+	ev.item.popup();
+};
+
+
+ZmMailPrefsPage.prototype._dateCalSelectionListener =
+function(ev) {
+	var parentButton = ev.item.parent.parent;
+
+	// do some error correction... maybe we can optimize this?
+	var sd = AjxDateUtil.simpleParseDateStr(this._startDateField.value);
+	var ed = AjxDateUtil.simpleParseDateStr(this._endDateField.value);
+	var newDate = AjxDateUtil.simpleComputeDateStr(ev.detail);
+
+	// change the start/end date if they mismatch
+	if (parentButton == this._startDateButton) {
+		if (ed.valueOf() < ev.detail.valueOf())
+			this._endDateField.value = AjxDateUtil.simpleComputeDateStr(AjxDateUtil.getDateForNextDay(AjxDateUtil.simpleParseDateStr(newDate),AjxDateUtil.FRIDAY));
+		this._startDateField.value = newDate;
+	} else {
+        if(ev.detail < new Date()) return;
+        if (sd.valueOf() > ev.detail.valueOf())
+			this._startDateField.value = newDate;
+		this._endDateField.value = newDate;
+	}
+    this._startDateVal.value =  this._formatter.format(AjxDateUtil.simpleParseDateStr(this._startDateField.value));
+    this._endDateVal.value =  this._formatter.format(AjxDateUtil.simpleParseDateStr(this._endDateField.value));
+    
+};
+
+
 
 ZmMailPrefsPage.prototype._setupCheckbox = function(id, setup, value) {
 	var cbox = ZmPreferencesPage.prototype._setupCheckbox.apply(this, arguments);
@@ -59,7 +139,14 @@ ZmMailPrefsPage.prototype._setupCheckbox = function(id, setup, value) {
 
 ZmMailPrefsPage.prototype._handleEnableVacationMsg = function(cbox, evt) {
 	var textarea = this.getFormObject(ZmSetting.VACATION_MSG);
-	if (textarea) {
+    if (textarea) {
 		textarea.setEnabled(cbox.isSelected());
-	}
+
+        this._startDateField.disabled = !cbox.isSelected();
+        this._endDateField.disabled = !cbox.isSelected();
+
+        this._startDateButton.setEnabled(cbox.isSelected());
+        this._endDateButton.setEnabled(cbox.isSelected());
+
+    }
 };
