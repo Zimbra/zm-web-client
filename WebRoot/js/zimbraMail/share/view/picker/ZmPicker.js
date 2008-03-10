7be862bc84cb837b9ad60c9e94fc90ec5e3a36f3
Unknown Perforce User <pshah>
2008-03-10 13:41:48 -0500

    bug: 25444 - offline: advanced search 'Tag' widget broken. cannot close it. -- Dont bother normalizing root Id
    http://bugzilla.zimbra.com/show_bug.cgi?id=25444
    
    Copied from Perforce
     Change: 87724

--- WebRoot/js/zimbraMail/share/view/picker/ZmPicker.js
+++ WebRoot/js/zimbraMail/share/view/picker/ZmPicker.js
@@ -20,16 +20,20 @@ ZmPicker = function(parent, id) {
 	DwtComposite.call(this, {parent:parent, className:"ZmPicker", posStyle:DwtControl.ABSOLUTE_STYLE});
 
 	this._header = new DwtToolBar({parent:this});
-	this._label = new DwtLabel(this._header, DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_LEFT, "ZmPickerLabel");
 	this._header.addFiller();
+
+	this._label = new DwtLabel(this._header, DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_LEFT, "ZmPickerLabel");
+
 	this._close = new DwtButton({parent:this._header, style:DwtLabel.IMAGE_LEFT, className:"DwtToolbarButton"});
 	this._close.setImage("Close");
 	this._close.setToolTipContent(ZmMsg.close);
+
 	this._picker = new DwtComposite(this, "ZmPickerOverview");
 	this._picker.setSize(Dwt.DEFAULT, parent.getH() - this._header.getH());
-	this._picker.setScrollStyle(DwtControl.SCROLL);
+	this._picker.setScrollStyle(Dwt.CLIP);
 	this._pickerEvent = new ZmEvent(ZmEvent.S_PICKER);
 	this._pickerEvent.set(ZmEvent.E_MODIFY, this);
+
 	this.id = id;
     this.setTitle(ZmMsg[ZmPicker.T_MSG_KEY[id]]);
     this.setImage(ZmPicker.IMAGE[id]);
@@ -57,46 +61,47 @@ ZmPicker.CLOSE		= i++;	// not really a picker
 
 ZmPicker.NEXT_ID	= i;
 
-ZmPicker.DEFAULT_PICKERS = [ZmPicker.ATTACHMENT,
-				ZmPicker.BASIC,
-				ZmPicker.DATE,
-				ZmPicker.DOMAIN,
-				ZmPicker.FOLDER];
-
+ZmPicker.DEFAULT_PICKERS = [
+	ZmPicker.ATTACHMENT,
+	ZmPicker.BASIC,
+	ZmPicker.DATE,
+	ZmPicker.DOMAIN,
+	ZmPicker.FOLDER
+];
 
 // Button labels
 ZmPicker.MSG_KEY = {};
-ZmPicker.MSG_KEY[ZmPicker.ATTACHMENT]	= "attachment";
-ZmPicker.MSG_KEY[ZmPicker.BASIC]		= "basic";
-ZmPicker.MSG_KEY[ZmPicker.CUSTOM]		= "custom";
-ZmPicker.MSG_KEY[ZmPicker.DATE]			= "date";
-ZmPicker.MSG_KEY[ZmPicker.DOMAIN]		= "domain";
-ZmPicker.MSG_KEY[ZmPicker.FLAG]			= "status";
-ZmPicker.MSG_KEY[ZmPicker.FOLDER]		= "folder";
-ZmPicker.MSG_KEY[ZmPicker.ZIMLET]		= "zimlets";
-ZmPicker.MSG_KEY[ZmPicker.SEARCH]		= "savedSearch";
-ZmPicker.MSG_KEY[ZmPicker.SIZE]			= "size";
-ZmPicker.MSG_KEY[ZmPicker.TAG]			= "tag";
-ZmPicker.MSG_KEY[ZmPicker.TIME]			= "time";
-ZmPicker.MSG_KEY[ZmPicker.RESET]		= "removeAll";
-ZmPicker.MSG_KEY[ZmPicker.CLOSE]		= "close";
+ZmPicker.MSG_KEY[ZmPicker.ATTACHMENT]		= "attachment";
+ZmPicker.MSG_KEY[ZmPicker.BASIC]			= "basic";
+ZmPicker.MSG_KEY[ZmPicker.CUSTOM]			= "custom";
+ZmPicker.MSG_KEY[ZmPicker.DATE]				= "date";
+ZmPicker.MSG_KEY[ZmPicker.DOMAIN]			= "domain";
+ZmPicker.MSG_KEY[ZmPicker.FLAG]				= "status";
+ZmPicker.MSG_KEY[ZmPicker.FOLDER]			= "folder";
+ZmPicker.MSG_KEY[ZmPicker.ZIMLET]			= "zimlets";
+ZmPicker.MSG_KEY[ZmPicker.SEARCH]			= "savedSearch";
+ZmPicker.MSG_KEY[ZmPicker.SIZE]				= "size";
+ZmPicker.MSG_KEY[ZmPicker.TAG]				= "tag";
+ZmPicker.MSG_KEY[ZmPicker.TIME]				= "time";
+ZmPicker.MSG_KEY[ZmPicker.RESET]			= "removeAll";
+ZmPicker.MSG_KEY[ZmPicker.CLOSE]			= "close";
 
 // Button and picker icons
 ZmPicker.IMAGE = {};
-ZmPicker.IMAGE[ZmPicker.ATTACHMENT]	= "Attachment";
-ZmPicker.IMAGE[ZmPicker.BASIC]		= "Message";
-ZmPicker.IMAGE[ZmPicker.CUSTOM]		= "Search";
-ZmPicker.IMAGE[ZmPicker.DATE]		= "Date";
-ZmPicker.IMAGE[ZmPicker.DOMAIN]		= "URL";
-ZmPicker.IMAGE[ZmPicker.FLAG]		= "FlagRed";
-ZmPicker.IMAGE[ZmPicker.FOLDER]		= "Folder";
-ZmPicker.IMAGE[ZmPicker.ZIMLET]		= "ZimbraIcon";
-ZmPicker.IMAGE[ZmPicker.SEARCH]		= "SearchFolder";
-ZmPicker.IMAGE[ZmPicker.SIZE]		= "Folder";
-ZmPicker.IMAGE[ZmPicker.TAG]		= "Tag";
-ZmPicker.IMAGE[ZmPicker.TIME]		= "Date";
-//ZmPicker.IMAGE[ZmPicker.RESET]		= "Close";	//MOW: no image for reset button
-ZmPicker.IMAGE[ZmPicker.CLOSE]		= "Close";
+ZmPicker.IMAGE[ZmPicker.ATTACHMENT]			= "Attachment";
+ZmPicker.IMAGE[ZmPicker.BASIC]				= "Message";
+ZmPicker.IMAGE[ZmPicker.CUSTOM]				= "Search";
+ZmPicker.IMAGE[ZmPicker.DATE]				= "Date";
+ZmPicker.IMAGE[ZmPicker.DOMAIN]				= "URL";
+ZmPicker.IMAGE[ZmPicker.FLAG]				= "FlagRed";
+ZmPicker.IMAGE[ZmPicker.FOLDER]				= "Folder";
+ZmPicker.IMAGE[ZmPicker.ZIMLET]				= "ZimbraIcon";
+ZmPicker.IMAGE[ZmPicker.SEARCH]				= "SearchFolder";
+ZmPicker.IMAGE[ZmPicker.SIZE]				= "Folder";
+ZmPicker.IMAGE[ZmPicker.TAG]				= "Tag";
+ZmPicker.IMAGE[ZmPicker.TIME]				= "Date";
+//ZmPicker.IMAGE[ZmPicker.RESET]			= "Close";	//MOW: no image for reset button
+ZmPicker.IMAGE[ZmPicker.CLOSE]				= "Close";
 
 // Button tooltips
 ZmPicker.TT_MSG_KEY = {};
@@ -117,34 +122,34 @@ ZmPicker.TT_MSG_KEY[ZmPicker.CLOSE]			= "closeSearchBuilder";
 
 // Picker titles
 ZmPicker.T_MSG_KEY = {};
-ZmPicker.T_MSG_KEY[ZmPicker.ATTACHMENT]	= "attachments";
-ZmPicker.T_MSG_KEY[ZmPicker.BASIC]		= "basicSearch";
-ZmPicker.T_MSG_KEY[ZmPicker.CUSTOM]		= "custom";
-ZmPicker.T_MSG_KEY[ZmPicker.DATE]		= "date";
-ZmPicker.T_MSG_KEY[ZmPicker.DOMAIN]		= "domains";
-ZmPicker.T_MSG_KEY[ZmPicker.FLAG]		= "status";
-ZmPicker.T_MSG_KEY[ZmPicker.FOLDER]		= "folders";
-ZmPicker.T_MSG_KEY[ZmPicker.ZIMLET]		= "zimlets";
-ZmPicker.T_MSG_KEY[ZmPicker.SEARCH]		= "savedSearches";
-ZmPicker.T_MSG_KEY[ZmPicker.SIZE]		= "size";
-ZmPicker.T_MSG_KEY[ZmPicker.TAG]		= "tags";
-ZmPicker.T_MSG_KEY[ZmPicker.TIME]		= "time";
+ZmPicker.T_MSG_KEY[ZmPicker.ATTACHMENT]		= "attachments";
+ZmPicker.T_MSG_KEY[ZmPicker.BASIC]			= "basicSearch";
+ZmPicker.T_MSG_KEY[ZmPicker.CUSTOM]			= "custom";
+ZmPicker.T_MSG_KEY[ZmPicker.DATE]			= "date";
+ZmPicker.T_MSG_KEY[ZmPicker.DOMAIN]			= "domains";
+ZmPicker.T_MSG_KEY[ZmPicker.FLAG]			= "status";
+ZmPicker.T_MSG_KEY[ZmPicker.FOLDER]			= "folders";
+ZmPicker.T_MSG_KEY[ZmPicker.ZIMLET]			= "zimlets";
+ZmPicker.T_MSG_KEY[ZmPicker.SEARCH]			= "savedSearches";
+ZmPicker.T_MSG_KEY[ZmPicker.SIZE]			= "size";
+ZmPicker.T_MSG_KEY[ZmPicker.TAG]			= "tags";
+ZmPicker.T_MSG_KEY[ZmPicker.TIME]			= "time";
 
 // Max number of instances for each picker
 // -1 means no limit
 ZmPicker.LIMIT = {};
-ZmPicker.LIMIT[ZmPicker.ATTACHMENT]	= 1;
-ZmPicker.LIMIT[ZmPicker.BASIC]		= -1;
-ZmPicker.LIMIT[ZmPicker.CUSTOM]		= 1;
-ZmPicker.LIMIT[ZmPicker.DATE]		= 2;
-ZmPicker.LIMIT[ZmPicker.DOMAIN]		= 2;
-ZmPicker.LIMIT[ZmPicker.FLAG]		= 4;
-ZmPicker.LIMIT[ZmPicker.FOLDER]		= 1;
-ZmPicker.LIMIT[ZmPicker.ZIMLET]		= -1; 
-ZmPicker.LIMIT[ZmPicker.SEARCH]		= -1;
-ZmPicker.LIMIT[ZmPicker.SIZE]		= 2;
-ZmPicker.LIMIT[ZmPicker.TAG]		= -1;
-ZmPicker.LIMIT[ZmPicker.TIME]		= 1;
+ZmPicker.LIMIT[ZmPicker.ATTACHMENT]			= 1;
+ZmPicker.LIMIT[ZmPicker.BASIC]				= -1;
+ZmPicker.LIMIT[ZmPicker.CUSTOM]				= 1;
+ZmPicker.LIMIT[ZmPicker.DATE]				= 2;
+ZmPicker.LIMIT[ZmPicker.DOMAIN]				= 2;
+ZmPicker.LIMIT[ZmPicker.FLAG]				= 4;
+ZmPicker.LIMIT[ZmPicker.FOLDER]				= 1;
+ZmPicker.LIMIT[ZmPicker.ZIMLET]				= -1;
+ZmPicker.LIMIT[ZmPicker.SEARCH]				= -1;
+ZmPicker.LIMIT[ZmPicker.SIZE]				= 2;
+ZmPicker.LIMIT[ZmPicker.TAG]				= -1;
+ZmPicker.LIMIT[ZmPicker.TIME]				= 1;
 
 ZmPicker.MULTI_JOIN = {};
 for (var i = 1; i <= ZmPicker.CLOSE; i++) {
@@ -239,11 +244,13 @@ function() {
 
 ZmPicker.prototype._setOverview =
 function(overviewId, parent, types) {
-	this._picker.setScrollStyle(Dwt.CLIP);
-	var opc = appCtxt.getOverviewController();
-	var params = {overviewId:overviewId, parent:parent,	headerClass:"DwtTreeItem",
-				  treeStyle:DwtTree.CHECKEDITEM_STYLE};
-	var overview = this._overview = opc.createOverview(params);
+	var params = {
+		overviewId: overviewId,
+		parent: parent,
+		headerClass: "DwtTreeItem",
+		treeStyle: DwtTree.CHECKEDITEM_STYLE
+	};
+	var overview = this._overview = appCtxt.getOverviewController().createOverview(params);
 	overview.set(types);
 	this._treeView = {};
 	for (var i = 0; i < types.length; i++) {
@@ -257,8 +264,7 @@ function(overviewId, parent, types) {
 
 ZmPicker.prototype._hideRoot =
 function(type) {
-	var rootId = ZmOrganizer.getSystemId(ZmOrganizer.ID_ROOT);
-	var ti = this._treeView[type].getTreeItemById(rootId);
+	var ti = this._treeView[type].getTreeItemById(ZmOrganizer.ID_ROOT);
 	Dwt.setVisible(ti._checkBoxCell, false);
 	ti.setExpanded(true);
 	ti.setVisible(false, true);
