e52a9acf89a0fa53ca46209c9ea6d820dc66a5f1
Unknown Perforce User <pshah>
2008-03-24 17:39:48 -0500

    bug: 26137 - offline: Showing wrong 'on behalf of' in drafts for multi account setup -- offline does not do on-behalf-of so don't test for it
    http://bugzilla.zimbra.com/show_bug.cgi?id=26137
    
    Copied from Perforce
     Change: 89917

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -1257,7 +1257,6 @@ function(soapDoc, parent, isDraft, accountName) {
 
 	if (accountName)
 	{
-        var mainAcct = ac.getMainAccount(true).getEmail();
         // when saving a draft, even if obo, we do it on the main account so reset the fro
         if (isDraft) {
 			var folder = appCtxt.getById(this.folderId);
@@ -1265,22 +1264,26 @@ function(soapDoc, parent, isDraft, accountName) {
 				accountName = folder.getOwner();
 			}
 		}
-        if (this._origMsg && this._origMsg.isDraft) {
+
+		var mainAcct = ac.getMainAccount().getEmail();
+		if (this._origMsg && this._origMsg.isDraft) {
             var from = this._origMsg.getAddresses(AjxEmailAddress.FROM).get(0);
 			// this means we're sending a draft msg obo so reset account name
 			if (from && from.address != mainAcct) {
 				accountName = from.address;
 			}
 		}
-        
+
         var from = soapDoc.set("e", null, parent);
 		from.setAttribute("t", "f");
 		from.setAttribute("a", accountName);
 
-		// the main account is *always* the sender
-		var sender = soapDoc.set("e", null, parent);
-		sender.setAttribute("t", "s");
-		sender.setAttribute("a", mainAcct);
+		if (!ac.isOffline) {
+			// the main account is *always* the sender
+			var sender = soapDoc.set("e", null, parent);
+			sender.setAttribute("t", "s");
+			sender.setAttribute("a", mainAcct);
+		}
 	}
 	else if (this.identity)
 	{
@@ -1289,7 +1292,7 @@ function(soapDoc, parent, isDraft, accountName) {
 
 		// bug fix #20630 - handling sending drafts obo
 		if (this._origMsg && this._origMsg.isDraft) {
-			var mainAcct = ac.getMainAccount(true).getEmail();
+			var mainAcct = ac.getMainAccount().getEmail();
 			var from = this._origMsg.getAddresses(AjxEmailAddress.FROM).get(0);
 			// this means we're sending a draft msg obo
 			if (from && from.address != mainAcct) {
