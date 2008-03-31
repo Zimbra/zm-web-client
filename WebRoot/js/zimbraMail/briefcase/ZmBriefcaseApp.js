f6644834a13461f20f24a79cd90b41993f6d6d1c
Unknown Perforce User <pshah>
2008-03-31 16:11:02 -0500

    Integrate change 90861 from main to FRANKLIN
    -------------
    bug: 26387 - offline: new calendar/address book doesn't comes up in Choose Folder dialog
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=26387
    
    Copied from Perforce
     Change: 90862

--- WebRoot/js/zimbraMail/briefcase/ZmBriefcaseApp.js
+++ WebRoot/js/zimbraMail/briefcase/ZmBriefcaseApp.js
@@ -1,17 +1,17 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- *
+ * 
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2007 Zimbra, Inc.
- *
+ * 
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- *
+ * 
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- *
+ * 
  * ***** END LICENSE BLOCK *****
  */
 
@@ -35,12 +35,11 @@ ZmApp.CLASS[ZmApp.BRIEFCASE]			= "ZmBriefcaseApp";
 ZmApp.SETTING[ZmApp.BRIEFCASE]			= ZmSetting.BRIEFCASE_ENABLED;
 ZmApp.LOAD_SORT[ZmApp.BRIEFCASE]		= 60;
 ZmApp.QS_ARG[ZmApp.BRIEFCASE]			= "briefcase";
-ZmApp.BUTTON_ID[ZmApp.BRIEFCASE]		= ZmId.BRIEFCASE_APP;
 
 ZmBriefcaseApp.prototype = new ZmApp;
 ZmBriefcaseApp.prototype.constructor = ZmBriefcaseApp;
 
-ZmBriefcaseApp.prototype.toString =
+ZmBriefcaseApp.prototype.toString = 
 function() {
 	return "ZmBriefcaseApp";
 }
@@ -57,18 +56,18 @@ ZmBriefcaseApp.prototype._defineAPI =
 function() {
 	AjxDispatcher.setPackageLoadFunction("BriefcaseCore", new AjxCallback(this, this._postLoadCore));
 	AjxDispatcher.setPackageLoadFunction("Briefcase", new AjxCallback(this, this._postLoad, ZmOrganizer.BRIEFCASE));
-	AjxDispatcher.registerMethod("GetBriefcaseController", ["BriefcaseCore", "Briefcase"], new AjxCallback(this, this.getBriefcaseController));
+	AjxDispatcher.registerMethod("GetBriefcaseController", ["BriefcaseCore", "Briefcase"], new AjxCallback(this, this.getBriefcaseController));	
 };
 
 ZmBriefcaseApp.prototype._registerOperations =
 function() {
 	ZmOperation.registerOp("NEW_BRIEFCASEITEM", {textKey:"newBriefcase", image:"NewFolder"});
 	ZmOperation.registerOp("NEW_FILE", {textKey:"uploadNewFile", tooltipKey:"newFile", image:"NewPage"});
-	ZmOperation.registerOp("SHARE_BRIEFCASE", {textKey:"shareFolder", image:"SharedMailFolder"}, ZmSetting.SHARING_ENABLED);
+	ZmOperation.registerOp("SHARE_BRIEFCASE", {textKey:"shareFolder", image:"Folder"}, ZmSetting.SHARING_ENABLED);
 	ZmOperation.registerOp("MOUNT_BRIEFCASE", {textKey:"mountBriefcase", image:"Notebook"}, ZmSetting.SHARING_ENABLED);
 	ZmOperation.registerOp("OPEN_FILE", {textKey:"openFile", tooltipKey:"openFileTooltip", image:"NewPage"});
-	ZmOperation.registerOp("VIEW_FILE_AS_HTML", {textKey:"viewAsHtml", tooltipKey:"viewAsHtml", image:"HtmlDoc"});
-	ZmOperation.registerOp("SEND_FILE", {textKey:"send", tooltipKey:"sendPageTT", image:"Send"});
+	ZmOperation.registerOp("VIEW_FILE_AS_HTML", {textKey:"viewAsHtml", tooltipKey:"viewAsHtml", image:"HtmlDoc"});	
+	ZmOperation.registerOp("SEND_FILE", {textKey:"send", tooltipKey:"sendPageTT", image:"Send"});	
 };
 
 ZmBriefcaseApp.prototype._registerItems =
@@ -95,26 +94,27 @@ function() {
 
 ZmBriefcaseApp.prototype._registerOrganizers =
 function() {
+	
+	
 	ZmOrganizer.registerOrg(ZmOrganizer.BRIEFCASE,
-                                { app            : ZmApp.BRIEFCASE,
-                                  nameKey        : "folders",
-                                  defaultFolder  : ZmOrganizer.ID_BRIEFCASE,
-                                  soapCmd        : "FolderAction",
-                                  firstUserId    : 256,
-                                  orgClass       : "ZmBriefcase",
-                                  orgPackage     : "BriefcaseCore",
-                                  treeController : "ZmBriefcaseTreeController",
-                                  labelKey       : "folders",
-                                  itemsKey       : "folders",
-                                  treeType       : ZmOrganizer.FOLDER,
-                                  views          : ["document"],
-                                  folderKey      : "briefcase",
-                                  mountKey       : "mountFolder",
-                                  createFunc     : "ZmOrganizer.create",
-                                  compareFunc    : "ZmBriefcase.sortCompare",
-                                  deferrable     : true,
-                                  hasColor       : true
-                                });
+							{app:				ZmApp.BRIEFCASE,
+							 nameKey:			"folders",
+							 defaultFolder:		ZmOrganizer.ID_BRIEFCASE,
+							 soapCmd:			"FolderAction",
+							 firstUserId:		256,
+							 orgClass:			"ZmBriefcase",
+							 orgPackage:		"BriefcaseCore",
+							 treeController:	"ZmBriefcaseTreeController",
+							 labelKey:			"folders",
+							 itemsKey:			"folders",
+							 treeType:			ZmOrganizer.FOLDER,
+							 views:				["document"],
+							 folderKey:			"briefcase",
+							 mountKey:			"mountFolder",
+							 createFunc:		"ZmOrganizer.create",
+							 compareFunc:		"ZmBriefcase.sortCompare",
+							 deferrable:		true
+							});
 };
 
 ZmBriefcaseApp.prototype._setupSearchToolbar =
@@ -170,81 +170,66 @@ ZmBriefcaseApp.prototype.deleteNotify =
 function(ids, force) {
 
 	if (!force && this._deferNotifications("delete", ids)) { return; }
-
-        var briefcaseController = AjxDispatcher.run("GetBriefcaseController");
-        for (var i = 0; i < ids.length; i++) {
-                // FIXME: sometimes ids[i] is null, which suggests a bug somewhere in ZmApp.js (?)
-                //        should investigate
-                var item = briefcaseController.getItemById(ids[i]);
-                if (item) {
-                        item.notifyDelete();
-                        briefcaseController.removeItem(item);
-                }
-        }
-
-/** <WTF ?!> **/
-
-// 	var nextData = null;
-// 	var idStr = ids.join(",")+",";
-// 	var folderInUse = false;
-// 	var briefcaseController = AjxDispatcher.run("GetBriefcaseController");
-// 	var shownFolder = briefcaseController._object;
-// 	var overviewController = appCtxt.getOverviewController();
-// 	var treeController = overviewController.getTreeController(ZmOrganizer.BRIEFCASE);
-// 	var treeView = treeController.getTreeView(this.getOverviewId());
-
-// 	if(!treeView){
-// 		return;
-// 	}
-
-// 	for (var i = 0; i < ids.length; i++) {
-// 			var tmp = treeView.getNextData(ids[i]);
-// 			//next node might also be in the delete list : parent deleted
-// 			if(tmp && idStr.indexOf(tmp.id+",")<0){
-// 				nextData = tmp;
-// 			}
-// 			if (shownFolder && shownFolder == ids[i]) {
-// 				folderInUse = true;
-// 			}
-// 	}
-
-// 	for (var i = 0; i < ids.length; i++) {
-
-// 		briefcaseController.removeItem({id:ids[i]});
-// 		appCtxt.cacheRemove(ids[i]);
-// 	}
-
-// 	if(nextData && folderInUse){
-// 	briefcaseController.show(nextData.id);
-// 	}else{
-// 	//handled in delete callback : currently we dont get notification
-// 	//for the op in remote folders, so handled differently
-// 	//briefcaseController.show(shownFolder);
-// 	}
-
-// 	for (var i = 0; i < ids.length; i++) {
-// 		var tmp1 = treeView.getTreeItemById(ids[i]);
-// 		if(tmp1){
-// 			tmp1.dispose();
-// 		}
-// 		ids[i] = null;
-// 	}
-
-/** </WTF ?!> **/
-
+	
+	var nextData = null;
+	var idStr = ids.join(",")+",";
+	var folderInUse = false;
+	var briefcaseController = AjxDispatcher.run("GetBriefcaseController");
+	var shownFolder = briefcaseController._object;
+	var overviewController = appCtxt.getOverviewController();
+	var treeController = overviewController.getTreeController(ZmOrganizer.BRIEFCASE);
+	var treeView = treeController.getTreeView(this.getOverviewId());
+	
+	if(!treeView){
+		return;
+	}
+	
+	for (var i = 0; i < ids.length; i++) {
+			var tmp = treeView.getNextData(ids[i]);
+			//next node might also be in the delete list : parent deleted
+			if(tmp && idStr.indexOf(tmp.id+",")<0){
+				nextData = tmp;
+			}
+			if (shownFolder && shownFolder == ids[i]) {
+				folderInUse = true;				
+			}
+	}	
+				
+	for (var i = 0; i < ids.length; i++) {
+	
+		briefcaseController.removeItem({id:ids[i]});
+		appCtxt.cacheRemove(ids[i]);
+	}
+	
+	if(nextData && folderInUse){
+	briefcaseController.show(nextData.id);
+	}else{
+	//handled in delete callback : currently we dont get notification
+	//for the op in remote folders, so handled differently
+	//briefcaseController.show(shownFolder);
+	}
+		
+	for (var i = 0; i < ids.length; i++) {
+		var tmp1 = treeView.getTreeItemById(ids[i]);
+		if(tmp1){
+			tmp1.dispose();
+		}
+		ids[i] = null;			
+	}
+	
 };
 
 /**
  * Checks for the creation of a notebook or a mount point to one, or of a page
  * or document.
- *
+ * 
  * @param creates	[hash]		hash of create notifications
  */
 ZmBriefcaseApp.prototype.createNotify =
 function(creates, force) {
 
 	if (!creates["folder"] && !creates["doc"] && !creates["link"]) { return; }
-
+	
 	if (!force && !this._noDefer && this._deferNotifications("create", creates)) { return; }
 
 	var bcController = AjxDispatcher.run("GetBriefcaseController");
@@ -253,21 +238,21 @@ function(creates, force) {
 		var list = creates[name];
 		for (var i = 0; i < list.length; i++) {
 			var create = list[i];
-			if (appCtxt.cacheGet(create.id)) { continue; }
-			if (name == "folder") {
-				this._handleCreateFolder(create, ZmOrganizer.BRIEFCASE);
+			if (appCtxt.cacheGet(create.id)) { continue; }	
+			if (name == "folder") {				
+				this._handleCreateFolder(create, ZmOrganizer.BRIEFCASE);		
 			}else if (name == "link") {
 				this._handleCreateLink(create, ZmOrganizer.BRIEFCASE);
-			}else if (name == "doc") {
+			}else if (name == "doc") {				
 				//DBG.println(AjxDebug.DBG1, "ZmBriefcaseApp: handling CREATE for node: " + name);
-				// REVISIT: use app context item cache
+				// REVISIT: use app context item cache				
 				//var doc = new ZmBriefcaseItem();
 				//doc.set(create);
 				//bcController.putItem(doc);
 			}
 		}
 	}
-
+	
 	//bcController.refreshFolder();
 };
 
@@ -286,7 +271,7 @@ function(modifies, force) {
 			var mod = list[i];
 			var id = mod.id;
 			if (!id) { continue; }
-
+	
 			 if (name == "doc") {
 				DBG.println(AjxDebug.DBG2, "ZmBriefcaseApp: handling modified notif for ID " + id + ", node type = " + name);
 				// REVISIT: Use app context item cache
@@ -326,10 +311,10 @@ ZmBriefcaseApp.prototype._handleNewItem =
 function() {
 	var briefcaseController = this.getBriefcaseController();
 	var callback  =new AjxCallback(this,this._handleUploadNewItem);
-	briefcaseController.__popupUploadDialog(callback,ZmMsg.uploadFileToBriefcase);
+	briefcaseController.__popupUploadDialog(callback,ZmMsg.uploadFileToBriefcase);	
 };
 
-ZmBriefcaseApp.prototype._handleUploadNewItem =
+ZmBriefcaseApp.prototype._handleUploadNewItem = 
 function(folder,filenames) {
 	var briefcaseController = this.getBriefcaseController();
 	briefcaseController.removeCachedFolderItems();
@@ -372,10 +357,11 @@ function(results, callback) {
 
 ZmBriefcaseApp.prototype._handleLoadShowSearchResults =
 function(results, callback) {
+
 //	this.getFileController().show(results, true);
-// 	if (callback) {
-// 		callback.run();
-// 	}
+	if (callback) {
+		callback.run();
+	}
 };
 
 ZmBriefcaseApp.prototype.setActive =
@@ -407,7 +393,7 @@ function(parent, name, color) {
 
 ZmBriefcaseApp.prototype.getBriefcaseController = function() {
 	if (!this._briefcaseController) {
-		this._briefcaseController = new ZmBriefcaseController(this._container, this);
+		this._briefcaseController = new ZmBriefcaseController(this._container, this);		
 	}
 	return this._briefcaseController;
 };
@@ -452,7 +438,7 @@ function(msgId, partId, name, folderId,items) {
 
 	var bController = this.getBriefcaseController();
 	if( bController.isReadOnly(folderId) ){
-		ZmOrganizer._showErrorMsg(ZmMsg.errorPermission);
+		ZmOrganizer._showErrorMsg(ZmMsg.errorPermission);	
 		return;
 	}else if(bController.isShared(folderId)) {
 		ZmOrganizer._showErrorMsg(ZmMsg.sharedFolderNotSupported);
@@ -461,14 +447,14 @@ function(msgId, partId, name, folderId,items) {
 
 	var itemFound = false;
 	for(var i in items){
-		var item = items[i];
+		var item = items[i];		
 		if(item.name == name){
 			itemFound = true;
 			break;
 		}
 	}
-	if(!itemFound){
-		var srcData = new ZmBriefcaseItem();
+	if(!itemFound){	
+		var srcData = new ZmBriefcaseItem();	
 		srcData.createFromAttachment(msgId, partId, name, folderId);
 	}else{
 		var	msg = AjxMessageFormat.format(ZmMsg.errorFileAlreadyExists, name);
@@ -476,14 +462,14 @@ function(msgId, partId, name, folderId,items) {
 	}
 };
 
-ZmBriefcaseApp.prototype.fixCrossDomainReference =
+ZmBriefcaseApp.prototype.fixCrossDomainReference = 
 function(url, restUrlAuthority) {
 	var refURL = window.location.protocol+"//"+window.location.host;
 	var urlParts = AjxStringUtil.parseURL(url);
 	if(urlParts.authority!=window.location.host){
 		var oldRef = urlParts.protocol +"://"+ urlParts.authority;
 		if((restUrlAuthority && url.indexOf(restUrlAuthority) >=0) || !restUrlAuthority){
-			url = url.replace(oldRef,refURL);
+			url = url.replace(oldRef,refURL);			
 		}
 	}
 	return url;
--- WebRoot/js/zimbraMail/core/ZmId.js
+++ WebRoot/js/zimbraMail/core/ZmId.js
@@ -38,10 +38,35 @@
  * 		zm		a menu
  * 		zmi		a menu item
  * 		zs		a select
+ * 		zov		an overview
+ * 		zt		a tree
+ * 		zti		a tree item
+ * 		ztih	a tree item header
  *
  * The skin defines its own IDs starting with "skin_", which we provide constants for here.
+ * 
+ * @author Conrad Damon
  */
 ZmId = function() {}
+
+// constants used in creating IDs
+
+// apps
+ZmId.APP_BRIEFCASE		= "Briefcase";
+ZmId.APP_CALENDAR		= "Calendar";
+ZmId.APP_CONTACTS		= "Contacts";
+ZmId.APP_IM				= "IM";
+ZmId.APP_MAIL			= "Mail";
+ZmId.APP_MIXED			= "Mixed";
+ZmId.APP_NOTEBOOK		= "Notebook";
+ZmId.APP_PORTAL			= "Portal";
+ZmId.APP_PREFERENCES	= "Options";
+ZmId.APP_TASKS			= "Tasks";
+ZmId.APP_VOICE			= "Voice";
+
+// toolbars
+ZmId.TB_INV	= "INV";
+ZmId.TB_NAV	= "NAV";
 
 // containers defined by the skin
 ZmId.SKIN_APP_BOTTOM_TOOLBAR		= "skin_container_app_bottom_toolbar";
@@ -102,119 +127,154 @@ ZmId.SEARCH_MENU_PAGES		= "zmi_searchPages";	// search menu item for notebook pa
 ZmId.SEARCH_MENU_TASKS		= "zmi_searchTasks";	// search menu item for tasks
 
 // app buttons in the app chooser toolbar
-ZmId.CONTACTS_APP		= "zb_contacts";
-ZmId.BRIEFCASE_APP		= "zb_briefcase";
-ZmId.CALENDAR_APP		= "zb_calendar";
-ZmId.IM_APP				= "zb_im";
-ZmId.MAIL_APP			= "zb_mail";
-ZmId.NOTEBOOK_APP		= "zb_notebook";
-ZmId.PORTAL_APP			= "zb_portal";
-ZmId.PREFERENCES_APP	= "zb_options";
-ZmId.TASKS_APP			= "zb_tasks";
-ZmId.VOICE_APP			= "zb_voice";
+
+/**
+ * Generates the ID for a button in the app chooser.
+ * 
+ * Example: zb_mail
+ * 
+ * @param app	[const]		app
+ */
+ZmId.getAppChooserButtonId =
+function(app) {
+	return ["zb_", app.toLowerCase()].join("");
+};
 
 // content area views and their components
-//
-// Note that there is a lot of repetition here, since each view has its own toolbar, even
-// though it has the same buttons as other toolbars. See bug 26421. If that bug gets fixed
-// (it's not certain at this point that it should be), then there would be only one set of
-// IDs for the buttons on a mail toolbar (for example, there would be one ID for MAIL_NEW_MENU_BUTTON
-// instead of the 4 versions we currently have).
+
+/**
+ * Generates the ID for a button in a toolbar. Intended for use with the top toolbar, nav toolbar,
+ * and invite toolbar.
+ * 
+ * Examples: zb_clvCheckMail zb_tvReply zb_composeSend zb_clvNavPageForward zb_clvInvReplyAccept
+ * 
+ * @param tbId		[string]	string that uniquely identifies toolbar (eg ID of owning view)
+ * @param op		[const]		the button operation
+ * @param tbType	[const]*	type of toolbar (eg invite or nav)
+ */
+ZmId.getToolbarButtonId =
+function(tbId, op, tbType) {
+	tbId = tbType ? [tbId, tbType].join("_") : tbId;
+	return ["zb_", AjxStringUtil.toMixed(tbId, "_", true), AjxStringUtil.toMixed(op, "_")].join("");
+};
 
 // conv list view (CLV)
 ZmId.CLV_LIST				= "z_clvListView";		// conv list view
 ZmId.CLV_MSG				= "z_clvMsgView";		// msg view within conv list view
 ZmId.CLV_TOOLBAR			= "ztb_clv";
-ZmId.CLV_NEW_MENU_BUTTON	= "zb_clvNewMenu";
-ZmId.CLV_CHECK_MAIL_BUTTON	= "zb_clvCheckMail";
-ZmId.CLV_DELETE_BUTTON		= "zb_clvDelete";
-ZmId.CLV_MOVE_BUTTON		= "zb_clvMove";
-ZmId.CLV_PRINT_BUTTON		= "zb_clvPrint";
-ZmId.CLV_REPLY_BUTTON		= "zb_clvReply";
-ZmId.CLV_REPLY_ALL_BUTTON	= "zb_clvReplyAll";
-ZmId.CLV_FORWARD_BUTTON		= "zb_clvForward";
-ZmId.CLV_EDIT_BUTTON		= "zb_clvEdit";
-ZmId.CLV_SPAM_BUTTON		= "zb_clvSpam";
-ZmId.CLV_TAG_MENU_BUTTON	= "zb_clvTag";
-ZmId.CLV_DETACH_BUTTON		= "zb_clvDetach";
-ZmId.CLV_VIEW_MENU_BUTTON	= "zb_clvView";
-
-ZmId.CLV_INV_REPLY_ACCEPT_BUTTON	= "zb_clvInvAccept";
-ZmId.CLV_INV_REPLY_TENTATIVE_BUTTON	= "zb_clvInvTentative";
-ZmId.CLV_INV_REPLY_DECLINE_BUTTON	= "zb_clvInvDecline";
 
 // traditional (msg) list view (TV)
 ZmId.TV_LIST				= "z_tvListView";		// msg list view
 ZmId.TV_MSG					= "z_tvMsgView";		// msg view within msg list view
 ZmId.TV_TOOLBAR				= "ztb_tv";
-ZmId.TV_NEW_MENU_BUTTON		= "zb_tvNewMenu";
-ZmId.TV_CHECK_MAIL_BUTTON	= "zb_tvCheckMail";
-ZmId.TV_DELETE_BUTTON		= "zb_tvDelete";
-ZmId.TV_MOVE_BUTTON			= "zb_tvMove";
-ZmId.TV_PRINT_BUTTON		= "zb_tvPrint";
-ZmId.TV_REPLY_BUTTON		= "zb_tvReply";
-ZmId.TV_REPLY_ALL_BUTTON	= "zb_tvReplyAll";
-ZmId.TV_FORWARD_BUTTON		= "zb_tvForward";
-ZmId.TV_EDIT_BUTTON			= "zb_tvEdit";
-ZmId.TV_SPAM_BUTTON			= "zb_tvSpam";
-ZmId.TV_TAG_MENU_BUTTON		= "zb_tvTag";
-ZmId.TV_DETACH_BUTTON		= "zb_tvDetach";
-ZmId.TV_VIEW_MENU_BUTTON	= "zb_tvView";
-
-ZmId.TV_INV_REPLY_ACCEPT_BUTTON		= "zb_tvInvAccept";
-ZmId.TV_INV_REPLY_TENTATIVE_BUTTON	= "zb_tvInvTentative";
-ZmId.TV_INV_REPLY_DECLINE_BUTTON	= "zb_tvInvDecline";
 
 // single conv view (CV)
 ZmId.CV_LIST				= "z_cvListView";		// msg list within conv view
 ZmId.CV_MSG					= "z_cvMsgView";		// msg view within conv view
 ZmId.CV_TOOLBAR				= "ztb_cv";
-ZmId.CV_NEW_MENU_BUTTON		= "zb_cvNewMenu";
-ZmId.CV_CHECK_MAIL_BUTTON	= "zb_cvCheckMail";
-ZmId.CV_DELETE_BUTTON		= "zb_cvDelete";
-ZmId.CV_MOVE_BUTTON			= "zb_cvMove";
-ZmId.CV_PRINT_BUTTON		= "zb_cvPrint";
-ZmId.CV_REPLY_BUTTON		= "zb_cvReply";
-ZmId.CV_REPLY_ALL_BUTTON	= "zb_cvReplyAll";
-ZmId.CV_FORWARD_BUTTON		= "zb_cvForward";
-ZmId.CV_EDIT_BUTTON			= "zb_cvEdit";
-ZmId.CV_SPAM_BUTTON			= "zb_cvSpam";
-ZmId.CV_TAG_MENU_BUTTON		= "zb_cvTag";
-ZmId.CV_DETACH_BUTTON		= "zb_cvDetach";
-ZmId.CV_VIEW_MENU_BUTTON	= "zb_cvView";
 
-ZmId.CV_INV_REPLY_ACCEPT_BUTTON		= "zb_cvInvAccept";
-ZmId.CV_INV_REPLY_TENTATIVE_BUTTON	= "zb_cvInvTentative";
-ZmId.CV_INV_REPLY_DECLINE_BUTTON	= "zb_cvInvDecline";
-
+// msg view (MV)
 ZmId.MSG_TOOLBAR			= "ztb_msg";
-ZmId.MSG_NEW_MENU_BUTTON	= "zb_msgNewMenu";
-ZmId.MSG_CHECK_MAIL_BUTTON	= "zb_msgCheckMail";
-ZmId.MSG_DELETE_BUTTON		= "zb_msgDelete";
-ZmId.MSG_MOVE_BUTTON		= "zb_msgMove";
-ZmId.MSG_PRINT_BUTTON		= "zb_msgPrint";
-ZmId.MSG_REPLY_BUTTON		= "zb_msgReply";
-ZmId.MSG_REPLY_ALL_BUTTON	= "zb_msgReplyAll";
-ZmId.MSG_FORWARD_BUTTON		= "zb_msgForward";
-ZmId.MSG_EDIT_BUTTON		= "zb_msgEdit";
-ZmId.MSG_SPAM_BUTTON		= "zb_msgSpam";
-ZmId.MSG_TAG_MENU_BUTTON	= "zb_msgTag";
-ZmId.MSG_DETACH_BUTTON		= "zb_msgDetach";
-ZmId.MSG_VIEW_MENU_BUTTON	= "zb_msgView";
-
-ZmId.MSG_INV_REPLY_ACCEPT_BUTTON	= "zb_msgInvAccept";
-ZmId.MSG_INV_REPLY_TENTATIVE_BUTTON	= "zb_msgInvTentative";
-ZmId.MSG_INV_REPLY_DECLINE_BUTTON	= "zb_msgInvDecline";
-
-ZmId.COMPOSE_SEND_BUTTON			= "zb_compSend";
-ZmId.COMPOSE_CANCEL_BUTTON			= "zb_compCancel";
-ZmId.COMPOSE_SAVE_DRAFT_BUTTON		= "zb_compSave";
-ZmId.COMPOSE_ATTACHMENT_BUTTON		= "zb_compAtt";
-ZmId.COMPOSE_SPELL_CHECK_BUTTON		= "zb_compSpell";
-ZmId.COMPOSE_ADD_SIGNATURE_BUTTON	= "zb_compAddSig";
-ZmId.COMPOSE_COMPOSE_OPTIONS_BUTTON	= "zb_compOptions";
-ZmId.COMPOSE_DETACH_COMPOSE_BUTTON	= "zb_compDetach";
 
 ZmId.PREF_SAVE_BUTTON				= "zb_prefSave";
 ZmId.PREF_CANCEL_BUTTON				= "zb_prefCancel";
+
+// overviews, trees, and tree items
+
+// main overviews
+ZmId.OVERVIEW_MAIL					= "zov_mail";
+ZmId.OVERVIEW_CONTACTS				= "zov_contacts";
+ZmId.OVERVIEW_CALENDAR				= "zov_calendar";
+ZmId.OVERVIEW_NOTEBOOK				= "zov_notebook";
+ZmId.OVERVIEW_TASKS					= "zov_tasks";
+ZmId.OVERVIEW_BRIEFCASE				= "zov_briefcase";
+
+// trees within the main overviews
+ZmId.TREE_MAIL_FOLDER			= "zt_mailFolder";
+ZmId.TREE_MAIL_SEARCH			= "zt_mailSearch";
+ZmId.TREE_MAIL_TAG				= "zt_mailTag";
+ZmId.TREE_MAIL_ZIMLET			= "zt_mailZimlet";
+
+ZmId.TREE_CONTACTS_ADDRBOOK		= "zt_contactsAddrbook";
+ZmId.TREE_CONTACTS_SEARCH		= "zt_contactsSearch";
+ZmId.TREE_CONTACTS_TAG			= "zt_contactsTag";
+ZmId.TREE_CONTACTS_ZIMLET		= "zt_contactsZimlet";
+
+ZmId.TREE_CALENDAR_CALENDAR		= "zt_calendarCalendar";
+ZmId.TREE_CALENDAR_SEARCH		= "zt_calendarSearch";
+ZmId.TREE_CALENDAR_TAG			= "zt_calendarTag";
+ZmId.TREE_CALENDAR_ZIMLET		= "zt_calendarZimlet";
+
+ZmId.TREE_NOTEBOOK_NOTEBOOK		= "zt_notebookNotebook";
+ZmId.TREE_NOTEBOOK_TAG			= "zt_notebookTag";
+ZmId.TREE_NOTEBOOK_ZIMLET		= "zt_notebookZimlet";
+
+ZmId.TREE_BRIEFCASE_BRIEFCASE	= "zt_briefcaseBriefcase";
+ZmId.TREE_BRIEFCASE_TAG			= "zt_briefcaseTag";
+ZmId.TREE_BRIEFCASE_ZIMLET		= "zt_briefcaseZimlet";
+
+ZmId.TREE_TASKS_TASKS			= "zt_tasksTasks";
+ZmId.TREE_TASKS_SEARCH			= "zt_tasksSearch";
+ZmId.TREE_TASKS_TAG				= "zt_tasksTag";
+ZmId.TREE_TASKS_ZIMLET			= "zt_tasksZimlet";
+
+/**
+ * Returns a tree item ID based on the underlying organizer and the overview ID (since the same
+ * organizer may be represented as tree items in more than one overview). Some sample IDs:
+ * 
+ * 		zti_mail_2				Inbox
+ * 		zti_mail_172			user-created item in mail overview
+ * 		zti_contacts_7			system Contacts folder
+ * 		zti_calendar_304		user-created item in calendar overview
+ * 		ztih_mail_folder		Folders header in mail overview
+ * 
+ * Constants for some system folder tree items have been provided as a convenience.
+ * 
+ * @param overviewId	[string]		unique ID for overview
+ * @param organizerId	[ZmOrganizer]	ID of the data object backing tree item
+ * @param type			[const]			organizer type (for headers only)
+ */
+ZmId.getTreeItemId =
+function(overviewId, organizerId, type) {
+	if (!organizerId && !type) { return; }
+	if (type) {
+		return ["ztih_", overviewId.toLowerCase(), "_", type.toLowerCase()].join("");
+	} else {
+		if (organizerId < 64) {
+			var idKey = ["TREEITEM", overviewId, ZmFolder.QUERY_NAME[organizerId]].join("_").toUpperCase();
+			if (ZmId[idKey]) {
+				return ZmId[idKey];
+			}
+		}
+		return ["zti_", overviewId.toLowerCase(), "_", organizerId].join("");
+	}
+};
+
+ZmId._OV_MAIL = "Mail";
+
+// system folder tree items
+
+ZmId.TREEITEM_MAIL_INBOX			= "zti_mail_2";
+ZmId.TREEITEM_MAIL_TRASH			= "zti_mail_3";
+ZmId.TREEITEM_MAIL_JUNK				= "zti_mail_4";
+ZmId.TREEITEM_MAIL_SENT				= "zti_mail_5";
+ZmId.TREEITEM_MAIL_DRAFTS			= "zti_mail_6";
+
+ZmId.TREEITEM_CONTACTS_CONTACTS		= "zti_contacts_7";
+
+ZmId.TREEITEM_CALENDAR_CALENDAR		= "zti_calendar_10";
+
+ZmId.TREEITEM_NOTEBOOK_NOTEBOOK		= "zti_notebook_12";
+
+ZmId.TREEITEM_TASKS_TASKS			= "zti_tasks_15";
+
+ZmId.TREEITEM_BRIEFCASE_BRIEFCASE	= "zti_briefcase_16";
+
+
+// constants:
+
+// App
+// View
+// Op
+// Overview
 
