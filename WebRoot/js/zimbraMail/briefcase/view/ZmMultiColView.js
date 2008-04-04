5af35430e05a9b321b89af6935000ff60f981261
Conrad Damon <cdamon@zimbra.com>
2008-04-04 18:35:39 -0500

    Integrate change 91711 from FRANKLIN to main
    -------------
    Bug: 22745 - Require signature names to be unique
    Handle errors when modifying signature.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=22745
    
    Copied from Perforce
     Change: 91712

--- WebRoot/js/zimbraMail/briefcase/view/ZmMultiColView.js
+++ WebRoot/js/zimbraMail/briefcase/view/ZmMultiColView.js
@@ -1,17 +1,17 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2007 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */
 
@@ -32,7 +32,7 @@ ZmMultiColView = 	function(parent, className, posStyle, controller, dropTgt) {
 	this._currentListView = null;
 	this._noOfCol = 0;
 	this._cellCache = [];
-	
+
 	var folderTree = appCtxt.getFolderTree();
 	if (folderTree) {
 		folderTree.addChangeListener(new AjxListener(this, this._fileTreeListener));
@@ -40,7 +40,7 @@ ZmMultiColView = 	function(parent, className, posStyle, controller, dropTgt) {
 
 	this._initialize(controller, dropTgt);
 
-	//override the default style set on DwtControl module	
+	//override the default style set on DwtControl module
 	var el = this.getHtmlElement();
 	el.style.overflow = "";
 }
@@ -88,7 +88,7 @@ function(ev, treeView) {
 
 ZmMultiColView.prototype._setHeaderInfo =
 function(clear) {
-	
+
 };
 
 
@@ -98,20 +98,20 @@ function(controller, dropTgt) {
 	this.setCurrentListView(this._listPart[0]);
 };
 
-ZmMultiColView.prototype.getListView = 
+ZmMultiColView.prototype.getListView =
 function(idx) {
 	return (idx >=0 )? this._listPart[idx] : this._listPart[0];
 };
 
 
-ZmMultiColView.prototype.addColumn = 
+ZmMultiColView.prototype.addColumn =
 function(dropTgt) {
 
 	var idx = this._colIndex++;
 	this._colIds[idx] = Dwt.getNextId();
-	
+
 	var el = this.getHtmlElement();
-	
+
 	if(!this._table){
 		this._tableId = Dwt.getNextId();
 		el.innerHTML = ['<table cellpadding=0 cellspacing=0 id="',this._tableId,'"><tbody><tr></tr></tbody></table>'].join("");
@@ -119,18 +119,18 @@ function(dropTgt) {
 		var tbody = this._table.tBodies[0];
 		this._row = tbody.rows[0];
 	}
-	
+
 	var div = this.getColumnDiv();
-	div.id = this._colIds[idx];	
-	
+	div.id = this._colIds[idx];
+
 	this._colDivs[idx] = div;
 	this._listPart[idx] = new ZmColListView(this, this._controller, dropTgt, idx);
 	this._listPart[idx].reparentHtmlElement(this._colIds[idx]);
-	
+
 	return this._listPart[idx];
 };
 
-ZmMultiColView.prototype.removeColumn = 
+ZmMultiColView.prototype.removeColumn =
 function(idx) {
 	var div = this._colDivs[idx];
 	if(div && div.parentNode) {
@@ -138,11 +138,11 @@ function(idx) {
 
 		var cell = div.parentNode;
 		cell.removeChild(div);
-		
+
 		if(this._row){
 			this._row.deleteCell(cell.cellIndex);
-		}		
-		
+		}
+
 		if(this._listPart[idx] && this._listPart[idx] == this._currentListView) {
 			this._currentListView = this._listPart[idx].getPreviousColumn();
 		}
@@ -153,24 +153,24 @@ function(idx) {
 };
 
 //todo: proper deletion logic
-ZmMultiColView.prototype.removeChildColumns = 
-function(idx) {	
+ZmMultiColView.prototype.removeChildColumns =
+function(idx) {
 	for(var i = idx+1;i<this._listPart.length;i++){
 		this.removeColumn(i);
-	}	
+	}
 	this.clearFolderProps();
 };
 
 ZmMultiColView.prototype.updateColumn =
 function(colView,folderId) {
-	
+
 	if(colView != this._currentListView) return;
-	
-	for(var i=0;i<this._colIndex;i++){	
+
+	for(var i=0;i<this._colIndex;i++){
 		if(this._listPart[i]!=colView){
 			this._listPart[i].set(folderId);
-		}		
-	}	
+		}
+	}
 };
 
 ZmMultiColView.prototype.setCurrentListView =
@@ -209,14 +209,14 @@ function(folderId) {
 	var listView = null;
 	if(len == 0){
 		this._currentListView = listView = this.addColumn();
-				
+
 	}else{
 		if(!this._controller.isRefreshing()){
 			this._currentListView  = listView = this._listPart[0];
-			this.removeChildColumns(0);	
+			this.removeChildColumns(0);
 		}else{
 			listView = this._currentListView;
-			this.removeChildColumns(listView.getColumnIndex());			
+			this.removeChildColumns(listView.getColumnIndex());
 		}
 	}
 	this._controller._listView[ZmController.BRIEFCASE_COLUMN_VIEW] = listView;
@@ -229,11 +229,11 @@ function(item) {
 	this.clearFolderProps();
 
 	var el = this.getHtmlElement();
-	
+
 	var div = this.getColumnDiv();
 	div.className = "ZmColListDiv  ZmColListDivPad";
-	
-	var contentType = item.contentType;	
+
+	var contentType = item.contentType;
 	if(contentType && contentType.match(/;/)) {
 			contentType = contentType.split(";")[0];
 	}
@@ -247,25 +247,25 @@ function(item) {
 	if(name.length > 20){
 		name = name.substring(0,20) + "..";
 	}
-	
+
 	var restUrl = item.getRestUrl();
 	restUrl = this._controller.getApp().fixCrossDomainReference(restUrl);
-	var fileLink = ["<a ","href='",restUrl,"' target='_blank'>",name,"</a>"].join("");
+	var fileLink = [ '<a href="', restUrl, '" target="_blank">', name, '</a>' ].join("");
 
 	var dateFormatter = AjxDateFormat.getDateTimeInstance(AjxDateFormat.FULL, AjxDateFormat.MEDIUM);
-	
+
 	var prop = [
 		{name:ZmMsg.name, value:fileLink},
 		{name:ZmMsg.path, value:path},
 		{name:ZmMsg.size, value:AjxUtil.formatSize(item.size)},
 		{name:ZmMsg.created, value:dateFormatter.format(item.createDate)},
-		{name:ZmMsg.creator, value:item.creator},		
+		{name:ZmMsg.creator, value:item.creator},
 		{name:ZmMsg.modified, value:dateFormatter.format(item.modifyDate)},
-		{name:ZmMsg.modifier, value:item.modifier}									
+		{name:ZmMsg.modifier, value:item.modifier}
 	];
-	
+
 	var imgSrc = restUrl.toLowerCase().match(/\.jpg$|\.gif$|\.jpeg$|\.bmp$$/) ? restUrl : null;
-	
+
 	var subs = {
 		imgSrc: imgSrc,
 		icon: icon,
@@ -276,18 +276,18 @@ function(item) {
 
     div.innerHTML = AjxTemplate.expand("briefcase.Briefcase#file_properties", subs);
 
-	this._folderPropsDiv = div;	
-	
+	this._folderPropsDiv = div;
+
 	this._controller.updateCurrentFolder(item.folderId);
 	if(this._currentListView) {
 		this._currentListView.setNextColumn(null);
 	}
-	
+
 	this._sizeChildren();
 	this.scrollToEnd();
 };
 
-ZmMultiColView.prototype.clearFolderProps = 
+ZmMultiColView.prototype.clearFolderProps =
 function() {
 	//clear the listener : if added
 	if(this._folderPropsDiv){
@@ -297,7 +297,7 @@ function() {
 	}
 };
 
-ZmMultiColView.prototype.getColumn = 
+ZmMultiColView.prototype.getColumn =
 function(folderId){
 	for(var i in this._listPart){
 		var listView = this._listPart[i];
@@ -307,7 +307,7 @@ function(folderId){
 	}
 };
 
-ZmMultiColView.prototype.setBounds = 
+ZmMultiColView.prototype.setBounds =
 function(x, y, width, height) {
 	DwtComposite.prototype.setBounds.call(this, x, y, width, height);
 	this._sizeChildren(width, height);
@@ -316,7 +316,7 @@ function(x, y, width, height) {
 
 ZmMultiColView.prototype._sizeChildren =
 function() {
-	var size = Dwt.getSize(this.getHtmlElement());	
+	var size = Dwt.getSize(this.getHtmlElement());
 	for(var i in this._colDivs){
 		Dwt.setSize(this._colDivs[i],Dwt.DEFAULT,size.y);
 	}
@@ -324,7 +324,7 @@ function() {
 		Dwt.setSize(this._cellCache[i],Dwt.DEFAULT,size.y);
 	}
 	if(this._folderPropsDiv){
-		Dwt.setSize(this._folderPropsDiv,Dwt.DEFAULT,size.y);	
+		Dwt.setSize(this._folderPropsDiv,Dwt.DEFAULT,size.y);
 	}
 };
 
@@ -339,18 +339,18 @@ ZmMultiColView.prototype.getColumnDiv =
 function() {
 
 	if(!this._row) return;
-	
+
 	var size = Dwt.getSize(this.getHtmlElement());
 	var limit = Math.max(Math.floor(size.x/250),4);
-	
-	if(this._noOfCol < limit){		
+
+	if(this._noOfCol < limit){
 		var neededCols = limit-this._noOfCol;
 		for(var i=0;i<(neededCols);i++) {
 			var div = this.createDiv();
-			this._cellCache.push(div);			
-		}		
+			this._cellCache.push(div);
+		}
 		return this._cellCache.shift();
-		
+
 	}else if(this._cellCache.length > 0){
 		return this._cellCache.shift();
 	}else{
@@ -359,7 +359,7 @@ function() {
 	}
 };
 
-ZmMultiColView.prototype.createDiv = 
+ZmMultiColView.prototype.createDiv =
 function() {
 	if(!this._row) return;
 	var cell = this._row.insertCell(this._row.cells.length);
@@ -367,7 +367,7 @@ function() {
 	var div = document.createElement("div");
 	div.className = "ZmColListDiv";
 	cell.appendChild(div);
-	this._noOfCol++;	
+	this._noOfCol++;
 	return div;
 };
 
