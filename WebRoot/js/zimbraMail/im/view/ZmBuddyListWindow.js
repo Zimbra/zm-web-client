d3c83b285e5498a41c9f4847dead0796ad74a4e8
Unknown Perforce User <dcomfort>
2008-04-07 13:07:31 -0500

    Bug: 25142 Fix close button and refactor to have less stuff happening in static methods.
    http://bugzilla.zimbra.com/show_bug.cgi?id=25142
    
    Copied from Perforce
     Change: 91935

--- WebRoot/js/zimbraMail/im/view/ZmBuddyListWindow.js
+++ WebRoot/js/zimbraMail/im/view/ZmBuddyListWindow.js
@@ -15,8 +15,55 @@
  * ***** END LICENSE BLOCK *****
  */
 
-ZmBuddyListWindow = function(params) {
-	DwtResizableWindow.call(this, params);
+ZmBuddyListWindow = function(wm) {
+	DwtResizableWindow.call(this, wm);
+	var cont = new DwtComposite(this);
+
+	var toolbar = new DwtToolBar({parent:cont, handleMouse: false});
+
+	var lab = new DwtLabel({parent:toolbar, style:DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_LEFT,
+		className:"ZmChatWindowLabel"});
+	lab.setImage("ImGroup");
+	lab.setText(ZmMsg.buddyList);
+
+	toolbar.addFiller();
+
+	var close = new DwtToolBarButton({parent:toolbar});
+	close.setImage("Close");
+	close.addSelectionListener(new AjxListener(this, function() {
+		this.popdown();
+	}));
+
+	this.enableMoveWithElement(toolbar);
+
+	var list = new ZmImOverview(cont, { posStyle   : Dwt.STATIC_STYLE,
+		isFloating : true });
+
+	var toolbar2 = new DwtToolBar({parent:cont});
+
+	var newBuddy = new DwtToolBarButton({parent:toolbar2});
+	newBuddy.setImage("ImBuddy");
+	newBuddy.setToolTipContent(ZmMsg.newRosterItem);
+	newBuddy.addSelectionListener(ZmImApp.INSTANCE.getRosterTreeController()._listeners[ZmOperation.NEW_ROSTER_ITEM]);
+
+	toolbar2.addFiller();
+
+	cont.addControlListener(new AjxListener(null, function(ev) {
+		var s1 = { x: ev.oldWidth, y: ev.oldHeight };
+		var s2 = { x: ev.newWidth, y: ev.newHeight };
+		if (s1.x != s2.x || s1.y != s2.y) {
+			var h = s2.y - toolbar.getSize().y - toolbar2.getSize().y;
+			list.setSize(s2.x, h);
+		}
+	}));
+
+	this.setView(cont);
+	this.setSize(200, 600);
+	var wm_size = wm.getSize();
+	var win_size = this.getSize();
+	wm.manageWindow(this, { x: wm_size.x - win_size.x - 50,
+		y: (wm_size.y - win_size.y) / 2
+	});
 };
 
 ZmBuddyListWindow.prototype = new DwtResizableWindow;
@@ -29,58 +76,10 @@ function() {
 
 ZmBuddyListWindow.create =
 function() {
-	var win = ZmBuddyListWindow.instance;
-	if (!win) {
+	if (!ZmBuddyListWindow.instance) {
 		var wm = ZmChatMultiWindowView.getInstance().getShellWindowManager();
-		ZmBuddyListWindow.instance = win = new ZmBuddyListWindow(wm);
-		var cont = new DwtComposite(win);
-
-		var toolbar = new DwtToolBar({parent:cont, handleMouse: false});
-
-		var lab = new DwtLabel({parent:toolbar, style:DwtLabel.IMAGE_LEFT | DwtLabel.ALIGN_LEFT,
-			className:"ZmChatWindowLabel"});
-		lab.setImage("ImGroup");
-		lab.setText(ZmMsg.buddyList);
-
-		toolbar.addFiller();
-
-		var close = new DwtToolBarButton({parent:toolbar});
-		close.setImage("Close");
-		close.addSelectionListener(new AjxListener(null, function() {
-			win.popdown();
-		}));
-
-		win.enableMoveWithElement(toolbar);
-
-		var list = new ZmImOverview(cont, { posStyle   : Dwt.STATIC_STYLE,
-			isFloating : true });
-
-		var toolbar2 = new DwtToolBar({parent:cont});
-
-		var newBuddy = new DwtToolBarButton({parent:toolbar2});
-		newBuddy.setImage("ImBuddy");
-		newBuddy.setToolTipContent(ZmMsg.newRosterItem);
-		newBuddy.addSelectionListener(ZmImApp.INSTANCE.getRosterTreeController()._listeners[ZmOperation.NEW_ROSTER_ITEM]);
-
-		toolbar2.addFiller();
-
-		cont.addControlListener(new AjxListener(null, function(ev) {
-			var s1 = { x: ev.oldWidth, y: ev.oldHeight };
-			var s2 = { x: ev.newWidth, y: ev.newHeight };
-			if (s1.x != s2.x || s1.y != s2.y) {
-				var h = s2.y - toolbar.getSize().y - toolbar2.getSize().y;
-				list.setSize(s2.x, h);
-			}
-		}));
-
-		win.setView(cont);
-		win.setSize(200, 600);
-		var wm_size = wm.getSize();
-		var win_size = win.getSize();
-		wm.manageWindow(win, { x: wm_size.x - win_size.x - 50,
-			y: (wm_size.y - win_size.y) / 2
-		});
+		ZmBuddyListWindow.instance = new ZmBuddyListWindow(wm);
 	}
-	return win;
+	return ZmBuddyListWindow.instance;
 };
 
