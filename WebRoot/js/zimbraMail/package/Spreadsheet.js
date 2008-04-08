842d5a09874691d77838cfdeb6af914d934f60c7
Unknown Perforce User <ssugumaran>
2008-04-08 09:00:27 -0500

    bug: 25804 handled a case when getminicalrequest throws fault error - used 	callback to continue maintenanceAction cycle
    http://bugzilla.zimbra.com/show_bug.cgi?id=25804
    
    Copied from Perforce
     Change: 92135

--- WebRoot/js/zimbraMail/package/Spreadsheet.js
+++ WebRoot/js/zimbraMail/package/Spreadsheet.js
@@ -4,5 +4,3 @@ AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheet");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetModel");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetFormulae");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetToolbars");
-
-AjxPackage.require("zimbraMail.ale.spreadsheet.test");
