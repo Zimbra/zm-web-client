08d095a8627aa6f8855785aafc4e58b5de11a7c1
Unknown Perforce User <pshah>
2008-05-12 16:21:48 -0500

    Integrate change 97251 from main to FRANKLIN
    -------------
    bug: 16777 - multipart/digest message causes web client to 500 error
    --
    Hide multipart/digest parts as attachments.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=16777
    
    Copied from Perforce
     Change: 97253

--- WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
@@ -169,7 +169,7 @@ function() {
 	// bug fix #7192 - disable detach toolbar button
 	this._toolbar.enable(ZmOperation.DETACH_COMPOSE, false);
 
-	var msg = this._composeView._msg;
+	var msg = this._composeView.getOrigMsg();
 	var addrs = this._composeView.getRawAddrFields();
 	var subj = this._composeView._subjectField.value;
 	var forAttHtml = this._composeView._attcDiv.innerHTML;
@@ -177,7 +177,7 @@ function() {
 	var composeMode = this._composeView.getComposeMode();
 	var identityId = this._composeView.getIdentity().id;
 	var backupForm = this._composeView.backupForm;
-	var sendUID = this._composeView.sendUID;
+	var sendUID = this._composeView.getSendUID();
 	var action = this._composeView._action || this._action;
 
 	// this is how child window knows what to do once loading:
@@ -381,7 +381,8 @@ function(initHide, composeMode) {
 	    this._composeView.enableInputs(false);
 	}
 
-	this._composeView.identitySelect.addChangeListener(new AjxListener(this, this._identityChangeListener, [true]));
+	var identitySelect = this._composeView.getIdentitySelect();
+	identitySelect.addChangeListener(new AjxListener(this, this._identityChangeListener, [true]));
 };
 
 ZmComposeController.prototype._identityChangeListener =
@@ -390,7 +391,9 @@ function(setSignature, event) {
 	var resetBody = this._composeView.isDirty();
 
 	// don't do anything if signature is same
-	if (signatureId == this._currentSignatureId) { return; }
+	if (signatureId == this._currentSignatureId) {
+		return;
+	}
 
 	// apply settings
 	this._applyIdentityToBody(setSignature, resetBody);
@@ -411,8 +414,7 @@ function(setSignature,resetBody) {
 	this._setAddSignatureVisibility(identity);
 };
 
-ZmComposeController.prototype._handleSelectSignature =
-function(evt) {
+ZmComposeController.prototype._handleSelectSignature = function(evt) {
 	var signatureId = evt.item.getData(ZmComposeController.SIGNATURE_KEY);
 	this.setSelectedSignature(signatureId);
 
@@ -802,7 +804,7 @@ function(composeMode, identity) {
 	var isReply = (this._action == ZmOperation.REPLY || this._action == ZmOperation.REPLY_ALL);
 	var isForward = (this._action == ZmOperation.FORWARD_ATT || this._action == ZmOperation.FORWARD_INLINE);
 	if (identity && (isReply || isForward)) {
-		var includePref = isReply ? appCtxt.get(ZmSetting.REPLY_INCLUDE_ORIG) : appCtxt.get(ZmSetting.FORWARD_INCLUDE_ORIG);
+		var includePref = isReply ? identity.getReplyOption() : identity.getForwardOption();
 		this._curIncOption = ZmComposeController.INC_OP[includePref];
 		menu.checkItem(ZmOperation.KEY_ID, this._curIncOption, true);
 		if (isReply) {
@@ -826,8 +828,8 @@ function(msg, identity) {
 			this._action == ZmOperation.REPLY_DECLINE ||
 			this._action == ZmOperation.REPLY_TENTATIVE) && identity)
 		{
-			var bComposeSameFormat = appCtxt.get(ZmSetting.COMPOSE_SAME_FORMAT);
-			var bComposeAsFormat = appCtxt.get(ZmSetting.COMPOSE_AS_FORMAT);
+			var bComposeSameFormat = identity.getComposeSameFormat();
+			var bComposeAsFormat = identity.getComposeAsFormat();
 			if ((!bComposeSameFormat && bComposeAsFormat == ZmSetting.COMPOSE_HTML) ||
 			    (bComposeSameFormat && msg.isHtmlMail()))
 			{
@@ -1019,7 +1021,7 @@ function(op) {
 		var curText = this._getBodyContent();
 		if (this._curIncOption == ZmOperation.INC_NO_PREFIX || this._curIncOption == ZmOperation.INC_PREFIX ||
                     this._curIncOption == ZmOperation.INC_PREFIX_FULL ||
-		    this._curIncOption == ZmOperation.INC_SMART) {
+			this._curIncOption == ZmOperation.INC_SMART) {
 			var idx = curText.indexOf(this._composeView._includedPreface.replace(/\s+$/, ""));
 			if (idx) {
 				var crlf = (this._composeView.getComposeMode() == DwtHtmlEditor.HTML) ? "<br>" : "\\r?\\n";
@@ -1169,7 +1171,7 @@ function() {
 
 ZmComposeController.prototype._popShieldDiscardCallback =
 function() {
-	this._deleteDraft(this._composeView._msg);
+	this._deleteDraft(this._composeView.getOrigMsg());
 	this._popShieldNoCallback();
 };
 
