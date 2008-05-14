7649a86e5197a62446f4acb5efeb033262bc7325
Unknown Perforce User <pshah>
2008-05-13 19:21:58 -0500

    bug: 27049 - offline: 'Mail Free & Busy Link' sends incorrect link -- remove option from right-click menu for offline
    http://bugzilla.zimbra.com/show_bug.cgi?id=27049
    
    Copied from Perforce
     Change: 97590

--- WebRoot/js/zimbraMail/calendar/controller/ZmCalendarTreeController.js
+++ WebRoot/js/zimbraMail/calendar/controller/ZmCalendarTreeController.js
@@ -174,9 +174,11 @@ function() {
 	ops.push(ZmOperation.CHECK_ALL);
 	ops.push(ZmOperation.CLEAR_ALL);
     ops.push(ZmOperation.BROWSE);
-    ops.push(ZmOperation.SEP,ZmOperation.FREE_BUSY_LINK);
+	if (!appCtxt.isOffline) {
+		ops.push(ZmOperation.SEP, ZmOperation.FREE_BUSY_LINK);
+	}
 
-    return ops;
+	return ops;
 };
 
 // Returns a list of desired action menu operations
