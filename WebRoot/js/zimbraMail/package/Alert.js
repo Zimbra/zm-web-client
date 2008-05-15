c2e9bc8b84f62231393986f9a32459e4b6aaef09
Unknown Perforce User <dcomfort>
2008-05-15 10:36:38 -0500

    Bug: 7482 Refactor browser/app/audio alerts so we can use them in calendar & mail apps.
    http://bugzilla.zimbra.com/show_bug.cgi?id=7482
    
    Copied from Perforce
     Change: 97939

---
+++ WebRoot/js/zimbraMail/package/Alert.js
@@ -0,0 +1,25 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2007 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+/*
+ * Package: Alert
+ * 
+ * A few classes for flashing titles, icons, etc. 
+ */
+AjxPackage.require("zimbraMail.share.view.ZmAlert");
+AjxPackage.require("zimbraMail.share.view.ZmAppAlert");
+AjxPackage.require("zimbraMail.share.view.ZmBrowserAlert");
+AjxPackage.require("zimbraMail.share.view.ZmSoundAlert");
---
+++ WebRoot/js/zimbraMail/share/view/ZmAlert.js
@@ -0,0 +1,84 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2008 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+ * Abstract base class of flahing alerts.
+ */
+ZmAlert = function() {
+	this._isLooping = false;
+};
+
+// Abstract methods.
+ZmAlert.prototype.start = null; // function ()
+ZmAlert.prototype.stop = null; // function ()
+ZmAlert.prototype._update = null; // function(status)
+
+ZmAlert.prototype._startLoop =
+function() {
+	if (!ZmAlertLoop.INSTANCE) {
+		ZmAlertLoop.INSTANCE = new ZmAlertLoop();
+	}
+	ZmAlertLoop.INSTANCE._add(this);
+	this._isLooping = true;
+};
+
+ZmAlert.prototype._stopLoop =
+function() {
+	if (ZmAlertLoop.INSTANCE) {
+		ZmAlertLoop.INSTANCE._remove(this);
+	}
+	this._isLooping = false;
+	this._update(false);
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Private class only used by ZmAlert.
+ * Manages an interval that tells alerts when to flash icons and titles and such. 
+ */
+ZmAlertLoop = function() {
+	this._alerts = new AjxVector();
+	this._flashOn = false;
+};
+
+ZmAlertLoop.prototype._add =
+function(alert) {
+	this._alerts.add(alert, 0, true);
+	if (!this._alertInterval) {
+		this._alertInterval = setInterval(AjxCallback.simpleClosure(this._alertTimerCallback, this), 333);
+	}
+};
+
+ZmAlertLoop.prototype._remove =
+function(alert) {
+	this._alerts.remove(alert);
+	if (this._alertInterval && !this._alerts.size()) {
+		clearInterval(this._alertInterval);
+		this._alertInterval = 0;
+	}
+};
+
+ZmAlertLoop.prototype._alertTimerCallback =
+function() {
+	this._flashOn = !this._flashOn;
+	for (var i = 0, count = this._alerts.size(); i < count; i++) {
+	    this._alerts.get(i)._update(this._flashOn);
+	}
+};
+
