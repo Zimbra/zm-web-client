816ae429b37e372e39ea711c4048dd59cacd67fe
Unknown Perforce User <schemers>
2008-06-05 16:38:21 -0500

    Integrate change 101173 from main to FRANKLIN
    	----------
    	Change 101173 by schemers@slapshot on 2008/06/05 14:37:41
    
    		bug: 25369
    		-  make sure all attachments are selected after a save draft
    
    
    	Affected files ...
    
    	... //depot/main/ZimbraWebClient/WebRoot/h/compose#97 edit
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=25369
    
    Copied from Perforce
     Change: 101174

--- WebRoot/js/zimbraMail/ale/spreadsheet/ZmSpreadSheet.js
+++ WebRoot/js/zimbraMail/ale/spreadsheet/ZmSpreadSheet.js
@@ -416,11 +416,7 @@ ZmSpreadSheet.prototype._getTopHeaderCell = function(td) {
 };
 
 ZmSpreadSheet.prototype._getLeftHeaderCell = function(td) {
-	var cell = null;
-	var table = this._getTable();
-	var rows = (table && table.rows)? table.rows : null;
-	var rowIndex = (td && td.parentNode)? td.parentNode.rowIndex : 0;
-	return (rows ? rows[rowIndex][0] : null);
+	return this._getTable().rows[td.parentNode.rowIndex].cells[0];
 };
 
 ZmSpreadSheet.prototype._colsize_mouseMove = function(ev) {
