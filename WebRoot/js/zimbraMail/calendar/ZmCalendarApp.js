7878e56c5f92459ecf2b8d9234195ecd0d1adc62
Unknown Perforce User <pshah>
2008-08-26 19:16:42 -0500

    Integrate change 112418 from main to FRANKLIN
    -------------
    bug: 31025 - try #2
    --
    Recreate XHR object (for Firefox only) each time AjxRpcRequest object gets reused.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31025
    
    Copied from Perforce
     Change: 112420

--- WebRoot/js/zimbraMail/calendar/ZmCalendarApp.js
+++ WebRoot/js/zimbraMail/calendar/ZmCalendarApp.js
@@ -263,7 +263,7 @@ function() {
 	ZmOperation.registerOp(ZmId.OP_MOUNT_CALENDAR, {textKey:"mountCalendar", image:"GroupSchedule"});
 	ZmOperation.registerOp(ZmId.OP_NEW_ALLDAY_APPT, {textKey:"newAllDayAppt", tooltipKey:"newAllDayApptTooltip", image:"NewAppointment"});
 	ZmOperation.registerOp(ZmId.OP_NEW_APPT, {textKey:"newAppt", tooltipKey:"newApptTooltip", image:"NewAppointment"});
-	ZmOperation.registerOp(ZmId.OP_NEW_CALENDAR, {textKey:"newCalendar", image:"NewAppointment", tooltipKey: "newCalendarTooltip"});
+	ZmOperation.registerOp(ZmId.OP_NEW_CALENDAR, {textKey:"newCalendar", image:"NewAppointment"});
 	ZmOperation.registerOp(ZmId.OP_REPLY_ACCEPT, {textKey:"replyAccept", image:"Check"});
 	ZmOperation.registerOp(ZmId.OP_REPLY_CANCEL);
 	ZmOperation.registerOp(ZmId.OP_REPLY_DECLINE, {textKey:"replyDecline", image:"Cancel"});
@@ -332,7 +332,6 @@ function() {
 							 mountKey:			"mountCalendar",
 							 createFunc:		"ZmCalendar.create",
 							 compareFunc:		"ZmCalendar.sortCompare",
-							 newOp:				ZmOperation.NEW_CALENDAR,
 							 deferrable:		false
 							});
 };
@@ -347,6 +346,11 @@ function() {
 								 setting:		ZmSetting.CALENDAR_ENABLED,
 								 id:			ZmId.getMenuItemId(ZmId.SEARCH, ZmId.ITEM_APPOINTMENT)
 								});
+};
+
+ZmCalendarApp.prototype._setupCurrentAppToolbar =
+function() {
+	ZmCurrentAppToolBar.registerApp(this.getName(), ZmOperation.NEW_CALENDAR, ZmOrganizer.CALENDAR);
 };
 
 ZmCalendarApp.prototype._registerApp =
