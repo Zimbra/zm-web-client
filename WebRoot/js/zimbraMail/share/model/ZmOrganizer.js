2fa29b2e1a5db650a7b1b020dfa8aa2d313165ed
Unknown Perforce User <pshah>
2008-09-11 18:52:03 -0500

    bug: 31482 - need to show "Archive" system folder in offline UI
    http://bugzilla.zimbra.com/show_bug.cgi?id=31482
    
    Copied from Perforce
     Change: 114343

--- WebRoot/js/zimbraMail/share/model/ZmOrganizer.js
+++ WebRoot/js/zimbraMail/share/model/ZmOrganizer.js
@@ -99,7 +99,7 @@ ZmOrganizer.ID_CHATS			= 14;
 ZmOrganizer.ID_TASKS			= 15;
 ZmOrganizer.ID_BRIEFCASE		= 16;
 ZmOrganizer.ID_SYNC_FAILURES	= 252		// offline only
-ZmOrganizer.ID_ARCHIVE    		= 253;
+ZmOrganizer.ID_ARCHIVE    		= 253;		// offline only
 ZmOrganizer.ID_OUTBOX    		= 254;		// offline only
 ZmOrganizer.ID_ZIMLET			= -1000;	// zimlets need a range.  start from -1000 incrementing up.
 ZmOrganizer.ID_ROSTER_LIST		= -11;
@@ -122,6 +122,7 @@ ZmOrganizer.FLAG_CHECKED			= "#";
 ZmOrganizer.FLAG_DISALLOW_SUBFOLDER	= "o";
 ZmOrganizer.FLAG_EXCLUDE_FREE_BUSY	= "b";
 ZmOrganizer.FLAG_IMAP_SUBSCRIBED	= "*";
+ZmOrganizer.FLAG_OFFLINE_ARCHIVE	= "@";
 ZmOrganizer.FLAG_OFFLINE_SYNCABLE	= "y";
 ZmOrganizer.FLAG_OFFLINE_SYNCING	= "~";
 ZmOrganizer.ALL_FLAGS = [
@@ -129,6 +130,7 @@ ZmOrganizer.ALL_FLAGS = [
 	ZmOrganizer.FLAG_IMAP_SUBSCRIBED,
 	ZmOrganizer.FLAG_EXCLUDE_FREE_BUSY,
 	ZmOrganizer.FLAG_DISALLOW_SUBFOLDER,
+	ZmOrganizer.FLAG_OFFLINE_ARCHIVE,
 	ZmOrganizer.FLAG_OFFLINE_SYNCABLE,
 	ZmOrganizer.FLAG_OFFLINE_SYNCING
 ];
@@ -139,6 +141,7 @@ ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_CHECKED]				= "isChecked";
 ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_IMAP_SUBSCRIBED]		= "imapSubscribed";
 ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_EXCLUDE_FREE_BUSY]	= "excludeFreeBusy";
 ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_DISALLOW_SUBFOLDER]	= "disallowSubFolder";
+ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_OFFLINE_ARCHIVE]		= "isOfflineArchive";
 ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_OFFLINE_SYNCABLE]	= "isOfflineSyncable";
 ZmOrganizer.FLAG_PROP[ZmOrganizer.FLAG_OFFLINE_SYNCING]		= "isOfflineSyncing";
 
