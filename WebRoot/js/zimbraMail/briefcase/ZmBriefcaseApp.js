948e7520fe4c144f17c37bfac1b5647ede40a66b
Unknown Perforce User <ccao>
2008-09-12 19:26:14 -0500

    bug: 31428
    Checkpoint: add the account types to new account and account edit view - briefly tested in the global admin. Need to make it work in domain admin. 
    
    ** Integrate from main to FRANKLIN **
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31428
    
    Copied from Perforce
     Change: 114560

--- WebRoot/js/zimbraMail/briefcase/ZmBriefcaseApp.js
+++ WebRoot/js/zimbraMail/briefcase/ZmBriefcaseApp.js
@@ -58,14 +58,14 @@ function() {
 
 ZmBriefcaseApp.prototype._registerOperations =
 function() {
-	ZmOperation.registerOp("NEW_BRIEFCASEITEM", {textKey:"newBriefcase", image:"NewFolder", tooltipKey:"newBriefcaseTooltip"});
-	ZmOperation.registerOp("NEW_FILE", {textKey:"uploadNewFile", tooltipKey:"uploadNewFile", image:"NewPage"});
-	ZmOperation.registerOp("SHARE_BRIEFCASE", {textKey:"shareFolder", image:"SharedMailFolder"}, ZmSetting.SHARING_ENABLED);
-	ZmOperation.registerOp("MOUNT_BRIEFCASE", {textKey:"mountBriefcase", image:"Notebook"}, ZmSetting.SHARING_ENABLED);
-	ZmOperation.registerOp("OPEN_FILE", {textKey:"openFile", tooltipKey:"openFileTooltip", image:"NewPage"});
+	ZmOperation.registerOp(ZmId.OP_NEW_BRIEFCASEITEM, {textKey:"newBriefcase", image:"NewFolder"});
+	ZmOperation.registerOp(ZmId.OP_NEW_FILE, {textKey:"uploadNewFile", tooltipKey:"uploadNewFile", image:"NewPage"});
+	ZmOperation.registerOp(ZmId.OP_SHARE_BRIEFCASE, {textKey:"shareFolder", image:"SharedMailFolder"}, ZmSetting.SHARING_ENABLED);
+	ZmOperation.registerOp(ZmId.OP_MOUNT_BRIEFCASE, {textKey:"mountBriefcase", image:"Notebook"}, ZmSetting.SHARING_ENABLED);
+	ZmOperation.registerOp(ZmId.OP_OPEN_FILE, {textKey:"openFile", tooltipKey:"openFileTooltip", image:"NewPage"});
+	ZmOperation.registerOp(ZmId.OP_VIEW_FILE_AS_HTML, {textKey:"viewAsHtml", tooltipKey:"viewAsHtml", image:"HtmlDoc"});	
+	ZmOperation.registerOp(ZmId.OP_SEND_FILE, {textKey:"send", tooltipKey:"sendPageTT", image:"Send"});	
 	ZmOperation.registerOp("SAVE_FILE", {textKey:"saveFile", tooltipKey:"saveFileTooltip", image:"Save"});
-	ZmOperation.registerOp("VIEW_FILE_AS_HTML", {textKey:"viewAsHtml", tooltipKey:"viewAsHtml", image:"HtmlDoc"});
-	ZmOperation.registerOp("SEND_FILE", {textKey:"send", tooltipKey:"sendPageTT", image:"Send"});
 };
 
 ZmBriefcaseApp.prototype._registerItems =
@@ -108,7 +108,6 @@ function() {
 							  createFunc     : "ZmOrganizer.create",
 							  compareFunc    : "ZmBriefcase.sortCompare",
 							  deferrable     : true,
-							  newOp			 : ZmOperation.NEW_BRIEFCASEITEM,
 							  hasColor       : true
 							});
 };
@@ -121,6 +120,11 @@ function() {
 								 tooltipKey:	"searchForPages",
 								 icon:			"SearchNotes"
 								});*/
+};
+
+ZmBriefcaseApp.prototype._setupCurrentAppToolbar =
+function() {
+	ZmCurrentAppToolBar.registerApp(this.getName(), ZmOperation.NEW_BRIEFCASEITEM, ZmOrganizer.BRIEFCASE);
 };
 
 ZmBriefcaseApp.prototype._registerApp =
