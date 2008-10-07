926a337ef91963780aacfc3a9bbe00f0fb59f415
Unknown Perforce User <pshah>
2008-10-07 13:41:57 -0500

    bug: 31785 - try #4 - hide the offline status icon when in full screen mode
    http://bugzilla.zimbra.com/show_bug.cgi?id=31785
    
    Copied from Perforce
     Change: 118089

--- WebRoot/js/zimbraMail/core/ZmAppViewMgr.js
+++ WebRoot/js/zimbraMail/core/ZmAppViewMgr.js
@@ -161,8 +161,8 @@ ZmAppViewMgr.ALL_COMPONENTS = [ZmAppViewMgr.C_BANNER, ZmAppViewMgr.C_USER_INFO, 
 ZmAppViewMgr.APP_COMPONENTS = [
     ZmAppViewMgr.C_CURRENT_APP,
     ZmAppViewMgr.C_TREE, ZmAppViewMgr.C_TREE_FOOTER,
-    ZmAppViewMgr.C_STATUS, ZmAppViewMgr.C_SASH ,
-    ZmAppViewMgr.C_PRESENCE
+    ZmAppViewMgr.C_OFFLINE_STATUS, ZmAppViewMgr.C_STATUS,
+	ZmAppViewMgr.C_SASH, ZmAppViewMgr.C_PRESENCE
 ];
 
 // keys for getting container IDs
