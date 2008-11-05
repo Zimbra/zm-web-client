12f071cb4911c9bac6abd8e306f1845ffbfd7e41
Unknown Perforce User <pshah>
2008-11-05 13:32:51 -0600

    bug: 32854 - can not add or delete in shared contacts
    http://bugzilla.zimbra.com/show_bug.cgi?id=32854
    
    Copied from Perforce
     Change: 121779

--- WebRoot/js/zimbraMail/share/model/ZmList.js
+++ WebRoot/js/zimbraMail/share/model/ZmList.js
@@ -551,7 +551,7 @@ function(params, batchCmd) {
     var respCallback = params.callback ? new AjxCallback(this, this._handleResponseItemAction, [type, idHash, params.callback]) : null;
 
 	if (batchCmd) {
-		batchCmd.addRequestParams(itemActionRequest, respCallback, errorCallback);
+		batchCmd.addRequestParams(itemActionRequest, respCallback, params.errorCallback);
 	} else {
         var params = { asyncMode: true, callback: respCallback };
         useJson ? params.jsonObj = itemActionRequest : params.soapDoc = itemActionRequest;
