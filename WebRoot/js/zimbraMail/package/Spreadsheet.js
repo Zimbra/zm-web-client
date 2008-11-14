4ee3b31f037df2fe3972b07d515032e90e4f0db5
Unknown Perforce User <ssugumaran>
2008-11-14 07:15:18 -0600

    bug:  31181 spreadsheet component for docs
    http://bugzilla.zimbra.com/show_bug.cgi?id=31181
    
    Copied from Perforce
     Change: 123134

--- WebRoot/js/zimbraMail/package/Spreadsheet.js
+++ WebRoot/js/zimbraMail/package/Spreadsheet.js
@@ -4,3 +4,8 @@ AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheet");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetModel");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetFormulae");
 AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetToolbars");
+AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetLoader");
+
+AjxPackage.require("zimbraMail.briefcase.model.ZmDocletMgr");
+
+AjxPackage.require("zimbraMail.ale.spreadsheet.ZmSpreadSheetEditController");
