57f12a5b870647066178651cf82d6a0f9a0c58b6
Unknown Perforce User <rpatil>
2008-12-08 01:07:17 -0600

    bug: 33040 Enabled document search in Notebook... Integrating to FRANKLIN
    http://bugzilla.zimbra.com/show_bug.cgi?id=33040
    
    Copied from Perforce
     Change: 125979

--- WebRoot/js/zimbraMail/notebook/controller/ZmNotebookFileController.js
+++ WebRoot/js/zimbraMail/notebook/controller/ZmNotebookFileController.js
@@ -46,8 +46,20 @@ function(searchResults, fromUserSearch) {
 	this._currentView = ZmId.VIEW_NOTEBOOK_FILE;
 	this._setup(this._currentView);
 
-	this._list = searchResults.getResults(ZmItem.MIXED);
-	var lv = this._listView[this._currentView];
+	var temp_list = searchResults.getResults(ZmItem.MIXED);
+    this._list = new ZmMailList(ZmItem.MIXED, searchResults.search);
+    //Filter briefcase documents
+    if(temp_list){
+        var temp_arr = temp_list.getArray();
+        for (var i=0; i < temp_arr.length ; i++) {
+            var r = temp_arr[i];
+            var org = appCtxt.getById(r.folderId);
+            if(org && org instanceof ZmNotebook){
+               this._list.add(r);
+            }
+        }
+    }
+    var lv = this._listView[this._currentView];
 	if (this._activeSearch) {
 		if (this._list) {
 			var items = this._list.getArray();
