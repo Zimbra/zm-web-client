0c682cc79aa9ef5835485f161d3b929b24cbadd7
Unknown Perforce User <ssugumaran>
2009-02-08 08:57:19 -0600

    bug: 34758, 34769
    - optimized maintenance cycle to aggregate all requests
    - reverted reminder srch range overlap merging 
    - appt creation is optimized to refresh only related folders
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=34758
    http://bugzilla.zimbra.com/show_bug.cgi?id=34769
    
    Copied from Perforce
     Change: 144819

--- WebRoot/js/zimbraMail/calendar/controller/ZmCalViewController.js
+++ WebRoot/js/zimbraMail/calendar/controller/ZmCalViewController.js
@@ -83,6 +83,7 @@ ZmCalViewController = function(container, calApp) {
 	ZmCalViewController.OP_TO_VIEW[ZmOperation.SCHEDULE_VIEW]	= ZmId.VIEW_CAL_SCHEDULE;
 
 	this._errorCallback = new AjxCallback(this, this._handleError);
+	this._clearCacheFolderMap = {};
 };
 
 ZmCalViewController.prototype = new ZmListController();
@@ -1776,8 +1777,12 @@ function(callback, list, userQuery) {
 // TODO: appt is null for now. we are just clearing our caches...
 ZmCalViewController.prototype.notifyCreate =
 function(create) {
-	if (!this._clearCache) {
-		this._clearCache = true;
+	//mark folders which needs to be cleared
+	if(create && create.l) {
+		var cal = appCtxt.getById(create.l);
+		if(cal && cal.id) {
+			this._clearCacheFolderMap[cal.id] = true;
+		}
 	}
 };
 
@@ -1818,10 +1823,23 @@ function(modifies) {
 ZmCalViewController.prototype.notifyComplete =
 function() {
 	DBG.println(AjxDebug.DBG2, "ZmCalViewController: notifyComplete: " + this._clearCache);
+	
+	var clearCacheByFolder = false;
+	
+	for(var i in this._clearCacheFolderMap) {
+		DBG.println("clear cache :" + i);
+		this._apptCache.clearCache(i);
+		clearCacheByFolder = true;
+	}
+	this._clearCacheFolderMap = {};
+	
 	if (this._clearCache) {
 		var act = new AjxTimedAction(this, this._refreshAction);
 		AjxTimedAction.scheduleAction(act, 0);
 		this._clearCache = false;
+	}else if(clearCacheByFolder) {
+		var act = new AjxTimedAction(this, this._refreshAction, [true]);
+		AjxTimedAction.scheduleAction(act, 0);		
 	}
 };
 
@@ -1860,6 +1878,7 @@ function() {
 
 ZmCalViewController.prototype._refreshAction =
 function(dontClearCache) {
+	DBG.println(AjxDebug.DBG1, "ZmCalViewController: _refreshAction: " + dontClearCache);
 	// reset cache
 	if (!dontClearCache) {
 		this._apptCache.clearCache();
@@ -1888,7 +1907,7 @@ function(work, view, list, skipMiniCalUpdate, query) {
 		this._handleError(list, new AjxCallback(this, this._maintErrorHandler));
 		return;
 	}
-	
+	DBG.println(AjxDebug.DBG2, "ZmCalViewController: _maintGetApptCallback: " + work);
 	this._userQuery = query;
 	
 	if (work & ZmCalViewController.MAINT_VIEW) {
@@ -1970,11 +1989,16 @@ function() {
 	
 	var maintainMiniCal = (work & ZmCalViewController.MAINT_MINICAL);
 	var maintainView = (work & ZmCalViewController.MAINT_VIEW);
+	var maintainRemainder = (work & ZmCalViewController.MAINT_REMINDER);
 	
-	if(maintainMiniCal || maintainView) {
+	if(work == ZmCalViewController.MAINT_REMINDER) {
+		this._app.getReminderController().refresh();
+	}
+	else if(maintainMiniCal || maintainView || maintainRemainder) {
 		var view = this.getCurrentView();
+		DBG.println(AjxDebug.DBG2, "ZmCalViewController: _maintenanceAction : View:" + view);
 		if (view && view.needsRefresh()) {
-			var rt = this.getOptimizedTimeRange(view);
+			var rt = view.getTimeRange();
 			var cb = new AjxCallback(this, this._maintGetApptCallback, [work, view]);
 			
 			var params = {start:rt.start, end:rt.end, fanoutAllDay:view._fanoutAllDay(), callback:cb};
@@ -1982,38 +2006,18 @@ function() {
 			params.query = this._userQuery;
 			
 			var miniCalParams = this.getMiniCalendarParams(work);
-			this._apptCache.batchRequest(params, miniCalParams);
+			var reminderParams = null;
+			
+			if(maintainRemainder) {
+				reminderParams = this._app.getReminderController().getRefreshParams();
+				reminderParams.callback = null;
+			}
+						
+			this._apptCache.batchRequest(params, miniCalParams, reminderParams);
 		    
 			view.setNeedsRefresh(false);
         }
 	}
-};
-
-ZmCalViewController.prototype.getOptimizedTimeRange =
-function(view) {
-	var timeRange = view.getTimeRange();
-	
-	var endOfDay = new Date();
-    endOfDay.setHours(23,59,59,999);
-
-    //optimization step: grab a week's appt backwards
-    var reminderEnd = endOfDay.getTime();
-
-    endOfDay.setDate(endOfDay.getDate()-7);
-	endOfDay.setHours(0,0,0, 0);
-
-    var reminderStart = endOfDay.getTime();
-
-	var startInRange = (reminderStart >= timeRange.start) && (reminderStart <= timeRange.end);
-	var endInRange = (reminderEnd >= timeRange.start) && (reminderEnd <= timeRange.end);
-
-	//if reminder search overlaps widen the time range to include the reminder search
-    if(startInRange || endInRange) {
-		timeRange.start = (reminderStart < timeRange.start) ? reminderStart : timeRange.start;
-		timeRange.end = (reminderEnd > timeRange.end) ? reminderEnd : timeRange.end;
-	}
-	
-	return timeRange;
 };
 
 ZmCalViewController.prototype.getKeyMapName =
