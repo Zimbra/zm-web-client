760ea1bb51d7eb2bea1515cae306932a6faebf4d
Unknown Perforce User <ssugumaran>
2009-03-31 10:20:42 -0500

    bug:  33639 handled a case where organizer is missing from invite component which throwed JS error on SendInviteReply request
    http://bugzilla.zimbra.com/show_bug.cgi?id=33639
    
    Copied from Perforce
     Change: 151792

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -688,6 +688,15 @@ function(edited, componentId, callback, errorCallback, instanceDate, accountName
 	var inv = this._origMsg.invite;
 	if (this.getAddress(AjxEmailAddress.TO) == null && !inv.isOrganizer()) {
 		var to = inv.getSentBy() || inv.getOrganizerEmail();
+        if(to == null) {
+            var ac = window.parentAppCtxt || window.appCtxt;
+            var mainAcct = ac.getMainAccount().getEmail();
+            var from = this._origMsg.getAddresses(AjxEmailAddress.FROM).get(0);
+            //bug: 33639 when organizer component is missing from invitation
+            if (from && from.address != mainAcct) {
+                to = from.address;
+            }
+        }
 		this.setAddress(AjxEmailAddress.TO, (new AjxEmailAddress(to)));
 	}
 
