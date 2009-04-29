fbd95d41e9449445ab11e610a5e89a67e5a6dd19
Unknown Perforce User <pshah>
2009-04-29 15:06:00 -0500

    bug: 37532 - Explorer View is broken in briefcase
    http://bugzilla.zimbra.com/show_bug.cgi?id=37532
    
    Copied from Perforce
     Change: 155976

--- WebRoot/js/zimbraMail/briefcase/view/ZmBriefcaseView.js
+++ WebRoot/js/zimbraMail/briefcase/view/ZmBriefcaseView.js
@@ -88,7 +88,7 @@ function(item, params) {
 	var span = document.createElement("span");
 	
 	if(item instanceof ZmBriefcaseItem){
-		span.innerHTML = ["<a href='",item.restUrl,"' target='_blank'>",name,"</a>"].join("");
+		span.innerHTML = ['<a href="',item.getRestUrl(),'" target="_blank">',name,'</a>'].join('');
 	}else{
 		span.innerHTML = item;
 	}
