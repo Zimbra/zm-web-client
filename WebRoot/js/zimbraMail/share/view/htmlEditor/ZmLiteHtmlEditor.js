0ad6b3a6adbf1f416c91509b0d656242d49757e3
Unknown Perforce User <dcomfort>
2009-05-11 17:27:05 -0500

    Bug: 37237 A better fix for inserting smilies in IE.
    http://bugzilla.zimbra.com/show_bug.cgi?id=37237
    
    Copied from Perforce
     Change: 157497

--- WebRoot/js/zimbraMail/share/view/htmlEditor/ZmLiteHtmlEditor.js
+++ WebRoot/js/zimbraMail/share/view/htmlEditor/ZmLiteHtmlEditor.js
@@ -223,16 +223,8 @@ function(enable){
 
 ZmLiteHtmlEditor.prototype.insertText =
 function(text) {
-	if (AjxEnv.isIE) {
-		this._textArea.focus();
-		document.selection.createRange().text = text;
-	} else if (this._textArea.createTextRange && this._textArea.selection) { // I don't believe this block ever gets executed.
-		var textRange = this._textArea.selection;
-		textRange.text = (textRange.text.charAt(textRange.text.length-1) == '') ? smiley + ' ' : text;
-		textRange.selection = nulltext;
-	} else {
-		this._textArea.value += text;
-	}
+	this._textArea.focus();
+	this._textArea.value += text;
 };
 
 ZmLiteHtmlEditor.prototype.getBasicToolBar = function() {
