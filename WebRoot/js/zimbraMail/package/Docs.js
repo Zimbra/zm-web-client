d3dcbb9c438e7f1de06983bc4239092ee90c086a
Unknown Perforce User <sraj>
2009-06-26 17:30:23 -0500

    bug:38742
    updated the package file
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=38742
    
    Copied from Perforce
     Change: 163417

--- WebRoot/js/zimbraMail/package/Docs.js
+++ WebRoot/js/zimbraMail/package/Docs.js
@@ -19,7 +19,12 @@
  *
  */
 
+AjxPackage.require("zimbraMail.docs.ZmDocsEditApp");
+
+AjxPackage.require("zimbraMail.docs.view.ZmDocsEditor");
 AjxPackage.require("zimbraMail.docs.view.ZmDocsEditView");
-AjxPackage.require("zimbraMail.briefcase.model.ZmDocletMgr");
+AjxPackage.require("zimbraMail.docs.view.ZmOpenDocDialog");
 
 AjxPackage.require("zimbraMail.docs.controller.ZmDocsEditController");
+
+AjxPackage.require("zimbraMail.briefcase.model.ZmDocletMgr");
