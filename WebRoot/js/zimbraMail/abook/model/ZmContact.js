74471d5f64114651747085e125f7c23c2ec4747e
Unknown Perforce User <rsegu>
2009-07-28 04:49:25 -0500

    bug: 33085
    Enable user to select one or more contacts from the address book to send a email to the selected contacts
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=33085
    
    Copied from Perforce
     Change: 167322

--- WebRoot/js/zimbraMail/abook/model/ZmContact.js
+++ WebRoot/js/zimbraMail/abook/model/ZmContact.js
@@ -740,10 +740,10 @@ function(phone, field) {
 ZmContact.prototype.getEmail =
 function() {
 	return (this.getAttr(ZmContact.F_email) ||
+            this.getAttr(ZmContact.F_workEmail1) ||
 			this.getAttr(ZmContact.F_email2) ||
+            this.getAttr(ZmContact.F_workEmail2) ||
 			this.getAttr(ZmContact.F_email3) ||
-			this.getAttr(ZmContact.F_workEmail1) ||
-			this.getAttr(ZmContact.F_workEmail2) ||
 			this.getAttr(ZmContact.F_workEmail3));
 };
 
