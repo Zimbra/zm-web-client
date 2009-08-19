e96fc1d25fb1dfcbb45225509ce1b214e0a0f109
Unknown Perforce User <rpatil>
2009-08-19 03:01:27 -0500

    bug:  39793 - Resolves the XSS issue without affecting any current bahavior.
    http://bugzilla.zimbra.com/show_bug.cgi?id=39793
    
    Copied from Perforce
     Change: 170056

--- WebRoot/js/zimbraMail/notebook/view/ZmNotebookPageView.js
+++ WebRoot/js/zimbraMail/notebook/view/ZmNotebookPageView.js
@@ -520,7 +520,7 @@ ZmNotebookPageView.prototype.createHistoryLink = function(doc,wikiName){
 };
 
 ZmNotebookPageView.prototype.editPage = function(pageName){
-
+    pageName = AjxStringUtil.htmlDecode(pageName);
 	var controller = this._controller;
 	var object = controller._object;
 	var cache = appCtxt.getApp(ZmApp.NOTEBOOK).getNotebookCache();
@@ -534,7 +534,7 @@ ZmNotebookPageView.prototype.editPage = function(pageName){
 };
 
 ZmNotebookPageView.prototype.deletePage = function(pageName){
-
+    pageName = AjxStringUtil.htmlDecode(pageName);
 	var controller = this._controller;
 	var object = controller._object;
 	var cache = appCtxt.getApp(ZmApp.NOTEBOOK).getNotebookCache();
@@ -547,7 +547,7 @@ ZmNotebookPageView.prototype.deletePage = function(pageName){
 };
 
 ZmNotebookPageView.prototype.showHistory = function(pageName){
-
+    pageName = AjxStringUtil.htmlDecode(pageName);
 	var controller = this._controller;
 	var object = controller._object;
 	var cache = appCtxt.getApp(ZmApp.NOTEBOOK).getNotebookCache();
