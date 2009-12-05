2412908bc5182fbd3d76382e5f3a7dded1eab841
Conrad Damon <cdamon@zimbra.com>
2009-12-04 19:34:17 -0600

    Bug: 43164 - New messages not shown when viewing folders under "All Mailboxes" Add null check
    http://bugzilla.zimbra.com/show_bug.cgi?id=43164
    
    Copied from Perforce
     Change: 220734

--- WebRoot/js/zimbraMail/share/model/ZmSearch.js
+++ WebRoot/js/zimbraMail/share/model/ZmSearch.js
@@ -805,7 +805,7 @@ function() {
 		}
 	}
 
-	if (this.isMultiAccount()) {
+	if (term && this.isMultiAccount()) {
 		if (term.op == "in" || term.op == "inid") {
 			var result = ZmOrganizer.parseId(term.arg);
 			this.folderId = result.id;
