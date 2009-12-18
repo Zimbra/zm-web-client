7d2fcbf082cf07b434c569eaec489392354cb739
Conrad Damon <cdamon@zimbra.com>
2009-12-18 14:54:24 -0600

    Bug: 43194 - Briefcase cleanup Update path of ZmUploadDialog
    http://bugzilla.zimbra.com/show_bug.cgi?id=43194
    
    Copied from Perforce
     Change: 222410

--- WebRoot/js/zimbraMail/package/Slides.js
+++ WebRoot/js/zimbraMail/package/Slides.js
@@ -24,7 +24,7 @@ AjxPackage.require("zimbraMail.slides.view.ZmSlideComponentEditor");
 AjxPackage.require("zimbraMail.slides.view.DwtAnimate");
 AjxPackage.require("zimbraMail.slides.view.ZmSlideEditView");
 AjxPackage.require("zimbraMail.briefcase.model.ZmDocletMgr");
-AjxPackage.require("zimbraMail.notebook.view.ZmUploadDialog");
+AjxPackage.require("zimbraMail.share.view.dialog.ZmUploadDialog");
 AjxPackage.require("zimbraMail.notebook.view.ZmUploadConflictDialog");
 
 AjxPackage.require("zimbraMail.slides.controller.ZmSlideEditController");
