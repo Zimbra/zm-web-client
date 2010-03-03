fdfe0be03c917fd879a32e1f4893fb3098ba1665
Unknown Perforce User <lars>
2010-03-03 08:57:05 -0600

    bug: 44934 - zimbraChildAccount PV problems
    Add extra check on account before bailing out on checking preconditions. (It was a little too eager before)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=44934
    
    Copied from Perforce
     Change: 227633

--- WebRoot/js/zimbraMail/prefs/view/ZmPrefView.js
+++ WebRoot/js/zimbraMail/prefs/view/ZmPrefView.js
@@ -158,7 +158,7 @@ function(sectionId) {
 ZmPrefView.prototype._addSection =
 function(section, index) {
 	// does the section meet the precondition?
-	if (!appCtxt.multiAccounts && !this._controller.checkPreCondition(section)) { return; }
+	if ((!appCtxt.multiAccounts || (appCtxt.multiAccounts && appCtxt.getActiveAccount().isMain)) && !this._controller.checkPreCondition(section)) { return; }
 
 	// create pref page's view
 	var view = (section.createView)
