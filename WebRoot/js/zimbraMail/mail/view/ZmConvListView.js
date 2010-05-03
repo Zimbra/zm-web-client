f6a1b0b1d9145162e1c6804432ffdce430367e26
Unknown Perforce User <steffen>
2010-05-03 08:38:00 -0500

    Bug: 45767
    Message list wil no longer show the "From" name in the "To" view column.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=45767
    
    Copied from Perforce
     Change: 233980

--- WebRoot/js/zimbraMail/mail/view/ZmConvListView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmConvListView.js
@@ -332,7 +332,12 @@ function(htmlArr, idx, item, field, colIdx, params) {
 		if (field == ZmItem.F_STATUS || field == ZmItem.F_FOLDER) {
 			htmlArr[idx++] = "&nbsp;";
 		} else if (field == ZmItem.F_FROM) {
-			htmlArr[idx++] = this._getParticipantHtml(item, this._getFieldId(item, ZmItem.F_PARTICIPANT));
+			var isFolder = this._isSentOrDraftsFolder();
+			if (isFolder.sent) {
+				htmlArr[idx++] = ""
+			} else {
+				htmlArr[idx++] = this._getParticipantHtml(item, this._getFieldId(item, ZmItem.F_PARTICIPANT));
+			}
 		} else if (field == ZmItem.F_SUBJECT) {
 			htmlArr[idx++] = item.subject ? AjxStringUtil.htmlEncode(item.subject, true) : AjxStringUtil.htmlEncode(ZmMsg.noSubject);
 			if (appCtxt.get(ZmSetting.SHOW_FRAGMENTS) && item.fragment) {
