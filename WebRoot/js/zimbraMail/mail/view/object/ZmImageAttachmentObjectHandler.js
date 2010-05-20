79f99ffc14d284f9fc7fad24699bf21f7ea8ecad
Unknown Perforce User <lasse>
2010-05-20 07:24:42 -0500

    bug 31471 - Image zimlet changes aspect ratio of image
    http://bugzilla.zimbra.com/show_bug.cgi?id=31471
    
    Copied from Perforce
     Change: 236152

--- WebRoot/js/zimbraMail/mail/view/object/ZmImageAttachmentObjectHandler.js
+++ WebRoot/js/zimbraMail/mail/view/object/ZmImageAttachmentObjectHandler.js
@@ -15,6 +15,7 @@
 
 ZmImageAttachmentObjectHandler = function() {
 	ZmObjectHandler.call(this, ZmImageAttachmentObjectHandler.TYPE);
+	this._imageHash = {};
 }
 
 ZmImageAttachmentObjectHandler.prototype = new ZmObjectHandler;
@@ -23,6 +24,7 @@ ZmImageAttachmentObjectHandler.prototype.constructor = ZmImageAttachmentObjectHa
 ZmImageAttachmentObjectHandler.TYPE = "imageAttachemnt";
 
 ZmImageAttachmentObjectHandler.THUMB_SIZE = 'width="320" height="240"';
+ZmImageAttachmentObjectHandler.THUMB_SIZE_MAX = 320;
 	
 // already htmlencoded!!
 ZmImageAttachmentObjectHandler.prototype._getHtmlContent =
@@ -33,10 +35,52 @@ function(html, idx, obj, context) {
 
 ZmImageAttachmentObjectHandler.prototype.getToolTipText =
 function(url, context) {
-	return '<img ' +ZmImageAttachmentObjectHandler.THUMB_SIZE + ' src="' + context.url + '"></img>';	
+	var image = this._imageHash[context.url];
+	if (!image) {
+		image = {id:Dwt.getNextId()};
+		this._imageHash[context.url] = image;
+		this._preload(context.url, image.id);	
+	}
+	
+	var el = document.getElementById(image.id);
+	if (el && !image.el) {
+		image.el = el;
+	}
+	if (image.el) {
+		return image.el.xml || image.el.outerHTML;
+	}
+	return '<img id="'+ image.id +'" style="visibility:hidden;"/>';
 };
 
 ZmImageAttachmentObjectHandler.prototype.getActionMenu =
 function(obj) {
 	return null;
 };
+
+ZmImageAttachmentObjectHandler.prototype._preload =
+function(url, id) {
+	var tmpImage = new Image();
+	tmpImage.onload = AjxCallback.simpleClosure(this._setSize, this, id, tmpImage);
+	tmpImage.src = url;
+}
+
+ZmImageAttachmentObjectHandler.prototype._setSize =
+function(id, tmpImage) {
+	var elm = document.getElementById(id);
+	if(elm) {
+		var width = tmpImage.width;
+		var height = tmpImage.height;
+		if(width > ZmImageAttachmentObjectHandler.THUMB_SIZE_MAX && width >= height) {
+			elm.width = ZmImageAttachmentObjectHandler.THUMB_SIZE_MAX;
+		} else if (height > ZmImageAttachmentObjectHandler.THUMB_SIZE_MAX && height > width) {
+			elm.height = ZmImageAttachmentObjectHandler.THUMB_SIZE_MAX;
+		} else {
+			elm.width = width;
+			elm.width = height;
+		}
+		elm.src = tmpImage.src;
+		elm.style.visibility = "visible";
+	}
+	tmpImage.onload = null;
+	tmpImage = null;
+}
