50d6c46931456820f2138622dba32b14cf36ba8c
Conrad Damon <cdamon@zimbra.com>
2010-06-14 18:03:39 -0500

    Bug: 47102 - deleting messages in the Trash from a conversation breaks conversation expansion Remove check, so now when user clicks on arrow we just expand. Considered doing replenishment, but that seems heavy for expanded convs.
    http://bugzilla.zimbra.com/show_bug.cgi?id=47102
    
    Copied from Perforce
     Change: 238472

--- WebRoot/js/zimbraMail/mail/controller/ZmConvListController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmConvListController.js
@@ -276,7 +276,7 @@ ZmConvListController.prototype._listSelectionListener =
 function(ev) {
 	var item = ev.item;
 	if (!item) { return; }
-	if (ev.field == ZmItem.F_EXPAND && (this._mailListView._isExpandable(item))) {
+	if (ev.field == ZmItem.F_EXPAND) {
 		this._toggle(item, false);
 	} else {
 		var handled = ZmDoublePaneController.prototype._listSelectionListener.apply(this, arguments);
