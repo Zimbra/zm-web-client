4d804297caec2e8b4bc0101c5ddd117bcd7f9c89
Unknown Perforce User <pshah>
2010-06-18 16:55:03 -0500

    bug: 48210 - sender name of external account is wrong since 6.0.7 --------- Use the "personal" name if exists when setting FROM field for external accounts
    http://bugzilla.zimbra.com/show_bug.cgi?id=48210
    
    Copied from Perforce
     Change: 239078

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -1988,7 +1988,7 @@ function(addrNodes, parentNode, isDraft, accountName) {
 				// main account is "sender"
 				if (!doNotAddSender) {
 					addrNode.t = "s";
-					addrNode.p = name || ac.get(ZmSetting.DISPLAY_NAME);
+					addrNode.p = ac.get(ZmSetting.DISPLAY_NAME);
 					addrNode = {};
 					addrNodes.push(addrNode);
 				}
@@ -1996,7 +1996,7 @@ function(addrNodes, parentNode, isDraft, accountName) {
 				addrNode.t = "f";
 				addrNode.a = dataSource.getEmail();
 				if (ac.get(ZmSetting.DEFAULT_DISPLAY_NAME)) {
-					addrNode.p = name || dataSource.getName();
+					addrNode.p = dataSource.userName || dataSource.getName();
 				}
 			}
 		}
