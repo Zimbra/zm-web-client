c28674d4740ec9220a5cb52e52d3e604f1f16a28
Unknown Perforce User <lars>
2010-07-12 01:28:37 -0500

    bug: 47591 - prefix "|" or ">" not used in reply when html composer is used
    http://bugzilla.zimbra.com/show_bug.cgi?id=47591
    
    Copied from Perforce
     Change: 242122

--- WebRoot/tiny_mce_plugins/zbreakquote/editor_plugin.js
+++ WebRoot/tiny_mce_plugins/zbreakquote/editor_plugin.js
@@ -31,7 +31,7 @@
 						}
 
 						if (ancestor) {
-							t.handleBlockquoteAdd(ancestor, element, range);
+							t.splitBlockquote(ancestor, element, ed);
 							if (tinymce.isIE)
 								return tinymce.dom.Event.cancel(ev);
 						}
@@ -46,10 +46,11 @@
 									child2 = child2.nextSibling;
 
 								if (child2 && child.tagName && child2.tagName && child.tagName.toLowerCase()=="blockquote" && child2.tagName.toLowerCase()=="blockquote") {
-									t.mergeBlockquotes(child, child2);
+									t.mergeBlockquotes(child, child2, ed);
 								}
 							}
 						}, this), 5);
+						if (ev.keyCode==46) {
 //							if (ev.preventDefault)
 //								ev.preventDefault();
 
@@ -66,7 +67,8 @@
 			});
 		},
 
-		handleBlockquoteAdd : function(blockquote, element, range) {
+		splitBlockquote : function(blockquote, element, ed) {
+			var selection = ed.selection, range = selection.getRng();
 			var el, offset=null, coffset=null;
 			var text1, text2;
 			range.collapse(false);
@@ -114,7 +116,9 @@
 				el2.innerHTML = text2;
 			} else if (coffset!==null) {
 				this.removeNextSiblings(el1.childNodes[coffset]); // cut away all siblings after breakpoint for el1
-				this.removePreviousSiblings(el2.childNodes[coffset]); // and all sibling before breakpoint for el2
+				var c2 = el2.childNodes[coffset];
+				this.removePreviousSiblings(c2); // and all sibling before breakpoint for el2
+				this.removeElement(c2);
 			}
 
 			// Prune off all "later" siblings in the blockquote tree
@@ -151,11 +155,29 @@
 
 				range.moveToElementText(span2);
 				range.collapse(0);
-				range.select();
+				//range.select();
+				selection.setRng(range);
 				span2.parentNode.removeChild(span2);
 
 			} else {
+				if (tinymce.isSafari) {
+					var t = this;
+					setTimeout(function(){
+						var p;
+						if (blockquote.lastChild && blockquote.lastChild.childElementCount==1 && blockquote.lastChild.firstChild instanceof HTMLBRElement)
+							p = blockquote.lastChild;
+						else if (blockquote2.firstChild && blockquote2.firstChild.childElementCount==1 && blockquote2.firstChild.firstChild instanceof HTMLBRElement)
+							p = blockquote2.firstChild;
+						if (p) {
+							blockquote.parentNode.insertBefore(p, blockquote2);
+							range.setStart(p,0);
+							selection.removeAllRanges();
+							selection.addRange(range);
+						}
+					},5);
+				}
 				range.setStartAfter(blockquote);
+				selection.setRng(range);
 			}
 		},
 
@@ -225,7 +247,35 @@
 			}
 		},
 
-		mergeBlockquotes : function(blockquote1, blockquote2) {
+		/*selectNode : function(el, range) {
+			var set;
+			var range = this.ed.selection.getRng();
+			while (!set && el) {
+				try {
+					range.setStartBefore(el);
+					set = true;
+				} catch (ex) {
+					el = el.parentNode;
+					set = false;
+				}
+			}
+		},*/
+
+		nextElement : function(el) {
+			if (el.childNodes && el.childNodes.length)
+				return el.childNodes[0];
+			if (el.nextSibling)
+				return el.nextSibling;
+			var p = el.parentNode;
+			while (!el.nextSibling) {
+				el = el.parentNode;
+				if (!el) return null;
+			}
+			return el.nextSibling;
+		},
+
+		mergeBlockquotes : function(blockquote1, blockquote2, ed) {
+			var selection = ed.selection, range = selection.getRng();
 
 			if (tinymce.isIE) {
 				this.removeIEFiller(blockquote1.lastChild);
@@ -235,10 +285,8 @@
 			var depth1 = 0;
 			while (el1.childNodes.length) { // Descend into blockquote1, finding the very last leaf node in the tree
 				el1 = el1.childNodes[el1.childNodes.length-1];
-				if (tinymce.isIE) {
-					while (el1.previousSibling && el1.tagName=="BR")
-						el1 = el1.previousSibling;
-				}
+				while (el1.previousSibling && el1.tagName=="BR")
+					el1 = el1.previousSibling;
 				depth1++;
 			}
 
@@ -257,19 +305,77 @@
 				el2 = el2.parentNode;
 				depth2--;
 			}
-			
+
 			if (depth1==depth2) { // Simplest case, just append the contents of el2 to el1.
-				el1.innerHTML = el1.innerHTML + el2.innerHTML;
+
+				var text = [el1.innerHTML, el2.innerHTML];
+				var dummy = "###"+tinymce.DOM.uniqueId()+"###";
+				el1.innerHTML = text.join(dummy);
+				var offset = el1.innerHTML.replace(/<br>/ig," ").replace(/<\/?[^>]+>/g,"").indexOf(dummy);
+				el1.innerHTML = text.join("");
+				if (tinymce.isIE) {
+					range.moveToElementText(el1);
+					range.moveStart("character", offset);
+				} else {
+					var p;
+					for (p = el1; p != null && offset > 0; p = this.nextElement(p)) { // Walk through elements, decrementing offset as we go, and set the range when we find an element where the remaining offset fits
+						var type = p.nodeType;
+						var textContent = p.textContent || p.innerText || p.innerHTML || "";
+
+						if (type==3 || type==4 || type==8) {
+							if (offset <= textContent.length)
+								break;
+							offset -= textContent.length;
+						} else {
+							if (p.tagName.toLowerCase()=="br")
+								offset--;
+							if (offset==0)
+								break;
+						}
+					}
+					range.setStart(p, offset);
+				}
 			} else if (depth1==depth2+1) { // We're merging at a node border, append all children of el2 to el1's parent (making them siblings of el1)
 				while (el2.firstChild) {
 					el1.parentNode.appendChild(el2.firstChild);
 				}
+				if (tinymce.isIE) {
+					var type = p.nodeType;
+					var offset = 1;
+					if (type==3 || type==4 || type==8) {
+						offset = p.length;
+						p = p.parentNode;
+					}
+					range.moveToElementText(p);
+					range.moveStart("character",offset);
+				} else {
+					if (el1.tagName && el1.tagName.toLowerCase()=="br")
+						range.setStartBefore(el1);
+					else
+						range.setStartAfter(el1);
+				}
 				el1 = el1.parentNode;
 
 			} else if (depth1+1==depth2) { // We're merging at a node border, append el2 and all its siblings to el1 (making them children of el1)
+				var p = el1.childNodes.length ? el1.childNodes[el1.childNodes.length-1] : el1;
 				el2 = el2.parentNode;
 				while (el2.firstChild) {
 					el1.appendChild(el2.firstChild);
+				}
+				if (tinymce.isIE) {
+					var type = p.nodeType;
+					var offset = 1;
+					if (type==3 || type==4 || type==8) {
+						offset = p.length;
+						p = p.parentNode;
+					}
+					range.moveToElementText(p);
+					range.moveStart("character",offset);
+				} else {
+					if (p.tagName && p.tagName.toLowerCase()=="br")
+						range.setStartBefore(p);
+					else
+						range.setStartAfter(p);
 				}
 			} else { // We don't handle nodes that are further apart
 				return;
@@ -283,6 +389,8 @@
 				el2 = el2.parentNode;
 			}
 			this.removeElement(blockquote2); // All significant contents have been transferred, kill blockquote2
+			range.collapse(true);
+			selection.setRng(range);
 		},
 
 		getInfo : function() {
