b3ff48810b64c40f42cc4ff8e91eda1639437150
Unknown Perforce User <pshah>
2010-07-19 13:18:41 -0500

    Integrate change 243380 from main to GNR
    -------------
    bug: 48187 - Localization is not being translated for Zimlet's panel items
    ---------
    Use the *main* account (instead of active account) when in offline mode to retrieve the zimlet tree
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48187
    
    Copied from Perforce
     Change: 243381

--- WebRoot/js/zimbraMail/share/view/dialog/ZmAttachDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmAttachDialog.js
@@ -591,23 +591,16 @@ function(inputEl, sizeEl){
 
     var files = inputEl.files;
     if(!files) return;
-
-    var sizeStr = [], className, totalSize =0;
-    for(var i=0; i<files.length;i++){
-        var file = files[i];
-        var size = file.size || file.fileSize /*Safari*/;
-        if(size > appCtxt.get(ZmSetting.ATTACHMENT_SIZE_LIMIT))
-            className = "RedC";
-        totalSize += size;
-    }
-
+    
+    var file = files[0];
+    var size = file.size || file.fileSize /*Safari*/;
     if(sizeEl) {
-        sizeEl.innerHTML = "  ("+AjxUtil.formatSize(totalSize, true)+")";
-        if(className)
+        sizeEl.innerHTML = "  ("+AjxUtil.formatSize(size, true)+")";
+        if(size > appCtxt.get(ZmSetting.ATTACHMENT_SIZE_LIMIT))
             Dwt.addClass(sizeEl, "RedC");
         else
             Dwt.delClass(sizeEl, "RedC");
-    }
+    }    
 };
 
 
@@ -694,12 +687,10 @@ function(){
 	for (var i = 0; i < atts.length; i++){
         file = atts[i].files;
         if(!file || file.length == 0) continue;
-        for(var j=0; j<file.length;j++){
-            var f = file[j];
-            size = f.size || f.fileSize /*Safari*/;
-            if(size > appCtxt.get(ZmSetting.ATTACHMENT_SIZE_LIMIT)){
-                return false;
-            }
+        file = file[0];
+        size = file.size || file.fileSize /*Safari*/;
+        if(size > appCtxt.get(ZmSetting.ATTACHMENT_SIZE_LIMIT)){
+            return false;
         }
     }
 	return true;
