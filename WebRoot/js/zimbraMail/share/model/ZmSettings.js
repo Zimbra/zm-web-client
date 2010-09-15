d9cd4cf2ea108847b89e040584f71954322003c4
Unknown Perforce User <kkarthikeyan>
2010-09-15 13:58:05 -0500

    Integrate change 250539 from  ZDESKTOP-608 ----------------- Change 250539 by kkarthikeyan@kk-mac on 2010/09/15 11:48:54
            bug:50668 - Message view and Reading pane options don't stick
    
            In offline modifyPrefRequest, if the setting is implicit and global, 
            use main Account
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=50668
    
    Copied from Perforce
     Change: 250545

--- WebRoot/js/zimbraMail/share/model/ZmSettings.js
+++ WebRoot/js/zimbraMail/share/model/ZmSettings.js
@@ -501,9 +501,9 @@ function(list, callback, batchCommand, account, isImplicit) {
 	var acct = account || appCtxt.getActiveAccount();
 	var soapDoc = AjxSoapDoc.create("ModifyPrefsRequest", "urn:zimbraAccount");
 	var gotOne = false;
-	var metaData = [], done = {};
+	var metaData = [], done = {}, setting;
 	for (var i = 0; i < list.length; i++) {
-		var setting = list[i];
+		setting = list[i];
         if (done[setting.id]) { continue; }
 		if (setting.type == ZmSetting.T_METADATA) {
 			metaData.push(setting);
@@ -539,6 +539,11 @@ function(list, callback, batchCommand, account, isImplicit) {
         done[setting.id] = true;
 		gotOne = true;
 	}
+
+    // bug: 50668 if the setting is implicit and global, use main Account
+    if(appCtxt.isOffline && ZmSetting.IS_IMPLICIT[setting.id] && ZmSetting.IS_GLOBAL[setting.id]) {
+        acct = appCtxt.accountList.mainAccount;
+    }
 
 	if (metaData.length > 0) {
 		var metaDataCallback = new AjxCallback(this, this._handleResponseSaveMetaData, [metaData]);
