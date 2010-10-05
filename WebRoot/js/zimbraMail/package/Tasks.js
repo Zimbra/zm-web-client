187e01d9bdfce1c3a17e2d74ea68bc26c93ec0c1
Unknown Perforce User <sraj>
2010-10-05 14:28:49 -0500

    bug:51490
    moved ZmTaskView and ZmTaskMultiView from taskscore to tasks
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=51490
    
    Copied from Perforce
     Change: 252735

--- WebRoot/js/zimbraMail/package/Tasks.js
+++ WebRoot/js/zimbraMail/package/Tasks.js
@@ -35,6 +35,9 @@ AjxPackage.require("zimbraMail.calendar.view.ZmCalItemView");
 
 AjxPackage.require("zimbraMail.calendar.controller.ZmCalendarTreeController");
 
+
+AjxPackage.require("zimbraMail.tasks.view.ZmTaskView");
+AjxPackage.require("zimbraMail.tasks.view.ZmTaskMultiView");
 AjxPackage.require("zimbraMail.tasks.view.ZmTaskEditView");
 AjxPackage.require("zimbraMail.tasks.view.ZmNewTaskFolderDialog");
 AjxPackage.require("zimbraMail.tasks.view.ZmTaskAssistant");
