446cf6496eb1c64f0b40f1e99279a7121516bf05
Eran Yarkon <eyarkon@zimbra.com>
2010-10-11 21:07:46 -0500

    Bug 48918
    if the browser supports html5 audio tag and playing wav files on it - use it instead of the embed tag. It should solve the problem for newer FF versions that support html5 embed.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48918
    
    Copied from Perforce
     Change: 253461

--- WebRoot/js/zimbraMail/share/view/ZmSoundAlert.js
+++ WebRoot/js/zimbraMail/share/view/ZmSoundAlert.js
@@ -18,7 +18,8 @@
  * @private
  */
 ZmSoundAlert = function() {
-	this.enabled = AjxPluginDetector.detectQuickTime() || AjxPluginDetector.detectWindowsMedia();
+	this.html5AudioEnabled = ZmSoundAlert.isHtml5AudioEnabled();
+	this.enabled = this.html5AudioEnabled || AjxPluginDetector.detectQuickTime() || AjxPluginDetector.detectWindowsMedia();
 	if (this.enabled) {
 		var element = this._element = document.createElement("DIV");
 		element.style.position = 'relative';
@@ -29,6 +30,26 @@ ZmSoundAlert = function() {
 		DBG.println("No QuickTime or Windows Media plugin detected. Sound alerts are disabled.")
 	}
 };
+
+//this tests for wav file support. we might want to refactor it for various file types if needed later
+ZmSoundAlert.isHtml5AudioEnabled =
+function() {
+
+	try {
+		var audio = new Audio("");
+		if (!audio.canPlayType) {
+			return false;
+		}
+	}
+	catch (e) {
+		return false;
+	}
+
+	// canPlayType(type) returns: "", "no", "maybe" or "probably"
+	var canPlayWav = audio.canPlayType("audio/wav");
+	return (canPlayWav !== "no" && canPlayWav !== "");
+
+}
 
 ZmSoundAlert.prototype.toString =
 function() {
@@ -42,42 +63,55 @@ function() {
 
 ZmSoundAlert.prototype.start =
 function(soundFile) {
-	if (this.enabled) {
-		var time = new Date().getTime();
-		if (!this._lastTime || ((time - this._lastTime) > 5000)) {
-			soundFile = soundFile || "/public/sounds/im/alert.wav";
-			var url = appContextPath + soundFile;
-			var embedId = Dwt.getNextId();
-			var htmlArr = [
-				"<object CLASSID='CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6' type='audio/wav'>",
-				"<param name='url' value='", url, "'>",
-				"<param name='autostart' value='true'>",
-				"<param name='controller' value='true'>",
-				"<embed id='", embedId, "' src='", url, "' controller='false' autostart='true' type='audio/wav'/>",
-				"</object>"
-			];
-			this._element.innerHTML = htmlArr.join("");
-			this._lastTime = time;
+	if (!this.enabled) {
+		return;
+	}
 
-			if (AjxEnv.isFirefox && AjxEnv.isWindows) {
-				// The quicktime plugin steals focus and breaks our keyboard nav.
-				// The best workaround I've found for this is to blur the embed
-				// element, and that only works after the sound plays.
-				//
-				// Unfortunately it seems that on a slow connection this prevents
-				// the sound from playing. I'm hoping this is less bad than killing
-				// keyboard focus.
-				//
-				// Mozilla bug: https://bugzilla.mozilla.org/show_bug.cgi?id=78414
-				if (this._blurActionId) {
-					AjxTimedAction.cancelAction(this._blurActionId);
-					this._blurActionId = null;
-				}
-				this._blurEmbedTimer(embedId, 0);
-			}
+	var time = new Date().getTime();
+	if (this._lastTime && ((time - this._lastTime) < 5000)) {
+		return;
+	}
+	this._lastTime = time;
+
+	soundFile = soundFile || "/public/sounds/im/alert.wav";
+	var url = appContextPath + soundFile;
+
+	var html;
+	if (this.html5AudioEnabled) {
+		html = '<audio src="' + url + '" autoplay="yes"></audio>';
+	}
+	else {
+		var embedId = Dwt.getNextId();
+		var htmlArr = [
+			"<object CLASSID='CLSID:6BF52A52-394A-11d3-B153-00C04F79FAA6' type='audio/wav'>",
+			"<param name='url' value='", url, "'>",
+			"<param name='autostart' value='true'>",
+			"<param name='controller' value='true'>",
+			"<embed id='", embedId, "' src='", url, "' controller='false' autostart='true' type='audio/wav'/>",
+			"</object>"
+		];
+		html = htmlArr.join("");
+	}
+	this._element.innerHTML = html;
+
+	if (!this.html5AudioEnabled && AjxEnv.isFirefox && AjxEnv.isWindows) {
+		// The quicktime plugin steals focus and breaks our keyboard nav.
+		// The best workaround I've found for this is to blur the embed
+		// element, and that only works after the sound plays.
+		//
+		// Unfortunately it seems that on a slow connection this prevents
+		// the sound from playing. I'm hoping this is less bad than killing
+		// keyboard focus.
+		//
+		// Mozilla bug: https://bugzilla.mozilla.org/show_bug.cgi?id=78414
+		if (this._blurActionId) {
+			AjxTimedAction.cancelAction(this._blurActionId);
+			this._blurActionId = null;
 		}
+		this._blurEmbedTimer(embedId, 0);
 	}
 };
+
 ZmSoundAlert.prototype._blurEmbedTimer =
 function(embedId, tries) {
 	var action = new AjxTimedAction(this, this._blurEmbed, [embedId, tries]);
