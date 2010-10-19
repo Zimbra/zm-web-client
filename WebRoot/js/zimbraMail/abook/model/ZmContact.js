c24ecf62afa45419bf6ac2e3bce07f3bfd0721c0
Unknown Perforce User <ssutar>
2010-10-19 02:59:27 -0500

    bug: 14823 Reverted the previous changes
    http://bugzilla.zimbra.com/show_bug.cgi?id=14823
    
    Copied from Perforce
     Change: 254431

--- WebRoot/js/zimbraMail/abook/model/ZmContact.js
+++ WebRoot/js/zimbraMail/abook/model/ZmContact.js
@@ -71,7 +71,6 @@ ZmContact.F_email					= "email";
 ZmContact.F_email2					= "email2";
 ZmContact.F_email3					= "email3";
 ZmContact.F_fileAs					= "fileAs";
-ZmContact.F_showExtImg				= "autoLoadRemoteImages";
 ZmContact.F_firstName				= "firstName";
 ZmContact.F_folderId				= "folderId";
 ZmContact.F_homeCity				= "homeCity";
@@ -149,9 +148,6 @@ ZmContact.FA_COMPANY_LAST_C_FIRST	= i++;
 ZmContact.FA_COMPANY_FIRST_LAST		= i++;
 ZmContact.FA_CUSTOM					= i++;
 
-ZmContact.SHOW_EXT_IMG = "Y";
-ZmContact.HIDE_EXT_IMG = "N";
-
 // Field information
 
 ZmContact.ADDRESS_FIELDS = [
@@ -219,8 +215,7 @@ ZmContact.PRIMARY_FIELDS = [
 	ZmContact.F_company,
 	ZmContact.F_fileAs,
 	ZmContact.F_folderId,
-	ZmContact.F_notes,
-	ZmContact.F_showExtImg
+	ZmContact.F_notes
 ];
 ZmContact.URL_FIELDS = [
     // NOTE: sync with field order in ZmEditContactView's templates
