25f3c39a683ec1a292a55dfa5ad901939b3b8fe0
Unknown Perforce User <ssutar>
2010-11-24 04:04:00 -0600

    bug: 53483 Changed border and BG color in sched assistant
    http://bugzilla.zimbra.com/show_bug.cgi?id=53483
    
    Copied from Perforce
     Change: 259540

--- WebRoot/js/zimbraMail/calendar/controller/ZmApptComposeController.js
+++ WebRoot/js/zimbraMail/calendar/controller/ZmApptComposeController.js
@@ -872,7 +872,7 @@ function(ev) {
 ZmApptComposeController.prototype._printListener =
 function() {
 	var calItem = this._composeView._apptEditView._calItem;
-	var url = ("/h/printappointments?id=" + calItem.invId + "&tz=" + AjxTimezone.getServerId(AjxTimezone.DEFAULT)); //bug:53493
+	var url = ("/h/printappointments?id=" + calItem.invId + "&tz=" + AjxTimezone.getServerId(AjxTimezone.DEFAULT));
 	window.open(appContextPath+url, "_blank");
 };
 
