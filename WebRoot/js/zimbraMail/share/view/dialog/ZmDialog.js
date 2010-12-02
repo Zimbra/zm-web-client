5e6c8bb35f88465f38e96a873356083da042f558
Eran Yarkon <eyarkon@zimbra.com>
2010-12-01 19:20:40 -0600

    Bug 53945
    revert bug 52983 fix. Fixing the timeout issue did not solve the bug 52983 problem reliably and introduced a possibility that users would not get the focus fast enough for their typing.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=53945
    
    Copied from Perforce
     Change: 260690

--- WebRoot/js/zimbraMail/share/view/dialog/ZmDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmDialog.js
@@ -94,9 +94,7 @@ function() {
 
 	DwtDialog.prototype.popup.call(this);
 	if (this._focusElement) {
-        setTimeout(function(){
-            appCtxt.getKeyboardMgr().grabFocus(this._focusElement);
-        }, 10);
+		appCtxt.getKeyboardMgr().grabFocus(this._focusElement);	
 	}
 };
 
