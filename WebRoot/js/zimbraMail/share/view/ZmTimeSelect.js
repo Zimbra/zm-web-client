b0b402d93f13c42e33c82b6c685e378d154c4357
Unknown Perforce User <gsolovyev>
2010-12-02 15:19:16 -0600

    bug 53906 expose the drafts folder in the dialog add two messages "Only drafts can be moved to Drafts folder" and "You cannot move a draft message to the selected destination folder" I added a flag "showDrafts" to params instead of remove DRAFTS from folders omited by default because there are 50 other places that rely on Drafts folder being hidden by default (integrate from main)
    http://bugzilla.zimbra.com/show_bug.cgi?id=53906
    
    Copied from Perforce
     Change: 260884

--- WebRoot/js/zimbraMail/share/view/ZmTimeSelect.js
+++ WebRoot/js/zimbraMail/share/view/ZmTimeSelect.js
@@ -755,6 +755,8 @@ function() {
     //create time select drop down button
     var timeSelectButton = this._timeSelectBtn = new DwtButton({parent:this});
     timeSelectButton.addDropDownSelectionListener(buttonListener);
+	//also add selection listener
+	timeSelectButton.addSelectionListener(buttonListener);
     timeSelectButton.setData(Dwt.KEY_ID, buttonId);
     if (AjxEnv.isIE) {
         timeSelectButton.setSize("20");
