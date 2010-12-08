e36b0bf417ae18e9ce1b4b4eb5a79f930e3171b5
Unknown Perforce User <jwagner>
2010-12-08 12:14:12 -0600

    Bug: 51190 Unable to manage/admin briefcase share from ZWC Added checks for isAdmin and if ROLE is Manager when creating "new folder" option
    http://bugzilla.zimbra.com/show_bug.cgi?id=51190
    
    Copied from Perforce
     Change: 261715

--- WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseTreeController.js
+++ WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseTreeController.js
@@ -90,7 +90,7 @@ function(actionMenu, type, id) {
             menuItem = actionMenu.getMenuItem(ZmOperation.NEW_BRIEFCASE);
             menuItem.setText(ZmMsg.newFolder);
             menuItem.setImage("NewSection");
-            menuItem.setEnabled(!isLinkOrRemote || ZmBriefcaseTreeController.__isAllowed(briefcase, ZmShare.PERM_CREATE_SUBDIR));
+            menuItem.setEnabled(!isLinkOrRemote || ZmBriefcaseTreeController.__isAllowed(briefcase, ZmShare.PERM_CREATE_SUBDIR) || briefcase.isAdmin() || ZmShare.getRoleFromPerm(briefcase.perm) == ZmShare.ROLE_MANAGER);
 
             if (appCtxt.get(ZmSetting.SHARING_ENABLED)) {
                 isBriefcase = (!isRoot && briefcase.parent.id == rootId);
