85317db48cc77b6cf6284d5c0291d816f977c05d
Eran Yarkon <eyarkon@zimbra.com>
2010-12-07 18:45:06 -0600

    Bug 51413
    when switching on this option it was not clearing up the field so it was still including the msg as attachment
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=51413
    
    Copied from Perforce
     Change: 261579

--- WebRoot/js/zimbraMail/mail/view/ZmComposeView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmComposeView.js
@@ -2086,6 +2086,8 @@ function(action, msg, extraBodyText) {
 		}
 	}
 
+	this._msgAttId = null; //clear it in case of switching from "as attachment" back to "include original message" or to "don't include original"
+
 	if (action == ZmOperation.REPLY_CANCEL) {
 	  	value += crlf + sigPre; 	
 	} else if (incOptions.what == ZmSetting.INC_NONE) {
