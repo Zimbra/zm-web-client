9fe6f3583eb514d09a6dfa46d516da13bde5b3e5
Unknown Perforce User <jwagner>
2010-12-13 11:09:20 -0600

    Bug: 54339 icons for attachments, flag, and tags clipped in Safari (looks OK in Firefox) Set width to variable for attachments, flag and tags.
    http://bugzilla.zimbra.com/show_bug.cgi?id=54339
    
    Copied from Perforce
     Change: 262537

--- WebRoot/js/zimbraMail/mail/view/ZmConvListView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmConvListView.js
@@ -372,10 +372,10 @@ function(item, colIdx) {
 	}
 	idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_SUBJECT, colIdx);
 	if (item.hasAttach) {
-		idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_ATTACHMENT, colIdx, "16", "valign=top");
+		idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_ATTACHMENT, colIdx, width, "valign=top");
 	}
 	if (appCtxt.get("FLAGGING_ENABLED")) {
-		idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_FLAG, colIdx, "16");
+		idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_FLAG, colIdx, width);
 	}
 	htmlArr[idx++] = "</tr></table>";
 
@@ -404,7 +404,7 @@ function(item, colIdx) {
 	}
 
 	idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_DATE, colIdx, ZmMsg.COLUMN_WIDTH_DATE, "align=right");
-	idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_TAG, colIdx, "16");
+	idx = this._getAbridgedCell(htmlArr, idx, item, ZmItem.F_TAG, colIdx, width);
 	htmlArr[idx++] = "</tr></table>";
 
 	return htmlArr.join("");
