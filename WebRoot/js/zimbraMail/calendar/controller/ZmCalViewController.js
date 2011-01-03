261ebe55b8435957ada5aa01083b76b7e3a6248e
Unknown Perforce User <naveenp>
2011-01-03 12:18:16 -0600

    bug: 53705
    changed the width and size of the login elements. Removed  
    unnecessary texts and links to keep the login page for handheld device neat and tidy.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=53705
    
    Copied from Perforce
     Change: 267449

--- WebRoot/js/zimbraMail/calendar/controller/ZmCalViewController.js
+++ WebRoot/js/zimbraMail/calendar/controller/ZmCalViewController.js
@@ -1344,7 +1344,7 @@ function(ev) {
 		for (var i = 0; i < list.length; i++) {
 			ids.push(list[i].invId);
 		}
-		url = "/h/printappointments?id=" + ids.join(",") + "&tz=" + AjxTimezone.getServerId(AjxTimezone.DEFAULT);
+		url = "/h/printappointments?id=" + ids.join(",") + "&tz=" + AjxTimezone.getServerId(AjxTimezone.DEFAULT);  //bug:53493
 
 	} else {
 		var date = this._viewMgr
@@ -1376,7 +1376,7 @@ function(ev) {
 			"/h/printcalendar?view=", view,
 			"&l=", l,
 			"&date=", date.getFullYear(), month, day,
-			"&tz=",AjxTimezone.getServerId(AjxTimezone.DEFAULT)
+            "&tz=",AjxTimezone.getServerId(AjxTimezone.DEFAULT)                 //bug:53493
 		].join("");
 	}
 
@@ -1657,7 +1657,7 @@ function(appt, mode) {
 	} else {
 		var msg = ZmMsg.confirmCancelAppt;
 		if (appt.isRecurring()) {
-	    	msg = (mode == ZmCalItem.MODE_DELETE_INSTANCE) ? AjxMessageFormat.format(ZmMsg.confirmCancelApptInst, appt.name) :  ZmMsg.confirmCancelApptSeries; 
+			msg = (mode == ZmCalItem.MODE_DELETE_INSTANCE) ? AjxMessageFormat.format(ZmMsg.confirmCancelApptInst, appt.name) :  ZmMsg.confirmCancelApptSeries;
 		}
 		confirmDialog.popup(msg, cancelNoReplyCallback);
 	}
