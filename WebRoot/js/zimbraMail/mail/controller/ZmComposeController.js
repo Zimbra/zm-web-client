7f46b8d0f64a15b5ca003885c911d6e87cbc5acd
Eran Yarkon <eyarkon@zimbra.com>
2011-01-21 16:45:07 -0600

    Integrate change 271663 from HELIX to main
    -------------
    Bug 55252
    this removes updating the checkbox on rollover.
    Coincidently, it fixes the problem with the calendar list view. The problem
    there was (still is in a sense) that the ZmListView._getToolTip was called
    twice
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=55252
    
    Copied from Perforce
     Change: 271664

--- WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -505,8 +505,10 @@ function(attId, docIds, draftType, callback, contactId) {
 	var requestReadReceipt = false;
 	if (appCtxt.get(ZmSetting.MAIL_READ_RECEIPT_ENABLED)) {
 		var menu = this._toolbar.getButton(ZmOperation.COMPOSE_OPTIONS).getMenu();
-		var mi = menu.getItemById(ZmOperation.KEY_ID, ZmOperation.REQUEST_READ_RECEIPT);
-		requestReadReceipt = (!!(mi && mi.getChecked()));
+        if (menu){
+		    var mi = menu.getItemById(ZmOperation.KEY_ID, ZmOperation.REQUEST_READ_RECEIPT);
+		    requestReadReceipt = (!!(mi && mi.getChecked()));
+        }
 	}
 
 	var respCallback = new AjxCallback(this, this._handleResponseSendMsg, [draftType, msg, callback]);
