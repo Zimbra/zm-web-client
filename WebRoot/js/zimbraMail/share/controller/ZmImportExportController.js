a5bbb1b4fc83dbb931ca189974ea056f205062c3
Unknown Perforce User <lthomsen>
2011-02-24 01:56:06 -0600

    Integrate Change 277414 by lhojgaard@cabo-linux on 2011/02/21 00:55:04
            bug: 53744 - __NC_BIZ_ Enhancement of the export interface: add 
            calendar I/F for the date search
    
            Added two DwtMiniCalendars to select the date between which we want 
            to export.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=53744
    
    Copied from Perforce
     Change: 278065

--- WebRoot/js/zimbraMail/share/controller/ZmImportExportController.js
+++ WebRoot/js/zimbraMail/share/controller/ZmImportExportController.js
@@ -428,6 +428,8 @@ function(params) {
 	if (isCSV) { formParams[type+"fmt"] = subType; }
 	if (isTGZ && params.views) { formParams["types"] = params.views; }
 	if (isTGZ && params.searchFilter) { formParams["query"] = params.searchFilter; }
+	if (params.start) { formParams["start"] = params.start; }
+	if (params.end) { formParams["end"] = params.end; }
 	if (params.skipMeta) { formParams["meta"] = "0"; }
 	if (params.filename) { formParams["filename"] = params.filename; }
 	formParams.emptyname = ZmMsg.exportEmptyName;
