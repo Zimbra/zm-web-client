f9b67c96eff53e2c1c63d9a593e28d0ea43fb903
Unknown Perforce User <jwagner>
2011-03-03 18:19:08 -0600

    Bug: 57494 Javascript Error: id.replace is not a function Add typeof string check before replace is called
    http://bugzilla.zimbra.com/show_bug.cgi?id=57494
    
    Copied from Perforce
     Change: 279695

--- WebRoot/js/zimbraMail/share/view/ZmPopupMenu.js
+++ WebRoot/js/zimbraMail/share/view/ZmPopupMenu.js
@@ -160,8 +160,9 @@ function(id, params) {
 
     //set context menu tr id
     var row = mi.getRowElement();
-    if (row)
+    if (row && id && typeof id == "string") {
         row.setAttribute("id", "POPUP" + "_" + id.replace(/\s/g, ""));
+    }
 
 	return mi;
 };
