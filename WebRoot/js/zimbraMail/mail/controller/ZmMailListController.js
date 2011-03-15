7ed8db67ef0c704a0191eea2952a0bf13f993ce5
Unknown Perforce User <rsegu>
2011-03-15 14:03:54 -0500

    bug: 57918
    Force load the full message before Draft / Reply / Forward actions if the message is truncated or not loaded.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57918
    
    Copied from Perforce
     Change: 281705

--- WebRoot/js/zimbraMail/mail/controller/ZmMailListController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmMailListController.js
@@ -861,7 +861,7 @@ function(params) {
 	if (action == ZmOperation.DRAFT || action == ZmOperation.FORWARD_INLINE ||
             action == ZmOperation.REPLY || action == ZmOperation.REPLY_ALL) {
 		var bp = msg.getBodyPart();
-		if (bp && bp.truncated) {
+		if ((bp && bp.truncated) || !msg._loaded) {
 			params.noTruncate = true;
 			params.forceLoad = true;
 		}
