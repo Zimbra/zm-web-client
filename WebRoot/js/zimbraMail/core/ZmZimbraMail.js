99704d574832585c56f8120654ed3e8471d524df
Unknown Perforce User <jflanigan>
2011-05-04 14:11:42 -0500

    Integrate change 283441 from ZDESKTOP-701        bug: 47503        - when network is up, always set a retry timer before sending        clientEventNotifyRequest        - fixed how response is checked and how timer is set
    http://bugzilla.zimbra.com/show_bug.cgi?id=47503
    
    Copied from Perforce
     Change: 303862

--- WebRoot/js/zimbraMail/core/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/core/ZmZimbraMail.js
@@ -1142,26 +1142,31 @@ function(event, isNetworkOn) {
 		},
 		asyncMode:true
 	};
+
     if (isNetworkOn) {
         params.callback = new AjxCallback(this, this.handleClientEventNotifyResponse, event);
         params.noBusyOverlay = true;
+
+        if (this.clientEventNotifyReqId) {
+            appCtxt.getRequestMgr().cancelRequest(this.clientEventNotifyReqId);
+        }
+        this.clientEventNotifyTimerId = 
+            AjxTimedAction.scheduleAction(new AjxTimedAction(this, this.sendClientEventNotify, [event, true]), 3000);
     } else {
         params.callback = new AjxCallback(this, this.setInstantNotify, true);
     }
-	this.sendRequest(params);
+
+    this.clientEventNotifyReqId = this.sendRequest(params);
 };
 
 ZmZimbraMail.prototype.handleClientEventNotifyResponse =
 function(event, res) {
-    var response = res.body && res.body.ClientEventNotifyResponse;
-    if (response) {
-        this.setInstantNotify(true);
+    if (!res.isException() && res.getResponse()) {
         if (this.clientEventNotifyTimerId) {
             AjxTimedAction.cancelAction(this.clientEventNotifyTimerId);
             this.clientEventNotityTimerId = null;
         }
-    } else {
-        this.clientEventNotifyTimerId = AjxTimedAction.scheduleAction(this.sendClientEventNotify(event, true), 2000);
+        this.setInstantNotify(true);
     }
 };
 
