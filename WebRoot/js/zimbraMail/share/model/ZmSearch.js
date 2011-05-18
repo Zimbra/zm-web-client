97413422ec62b7be61e521f156782c3b40a0fcd7
Unknown Perforce User <komalk>
2011-05-18 14:25:19 -0500

    Integrate the following change from HELIX to main Change 311110 by komalk@komalk_macmain1 on 2011/05/18 12:12:57
            bug:60037 - Dogfood: NPE in org.apache.jsp.tag.web.imginfo_tag.doTag
            -Added code to avoid a possible NPE, make sure the cache is always 
            initialized.
    
            review URL: https://reviewboard.eng.vmware.com/r/233151
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=60037
    
    Copied from Perforce
     Change: 311117

--- WebRoot/js/zimbraMail/share/model/ZmSearch.js
+++ WebRoot/js/zimbraMail/share/model/ZmSearch.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -150,9 +150,9 @@ function() {
 ZmSearch.prototype.execute =
 function(params) {
 	if (params.batchCmd || this.soapInfo) {
-		this._executeSoap(params);
+		return this._executeSoap(params);
 	} else {
-		this._executeJson(params);
+		return this._executeJson(params);
 	}
 };
 
