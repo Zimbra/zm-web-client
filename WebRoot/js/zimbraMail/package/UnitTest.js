1372594e1ceded59ec9a550acb2d807c91aecad8
Unknown Perforce User <jwagner>
2011-06-01 16:34:04 -0500

    Bug: 59760 Zimlet to embed youtube video Use ID for hash of YouTube Links rather than URL.  Implemented UnitTests for parsing IDs from YouTube link, parsing links from message text.  Added support for youtu.be links.
    http://bugzilla.zimbra.com/show_bug.cgi?id=59760
    
    Copied from Perforce
     Change: 315374

--- WebRoot/js/zimbraMail/package/UnitTest.js
+++ WebRoot/js/zimbraMail/package/UnitTest.js
@@ -1,4 +1,5 @@
 // load via <script> so that debuggers have access to their source
 AjxPackage.require({name:"zimbraMail.unittest.UtString", method:AjxPackage.METHOD_SCRIPT_TAG});
 AjxPackage.require({name:"zimbraMail.unittest.UtCompose", method:AjxPackage.METHOD_SCRIPT_TAG});
-AjxPackage.require({name:"zimbraMail.unittest.UtBubbles", method:AjxPackage.METHOD_SCRIPT_TAG});
+AjxPackage.require({name:"zimbraMail.unittest.UtBubbles", method:AjxPackage.METHOD_SCRIPT_TAG});;
+AjxPackage.require({name:"zimbraMail.unittest.UtYouTube", method:AjxPackage.METHOD_SCRIPT_TAG});
