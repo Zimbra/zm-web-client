4bfdf959efa8a539854914e3a6446bcc345b75da
Unknown Perforce User <lthomsen>
2011-07-20 02:24:27 -0500

    bug: 59456 - Message content is getting displayed multiple times.
    Reload attachment metadata when reloading message.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=59456
    
    Copied from Perforce
     Change: 322013

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -717,11 +717,13 @@ function(params, callback, result) {
 
 	// clear all attachments and body data
 	this.attachments.length = this._bodyParts.length = 0;
+	this.findAttsFoundInMsgBodyDone = false;
 
 	this._loadFromDom(response.m[0]);
 	if (!this.isReadOnly() && params.markRead) {
 		this._markReadLocal(true);
 	}
+	this.findAttsFoundInMsgBody();
 
 	// return result so callers can check for exceptions if they want
 	if (this._loadCallback) {
