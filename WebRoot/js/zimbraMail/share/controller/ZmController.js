927a06c2aff497e127398ccdb4dd498d91511202
Unknown Perforce User <jwagner>
2011-08-09 17:00:58 -0500

    Bug: 63005 Preferences - HTML form problem (2) HTML encode the error dialog message and stack trace exception
    http://bugzilla.zimbra.com/show_bug.cgi?id=63005
    
    Copied from Perforce
     Change: 324885

--- WebRoot/js/zimbraMail/share/controller/ZmController.js
+++ WebRoot/js/zimbraMail/share/controller/ZmController.js
@@ -97,7 +97,7 @@ function(msg, ex, noExecReset, hideReportButton, expanded) {
 		html[i++] = "<table>";
 		for (var j = 0; j < fields.length; j++) {
 			var fld = fields[j];
-			var value = ex[fld];
+			var value = AjxStringUtil.htmlEncode(ex[fld]);
 			if (value) {
 				if (fld == "request") {
 					value = ["<pre>", value, "</pre>"].join("");
@@ -113,7 +113,7 @@ function(msg, ex, noExecReset, hideReportButton, expanded) {
 		detailStr = html.join("");
 	}
 	errorDialog.registerCallback(DwtDialog.OK_BUTTON, this._errorDialogCallback, this);
-	errorDialog.setMessage(msg, detailStr, DwtMessageDialog.CRITICAL_STYLE, ZmMsg.zimbraTitle);
+	errorDialog.setMessage(AjxStringUtil.htmlEncode(msg), detailStr, DwtMessageDialog.CRITICAL_STYLE, ZmMsg.zimbraTitle);
 	errorDialog.popup(null, hideReportButton);
 	if (expanded)
 		errorDialog.showDetail();
