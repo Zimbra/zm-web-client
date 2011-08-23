03b3c13a25213dae65d3fcba97fb963c3e1ca41e
Unknown Perforce User <kkarthikeyan>
2011-08-23 16:24:58 -0500

    Integrate change 326911 from ZDESKTOP-712 --------------------------- bug:63287 - URL for multi-item download in shared briefcase is incorrect --------------------------- use fully qualified id only for shared folder.
    http://bugzilla.zimbra.com/show_bug.cgi?id=63287
    
    Copied from Perforce
     Change: 326913

--- WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseController.js
+++ WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseController.js
@@ -916,8 +916,8 @@ function(items){
         for(var i=0; i< length; i++){
             item = items[i];
             var itemId;
-            if (appCtxt.isOffline) {
-                itemId = [organizer.getAccount().id, ":", item.getNormalizedItemId()].join('');
+            if (appCtxt.isOffline && organizer.isShared()) {
+                itemId = item.id;
             } else {
                 itemId = item.getNormalizedItemId();
             }
