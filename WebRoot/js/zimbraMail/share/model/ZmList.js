98313e0973275978b6d187a753ffb98f03a0b2eb
Eran Yarkon <eyarkon@zimbra.com>
2011-08-25 13:29:24 -0500

    Bug 63743
    Folder name can be included JavaScript and possible to execute
    sanitize summary message that's displayed via the toast status message.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63743
    
    Copied from Perforce
     Change: 327332

--- WebRoot/js/zimbraMail/share/model/ZmList.js
+++ WebRoot/js/zimbraMail/share/model/ZmList.js
@@ -1007,6 +1007,7 @@ function(summary, actionLogItem, showToastOnParentWindow) {
 		ZmList.progressDialog = null;
 	}
 	if (summary) {
+		summary = AjxStringUtil.htmlEncode(summary); //encode html special chars such as < and > so won't be interpreted as html (both for security and for not losing visibility of characters)
 		var ctxt = showToastOnParentWindow ? parentAppCtxt : appCtxt;
 		var actionController = ctxt.getActionController();
 		var undoLink = actionLogItem && actionController && actionController.getUndoLink(actionLogItem);
