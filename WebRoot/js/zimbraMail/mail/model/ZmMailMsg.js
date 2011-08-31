f14baac9e0805916f6cbe094712ac97e08003131
Unknown Perforce User <jwagner>
2011-08-31 16:26:48 -0500

    Bug: 63979 Inline Image attachment gets duplicated on saving draft below the subject field Inline images now reference insrc=cid:
    http://bugzilla.zimbra.com/show_bug.cgi?id=63979
    
    Copied from Perforce
     Change: 328448

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -1697,7 +1697,7 @@ function() {
 		if (bodyPart.ct == ZmMimeTable.TEXT_HTML) {
 			content = bodyPart.content;
 			var msgRef = this;
-			content.replace(/dfsrc=([\x27\x22])cid:([^\x27\x22]+)\1/ig, function(s, q, cid) {
+			content.replace(/src=([\x27\x22])cid:([^\x27\x22]+)\1/ig, function(s, q, cid) {
 				var attach = msgRef.findInlineAtt("<" + AjxStringUtil.urlComponentDecode(cid)  + ">");
 				if (attach) {
 					attach.foundInMsgBody = true;
@@ -1713,7 +1713,7 @@ function() {
 	var body = this.getBodyPart(ZmMimeTable.TEXT_HTML);
 	if (body) {
 		body = AjxUtil.isString(body) ? body : body.content;
-		if (body && body.search(/dfsrc=([\x27\x22])cid:([^\x27\x22]+)\1/ig) != -1) {
+		if (body && body.search(/src=([\x27\x22])cid:([^\x27\x22]+)\1/ig) != -1) {
 			return true;
 		}
 	}
