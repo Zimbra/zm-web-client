09746d7f3bd70003ac2771384e1245c4f417f783
Unknown Perforce User <rajar>
2011-09-12 16:20:16 -0500

    bug:64375 appCtxt.notifyZimlets should return true/false depending on whether a Zimlet has handled the notification or not as this is a requirement for ZmTooltipMgr.getPersonToolTip
    http://bugzilla.zimbra.com/show_bug.cgi?id=64375
    
    Copied from Perforce
     Change: 330069

--- WebRoot/js/zimbraMail/core/ZmAppCtxt.js
+++ WebRoot/js/zimbraMail/core/ZmAppCtxt.js
@@ -1496,7 +1496,7 @@ function() {
  * @param {Hash}	options			a hash of options
  * @param {Boolean}	options.noChildWindow		if <code>true</code>, skip notify if we are in a child window
  * @param	{Boolean}	options.waitUntilLoaded	if <code>true</code> and zimlets are not yet loaded, add a listener so that notify happens on load
- * 
+ * @return	{Boolean} Returns <code>true</code> if at least one Zimlet handles the notification
  */
 ZmAppCtxt.prototype.notifyZimlets =
 function(event, args, options) {
@@ -1504,16 +1504,16 @@ function(event, args, options) {
 
 	var context = this.isChildWindow ? parentAppCtxt : this;
 
-	if (options && options.noChildWindow && this.isChildWindow) { return; }
+	if (options && options.noChildWindow && this.isChildWindow) { return false; }
 
 	if (!context.areZimletsLoaded()) {
 		if (options && options.waitUntilLoaded) {
 			context.addZimletsLoadedListener(new AjxListener(this, this.notifyZimlets, [event, args]));
 		}
-		return;
+		return false;
 	}
 
-	this.getZimletMgr().notifyZimlets(event, args);
+	return this.getZimletMgr().notifyZimlets(event, args);
 };
 
 ZmAppCtxt.prototype.notifySkin =
