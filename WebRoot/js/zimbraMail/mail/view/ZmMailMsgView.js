ba8100794d180ed4c1088f31b0d39a6db848e09a
Unknown Perforce User <jwagner>
2011-09-21 16:51:22 -0500

    Bug: 64777 Link to show external images not shown when dfsrc is present. Added isExternalImage method to check for dfsrc; added check data:        for unfang iternal image.
    http://bugzilla.zimbra.com/show_bug.cgi?id=64777
    
    Copied from Perforce
     Change: 331449

--- WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
@@ -589,8 +589,9 @@ function(msg, idoc) {
 		this.onload = null; // *this* is reference to <img> el.
 	};
 	for (var i = 0; i < images.length; i++) {
-		var external = ZmMailMsgView.__unfangInternalImage(msg, images[i], "src");
+		var external = ZmMailMsgView._isExternalImage(images[i]);
 		if(!external){ //Inline image
+			ZmMailMsgView.__unfangInternalImage(msg, images[i], "src");
 			images[i].onload = onload;
 		}
 		hasExternalImages = external || hasExternalImages;
@@ -610,24 +611,46 @@ function(msg, node) {
 		var child = node.firstChild;
 		while (child) {
 			if (child.nodeType == AjxUtil.ELEMENT_NODE) {
-				hasExternalImages = ZmMailMsgView.__unfangInternalImage(msg, child, "background") || hasExternalImages;
+				hasExternalImages = ZmMailMsgView.__unfangInternalImage(msg, child, "background", true) || hasExternalImages;
 				recurse(child);
 			}
 			child = child.nextSibling;
 		}
 	}
 
-	if(node.innerHTML.indexOf("dfbackground") != -1){
+	if(node.innerHTML.indexOf("dfbackground") != -1) {
 		recurse(node);
+	}
+	else if (node.attributes && node.getAttribute("dfbackground") != -1) {
+		hasExternalImages = ZmMailMsgView.__unfangInternalImage(msg, node, "background", true);	
 	}
 
 	return hasExternalImages;
 };
 
+/**
+ * Determines if an img element references an external image
+ * @param elem {HTMLelement}
+ * @return {Boolean} true if image is external
+ */
+ZmMailMsgView._isExternalImage = 
+function(elem) {
+	if (!elem) {
+		return false;
+	}
+	return Boolean(elem.getAttribute("dfsrc"));
+}
+
+/**
+ * Unfangs images
+ * @param msg {ZmMailMsg} mail message
+ * @param elem  {HTMLElement} element to be checked
+ * @param aname {String} attribute name
+ * @param prependDF  {Boolean} true to prepend "df" to attribute name 
+ */
 ZmMailMsgView.__unfangInternalImage =
-function(msg, elem, aname) {
-	//var df_aname = "df"+aname;
-	var avalue = elem.getAttribute(aname);
+function(msg, elem, aname, prependDF) {
+	var avalue = elem.getAttribute(prependDF ? "df" + aname : aname);
 	if (avalue) {
 		if (avalue.substr(0,4) == "cid:") {
 			var cid = "<" + AjxStringUtil.urlComponentDecode(avalue.substr(4)) + ">"; // Parse percent-escaping per bug #52085 (especially %40 to @)
@@ -653,6 +676,8 @@ function(msg, elem, aname) {
 				//elem.setAttribute(df_aname, avalue)
 				return false;
 			}
+		} else if (avalue.substring(0,5) == "data:") {
+			return false;
 		}
 		return true;
 	}
