47004187e08eea051febb64d2aa77f62d67e2aea
Unknown Perforce User <rottikunta>
2011-10-11 16:03:19 -0500

    Bug:62508 - "Find Shares" is misplaced after clicking "Show Remaining Folders"
    http://bugzilla.zimbra.com/show_bug.cgi?id=62508
    
    Copied from Perforce
     Change: 334016

--- WebRoot/js/zimbraMail/share/view/ZmTreeView.js
+++ WebRoot/js/zimbraMail/share/view/ZmTreeView.js
@@ -479,7 +479,7 @@ function(parentNode, organizer, index, noTooltips, omit) {
 		}
 		if (this._addShareLink && this._addShareLink.parent == parentNode) {
 			var addShareIndex = parentNode.getChildIndex(this._addShareLink);
-			if (addShareIndex >= 0 && index > addShareIndex)
+			if (addShareIndex >= 0 && (!index || index > addShareIndex))
 				index = addShareIndex; // Bug 52053: We must make sure nothing has a higher index than that of this._addShareLink
 		}
 		var params = {
