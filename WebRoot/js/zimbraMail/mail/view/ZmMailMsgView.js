a466cccfaafc1a33435fc8a3dbca399f619fa312
Conrad Damon <cdamon@zimbra.com>
2011-11-29 18:26:46 -0600

    Integrate change 340690 from GNR to HELIX ------------- Bug:67744 - XSS injecting malicious email Instead of looking at the msg as a whole to see whether to sanitize text, look at each part and sanitize it if appropriate. The only content that the server returns that is defanged and trustworthy is of type text/html. Any other content returned should be sanitized so that tags are not executed by the browser.
    http://bugzilla.zimbra.com/show_bug.cgi?id=67744
    
    Copied from Perforce
     Change: 340697

--- WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
@@ -908,15 +908,9 @@ function(container, html, isTextMsg, isTruncated) {
 			if (msgSize <= maxHighlightSize) {
 				//Using callback to lazily find objects instead of doing it on a run.
 				callback = new AjxCallback(this, this.lazyFindMailMsgObjects, [500]);
-				html = AjxStringUtil.convertToHtml(html);
 			} else {
 				this._makeHighlightObjectsDiv(html);
-				html = AjxStringUtil.convertToHtml(html);
 			}
-		} else {
-			// we get here when viewing text attachments and we need to HTMLize
-			// the text message in order to be displayed correctly (bug 8714).
-			html = AjxStringUtil.convertToHtml(html);
 		}
 		if (AjxEnv.isSafari) {
 			html = "<html><head></head><body>" + html + "</body></html>";
@@ -1352,23 +1346,24 @@ function(msg, container, callback) {
 		for (var i = 0; i < len; i++) {
 			var bp = bodyParts[i];
 			if (ZmMimeTable.isRenderableImage(bp.ct)) {
-				// Hack: (Bug:27320) Done specifically for sMime implementationu are.
+				// Hack: (Bug:27320) Done specifically for sMime
 				var imgHtml = (bp.content)
 					? ["<img zmforced='1' class='InlineImage' src='", bp.content, "'>"].join("")
 					: ["<img zmforced='1' class='InlineImage' src='", appCtxt.get(ZmSetting.CSFE_MSG_FETCHER_URI), "&id=", msg.id, "&part=", bp.part, "'>"].join("");
 				html.push(imgHtml);
 			} else {
+				var content = (bp.ct != ZmMimeTable.TEXT_HTML) ? AjxStringUtil.convertToHtml(bp.content) : bp.content;
 				if (bp.ct == ZmMimeTable.TEXT_PLAIN) {
 					html.push(hasHtmlPart ? "<pre>" : "");
-					html.push(bp.content);
+					html.push(content);
 					html.push(hasHtmlPart ? "</pre>" : "");
 				} else {
 					if (appCtxt.get(ZmSetting.VIEW_AS_HTML)) {
-						html.push(bp.content);
+						html.push(content);
 					} else {
 						// bug fix #31840 - convert HTML to text
 						var div = document.createElement("div");
-						div.innerHTML = bp.content;
+						div.innerHTML = content;
 						var convert = AjxStringUtil.convertHtml2Text(div);
 
 						html.push(hasHtmlPart ? "<pre>" : "");
@@ -1429,6 +1424,7 @@ function(msg, container, callback) {
 					}
 					
 					if (content != null) {
+						content = (bodyPart.ct != ZmMimeTable.TEXT_HTML) ? AjxStringUtil.convertToHtml(content) : content;
 						this._makeIframeProxy(el, content, true);
 					}
 					if (callback) { callback.run(); }
@@ -1446,7 +1442,7 @@ function(msg, container, callback) {
                         isTextMsg = false; //To make sure we display html content properly
 
                     }
-					this._makeIframeProxy(el, c, isTextMsg, bodyPart.truncated);
+					this._makeIframeProxy(el, AjxStringUtil.convertToHtml(c), isTextMsg, bodyPart.truncated);
 				}
 			}
 		}
@@ -1500,6 +1496,7 @@ function(el, bodyPart, callback, result, isTruncated) {
 		}
 	}
 
+	content = (bodyPart.ct != ZmMimeTable.TEXT_HTML) ? AjxStringUtil.convertToHtml(content) : content;
 	this._makeIframeProxy(el, (content || ""), true, isTruncated);
 
 	this._setAttachmentLinks();
