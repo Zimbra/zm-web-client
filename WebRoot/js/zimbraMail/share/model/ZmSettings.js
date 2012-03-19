a0e4f33df3ae16d70287b1fdc1508270757bbc10
Unknown Perforce User <dcomfort>
2012-03-19 17:33:09 -0500

    Bug: 56319 - allow admin to declare if zimlets must be loaded at/before view initialization
    http://bugzilla.zimbra.com/show_bug.cgi?id=56319
    
    Copied from Perforce
     Change: 354020

--- WebRoot/js/zimbraMail/share/model/ZmSettings.js
+++ WebRoot/js/zimbraMail/share/model/ZmSettings.js
@@ -351,12 +351,13 @@ ZmSettings.prototype.setUserSettings = function(params) {
 	// load zimlets *only* for the main account
 	if (!accountName) {
 		if (info.zimlets && info.zimlets.zimlet) {
-			var listener = new AjxListener(this,
-				function() {
-					var zimletsCallback = new AjxCallback(this, this._loadZimlets, [info.zimlets.zimlet, info.props.prop]);
-					AjxDispatcher.require("Zimlet", false, zimletsCallback);
-				});
-			appCtxt.getAppController().addListener(ZmAppEvent.POST_STARTUP, listener);
+            if (this.get(ZmSetting.ZIMLETS_SYNCHRONOUS)) {
+                var action = new AjxTimedAction(this, this._beginLoadZimlets, [info.zimlets.zimlet, info.props.prop, true]);
+                AjxTimedAction.scheduleAction(action, 0);
+            } else {
+                var listener = new AjxListener(this, this._beginLoadZimlets, [info.zimlets.zimlet, info.props.prop, false]);
+                appCtxt.getAppController().addListener(ZmAppEvent.POST_STARTUP, listener);
+            }
 		} else {
 			appCtxt.allZimletsLoaded();
 		}
@@ -415,8 +416,17 @@ ZmSettings.prototype.setUserSettings = function(params) {
 /**
  * @private
  */
+ZmSettings.prototype._beginLoadZimlets =
+function(zimlet, prop, sync) {
+    var zimletsCallback = new AjxCallback(this, this._loadZimlets, [zimlet, prop, sync]);
+    AjxDispatcher.require("Zimlet", false, zimletsCallback);
+};
+
+/**
+ * @private
+ */
 ZmSettings.prototype._loadZimlets =
-function(allZimlets, props) {
+function(allZimlets, props, sync) {
 
 	allZimlets = allZimlets || [];
 	this.registerSetting("ZIMLETS",		{type:ZmSetting.T_CONFIG, defaultValue:allZimlets, isGlobal:true});
@@ -426,7 +436,7 @@ function(allZimlets, props) {
 
 	DBG.println(AjxDebug.DBG1, "Zimlets - Loading " + zimlets.length + " Zimlets");
 	var zimletMgr = appCtxt.getZimletMgr();
-	zimletMgr.loadZimlets(zimlets, props);
+	zimletMgr.loadZimlets(zimlets, props, null, null, sync);
 
 	if (zimlets && zimlets.length) {
 		var activeApp = appCtxt.getCurrentApp();
@@ -942,6 +952,8 @@ function() {
 	this.registerSetting("CHECKED_ZIMLETS_ENABLED",			{name:"zimbraFeatureManageZimlets", type:ZmSetting.T_COS, dataType:ZmSetting.D_BOOLEAN, defaultValue:true, isGlobal:true});
 	this.registerSetting("CHECKED_ZIMLETS",					{name:"zimbraPrefZimlets", type:ZmSetting.T_PREF, dataType:ZmSetting.D_LIST, isGlobal:true});
     this.registerSetting("MANDATORY_ZIMLETS",		        {name:"zimbraZimletMandatoryZimlets", type:ZmSetting.T_COS, dataType:ZmSetting.D_LIST});
+    this.registerSetting("ZIMLETS_SYNCHRONOUS",		        {name:"zimbraZimletLoadSynchronously", type:ZmSetting.T_COS, dataType:ZmSetting.D_BOOLEAN, defaultValue:false});
+
 };
 
 /**
