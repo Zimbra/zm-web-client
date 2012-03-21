6ca1e30a25a12c8335d66a8f0c924e3ebb057e3b
Vince B. Bellows <vbellows@zimbra.com>
2012-03-21 11:53:43 -0500

    Bug 71889 : ZmInvite is not specified in the NewWindow_2 package
    Review : https://reviewboard.eng.vmware.com/r/318975/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=71889
    
    Copied from Perforce
     Change: 354392

--- WebRoot/js/zimbraMail/package/NewWindow_2.js
+++ WebRoot/js/zimbraMail/package/NewWindow_2.js
@@ -155,6 +155,7 @@ AjxPackage.require("zimbraMail.mail.model.ZmMimePart");
 AjxPackage.require("zimbraMail.mail.model.ZmMailList");
 AjxPackage.require("zimbraMail.mail.model.ZmIdentity");
 AjxPackage.require("zimbraMail.mail.view.ZmComposeView");
+AjxPackage.require("zimbraMail.share.model.ZmInvite");
 AjxPackage.require("zimbraMail.mail.view.ZmInviteMsgView");
 AjxPackage.require("zimbraMail.mail.view.ZmMailMsgView");
 AjxPackage.require("zimbraMail.mail.view.ZmMailConfirmView");
