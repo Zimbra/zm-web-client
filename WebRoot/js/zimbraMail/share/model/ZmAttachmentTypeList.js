a8e0609efffe00f7ac08bd1a12ef307f3f2f5e4a
Unknown Perforce User <rottikunta>
2012-04-09 17:16:56 -0500

    Bug:68969 - Show fewer attachment types in Search
    Supported zip file formats ( application/zip and application/x-zip-compressed) cannot be grouped like other types(image/*, text/* etc).  So it requires special processing. 
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=68969
    
    Copied from Perforce
     Change: 363724

--- WebRoot/js/zimbraMail/share/model/ZmAttachmentTypeList.js
+++ WebRoot/js/zimbraMail/share/model/ZmAttachmentTypeList.js
@@ -85,11 +85,19 @@ function(callback) {
 ZmAttachmentTypeList.prototype._handleResponseLoad =
 function(callback, result) {
 	var att = this._organizeTypes(result.getResponse().BrowseResponse.bd);
+    var isZipFileIncluded = false;
 	if (att) {
 		for (var i = 0; i < att.length; i++) {
 			var type = att[i]._content;
 			if (!ZmMimeTable.isIgnored(type)) {
-				this._attachments.push(ZmMimeTable.getInfo(type, true));
+               var mimeInfo = ZmMimeTable.getInfo(type, true);
+               // To handle application/zip and application/x-zip-compressed type
+               if (type == ZmMimeTable.APP_ZIP  || type == ZmMimeTable.APP_ZIP2 ){
+                   if (isZipFileIncluded) continue;
+                   isZipFileIncluded = true;
+                   mimeInfo.type =  ZmMimeTable.APP_ZIP + " or " + "type: " + ZmMimeTable.APP_ZIP2;
+                }
+				this._attachments.push(mimeInfo);
 			}
 		}
 		this._attachments.sort(ZmAttachmentTypeList.compareEntry);
