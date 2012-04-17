fbdca271c879a37c2a83d3723fe7e003c1ae2621
Unknown Perforce User <jwagner>
2012-04-17 01:00:35 -0500

    Bug: 71613 double clicking a draft should bring up edit compose view add check to see if folder is draft when opening edit on double click
    http://bugzilla.zimbra.com/show_bug.cgi?id=71613
    
    Copied from Perforce
     Change: 364751

--- WebRoot/js/zimbraMail/mail/controller/ZmDoublePaneController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmDoublePaneController.js
@@ -487,7 +487,7 @@ function(ev) {
 
 		var respCallback = new AjxCallback(this, this._handleResponseListSelectionListener, item);
 		var folder = appCtxt.getById(item.getFolderId());
-		if (item.isDraft && item.type != ZmId.ITEM_CONV && (!folder || !folder.isReadOnly())) {
+		if (item.isDraft && folder && folder.id == ZmFolder.ID_DRAFTS && (!folder || !folder.isReadOnly())) {
 			this._doAction({ev:ev, action:ZmOperation.DRAFT});
 			return true;
 		} else if (appCtxt.get(ZmSetting.OPEN_MAIL_IN_NEW_WIN)) {
