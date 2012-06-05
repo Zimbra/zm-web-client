95cb4525ac9ec9bb39c21b5728c377bab61ec7cf
Eran Yarkon <eyarkon@zimbra.com>
2012-06-04 23:39:29 -0500

    Bug 74290
    DL Edit page: Error message for saving an empty member list
    changed the error message for DL to "Distribution List members must be
    specified.". Note that the DL name check was already done separately so no need to combine (not sure why it is in the group case) the two to one message. Moved the DL name test to before the "empty" test so that the correct message is displayed in case the DL name is empty.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=74290
    
    Copied from Perforce
     Change: 375415

--- WebRoot/js/zimbraMail/abook/view/ZmGroupView.js
+++ WebRoot/js/zimbraMail/abook/view/ZmGroupView.js
@@ -446,11 +446,11 @@ function() {
 		return [];
 	}
 	var items = [];
-	if (this.isEmpty(true)) {
-		items.push("members");
-	}
 	if (!this.isValidDlName()) {
 		items.push("dlName");
+	}
+	if (this.isEmpty(true)) {
+		items.push("members");
 	}
 	if (!this.isValidDlDomainName()) {
 		items.push("dlDomainName");
@@ -469,7 +469,7 @@ ZmGroupView.prototype.getErrorMessage = function(id) {
 		return null;
 	}
 	if (id == "members") {
-		return ZmMsg.errorMissingGroup;
+		return this.isDistributionList() ? ZmMsg.errorMissingDlMembers : ZmMsg.errorMissingGroup;
 	}
 	if (id == "dlName") { 
 		return ZmMsg.dlInvalidName; 
