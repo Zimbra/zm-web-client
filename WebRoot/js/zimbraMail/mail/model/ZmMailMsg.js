659d1d04c595d1ade422758593e0ac924f458536
Unknown Perforce User <ssutar>
2012-06-08 01:11:29 -0500

    bug: 74460 Calendar appointment does not show content
    - Set the loaded property to true if the invite is loaded in the mesaage
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=74460
    
    Copied from Perforce
     Change: 376360

--- WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
+++ WebRoot/js/zimbraMail/mail/model/ZmMailMsg.js
@@ -2146,6 +2146,7 @@ function(msgNode) {
 			{
 				this.flagLocal(ZmItem.FLAG_ATTACH, true);
 			}
+            this._loaded = true;
 		} catch (ex) {
 			// do nothing - this means we're trying to load an ZmInvite in new
 			// window, which we dont currently load (re: support).
