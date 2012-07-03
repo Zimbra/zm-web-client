fc52994f43a33813ff362c53f644af558ae7d62b
Unknown Perforce User <jwagner>
2012-07-03 16:05:48 -0500

    Bug: 75418 External account icon missing in overview pane set icon for IMAP
    http://bugzilla.zimbra.com/show_bug.cgi?id=75418
    
    Copied from Perforce
     Change: 382237

--- WebRoot/js/zimbraMail/share/view/ZmTreeView.js
+++ WebRoot/js/zimbraMail/share/view/ZmTreeView.js
@@ -450,7 +450,7 @@ function(parentNode, organizer, index, noTooltips, omit) {
 
 	if (ds && ds.type == ZmAccount.TYPE_IMAP) {
 		var cname = appCtxt.isFamilyMbox ? null : this._headerClass;
-		var icon = appCtxt.isFamilyMbox ? "AccountIMAP" : null;
+		var icon =  "Folder";
 		ti = new DwtTreeItem({parent:this, text:organizer.getName(), className:cname, imageInfo:icon});
 		ti.enableSelection(false);
 	} else {
