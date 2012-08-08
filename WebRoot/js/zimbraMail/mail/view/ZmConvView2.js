977cd661b9883bd0a95feb05108550aa008fd6d9
Conrad Damon <cdamon@zimbra.com>
2012-08-08 17:55:47 -0500

    Bug: 76772 - Message display is clipped at the bottom Check the BODY element's computed style object for height before we resort to summing the heights of the child nodes, which can be inaccurate.
    http://bugzilla.zimbra.com/show_bug.cgi?id=76772
    
    Copied from Perforce
     Change: 391732

--- WebRoot/js/zimbraMail/mail/view/ZmConvView2.js
+++ WebRoot/js/zimbraMail/mail/view/ZmConvView2.js
@@ -1257,10 +1257,15 @@ function() {
 	}
 };
 
-// Resize IFRAME to match its content. IFRAMEs have a default height of 150, so we need to
-// explicitly set the correct height if the content is smaller. The easiest way would be
-// to measure the height of the HTML or BODY element, but some browsers (mainly IE) report
-// that to be 150. So we add up the heights of the BODY's children instead.
+/**
+ * Resize IFRAME to match its content. IFRAMEs have a default height of 150, so we need to
+ * explicitly set the correct height if the content is smaller. The easiest way would be
+ * to measure the height of the HTML or BODY element, but some browsers (mainly IE) report
+ * that to be 150. So we end up trying these ways in order to get an accurate height:
+ *		- height of BODY
+ *		- height from BODY's computed style object
+ *		- cumulative height of BODY's child nodes
+ */
 ZmMailMsgCapsuleView.prototype._resize =
 function() {
 
@@ -1268,8 +1273,17 @@ function() {
 	if (!this._expanded || !this._usingIframe || this._hasBeenSized) { return; }
 	
 	var body = this.getContentContainer();
-	if (body && body.childNodes) {
-		var height = 0;
+	if (!body) { return; }
+	
+	var height = Dwt.getSize(body).y;
+	if (!height || height == 150) {
+		var styleObj = DwtCssStyle.getComputedStyleObject(body);
+		if (styleObj && styleObj.height) {
+			height = parseInt(styleObj.height);
+		}
+	}
+	if (!height || height == 150) {
+		height = 0;
 		for (var i = 0, len = body.childNodes.length; i < len; i++) {
 			var el = body.childNodes[i];
 			if (!el || el.nodeType != AjxUtil.ELEMENT_NODE) {
@@ -1279,17 +1293,16 @@ function() {
 			height += Dwt.getSize(el).y;
 			var styleObj = DwtCssStyle.getComputedStyleObject(el);
 			if (!styleObj) {
-				height = -1;
-				break;
+				return;
 			}
 			height += parseInt(styleObj.marginTop) + parseInt(styleObj.marginBottom);
 		}
-		if (height > 0 && height < 150) {
-			height += 12;	// fudge to make sure nothing is cut off
-			DBG.println(AjxDebug.DBG1, "resizing capsule msg view IFRAME height to " + height);
-			Dwt.setSize(this.getIframeElement(), Dwt.DEFAULT, height);
-			this._hasBeenSized = true;
-		}
+	}
+	if (height > 0 && height < 150) {
+		height += 12;	// fudge to make sure nothing is cut off
+		DBG.println(AjxDebug.DBG1, "resizing capsule msg view IFRAME height to " + height);
+		Dwt.setSize(this.getIframeElement(), Dwt.DEFAULT, height);
+		this._hasBeenSized = true;
 	}
 };
 
