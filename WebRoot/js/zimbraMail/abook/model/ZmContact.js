70ecae99a1f1c78b17fdc6f081179e446b9fc2cb
Rahul Shah <rshah@zimbra.com>
2012-08-23 15:37:01 -0500

    Bug 76859 - S/MIME: Toast message for Contacts saved should not be shown Add a argument to the create and modify functions for auto save/create. If the value is true then do not show the toast message.
    http://bugzilla.zimbra.com/show_bug.cgi?id=76859
    
    Copied from Perforce
     Change: 395203

--- WebRoot/js/zimbraMail/abook/model/ZmContact.js
+++ WebRoot/js/zimbraMail/abook/model/ZmContact.js
@@ -1056,9 +1056,10 @@ ZmContact.prototype.getNormalizedAttrs = function(prefix) {
 *
 * @param {Hash}	attr			the attribute/value pairs for this contact
 * @param {ZmBatchCommand}	batchCmd	the batch command that contains this request
+* @param {boolean} isAutoCreate true if this is a auto create and toast message should not be shown
 */
 ZmContact.prototype.create =
-function(attr, batchCmd) {
+function(attr, batchCmd, isAutoCreate) {
 
 	if (this.isDistributionList()) {
 		this._createDl(attr);
@@ -1093,7 +1094,7 @@ function(attr, batchCmd) {
 		}
 	}
 
-	var respCallback = new AjxCallback(this, this._handleResponseCreate, [attr, batchCmd != null]);
+	var respCallback = new AjxCallback(this, this._handleResponseCreate, [attr, batchCmd != null, isAutoCreate]);
 
 	if (batchCmd) {
 		batchCmd.addRequestParams(jsonObj, respCallback);
@@ -1106,7 +1107,7 @@ function(attr, batchCmd) {
  * @private
  */
 ZmContact.prototype._handleResponseCreate =
-function(attr, isBatchMode, result) {
+function(attr, isBatchMode, isAutoCreate, result) {
 	// dont bother processing creates when in batch mode (just let create
 	// notifications handle them)
 	if (isBatchMode) { return; }
@@ -1129,8 +1130,10 @@ function(attr, isBatchMode, result) {
 			this.attr[ZmContact.F_groups] = groupMembers;
 			cn._attrs[ZmContact.F_groups] = groupMembers;
 		}
-		var msg = this.isGroup() ? ZmMsg.groupCreated : ZmMsg.contactCreated;
-		appCtxt.getAppController().setStatusMsg(msg);
+		if (!isAutoCreate) {
+			var msg = this.isGroup() ? ZmMsg.groupCreated : ZmMsg.contactCreated;
+			appCtxt.getAppController().setStatusMsg(msg);
+		}
 		appCtxt.getApp(ZmApp.CONTACTS).updateIdHash(cn, false);
 	} else {
 		var msg = this.isGroup() ? ZmMsg.errorCreateGroup : ZmMsg.errorCreateContact;
@@ -1182,9 +1185,10 @@ function(ex) {
  *
  * @param {Hash}	attr		a set of attributes and new values
  * @param {AjxCallback}	callback	the callback
+ * @param {boolean} isAutoSave  true if it is a auto save and toast should not be displayed.
  */
 ZmContact.prototype.modify =
-function(attr, callback) {
+function(attr, callback, isAutoSave) {
 	if (this.isDistributionList()) {
 		this._modifyDl(attr);
 		return;
@@ -1223,7 +1227,7 @@ function(attr, callback) {
     }
 
 	if (continueRequest) {
-		var respCallback = new AjxCallback(this, this._handleResponseModify, [attr, callback]);
+		var respCallback = new AjxCallback(this, this._handleResponseModify, [attr, callback, isAutoSave]);
 		appCtxt.getAppController().sendRequest({jsonObj:jsonObj, asyncMode:true, callback:respCallback});
 	} else {
 		if (attr[ZmContact.F_folderId]) {
@@ -1553,7 +1557,7 @@ function () {
  * @private
  */
 ZmContact.prototype._handleResponseModify =
-function(attr, callback, result) {
+function(attr, callback, isAutoSave, result) {
 	var resp = result.getResponse().ModifyContactResponse;
 	var cn = resp ? resp.cn[0] : null;
 	var id = cn ? cn.id : null;
@@ -1564,7 +1568,9 @@ function(attr, callback, result) {
 	}
 
 	if (id && id == this.id) {
-		appCtxt.setStatusMsg(this.isGroup() ? ZmMsg.groupSaved : ZmMsg.contactSaved);
+		if (!isAutoSave) {
+			appCtxt.setStatusMsg(this.isGroup() ? ZmMsg.groupSaved : ZmMsg.contactSaved);
+		}
 		// was this contact moved to another folder?
 		if (attr[ZmContact.F_folderId] && this.folderId != attr[ZmContact.F_folderId]) {
 			this._setFolder(attr[ZmContact.F_folderId]);
