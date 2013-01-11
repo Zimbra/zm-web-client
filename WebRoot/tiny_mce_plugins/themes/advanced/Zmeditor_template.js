2271448ae77a94d9ae16fcd15d57f7780f80a23a
Hem Aravind Eswaran <heswaran@zimbra.com>
2013-01-11 12:57:34 -0600

    Bug:77787
    Pasting table after first time appears with border now. Taking snapshot of content for undo/redo operation during paste event caused this bug. Now content snapshot is taken during paste post process event which fixed both the bugs (71074 and 77787).
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=77787
    
    Copied from Perforce
     Change: 435333

--- WebRoot/tiny_mce_plugins/themes/advanced/Zmeditor_template.js
+++ WebRoot/tiny_mce_plugins/themes/advanced/Zmeditor_template.js
@@ -460,10 +460,6 @@ Zmeditor_template.getFontSize = function(value){
     tinymce.create('tinymce.plugins.onEditorEvent', {
         init : function(ed) {
 
-            ed.onPaste.add(function(ed) {
-                ed.undoManager.add();
-            });
-
             ed.onBeforeSetContent.add(function(ed, o) {
                 // Replaces all double br elements for avoiding enter issue
                 o.content = o.content.replace(/<br><br>/ig, '<br><div><br></div>');
