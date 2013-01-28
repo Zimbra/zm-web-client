f947e6f7334fdd2b331f558c01d3ba3ba18026e7
Hem Aravind Eswaran <heswaran@zimbra.com>
2013-01-28 00:17:47 -0600

    Bug:79875,76762
    Data uri images in the compose window are uploaded before saving/sending. Firefox dropped the support of blobbuilder from version 18, which we used it for converting data uri images to blobs for uploading. Used both blobbuilder (for backward compatibility) and blob (new API) for the above conversion.
    
    Mozilla bug for removing blobbuilder from version 18 : https://bugzilla.mozilla.org/show_bug.cgi?id=744907
    
    Simple steps to reproduce: screen shot paste in firefox and send mail to gmail. It will not render there as gmail is not yet allowing data uri images.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79875
    http://bugzilla.zimbra.com/show_bug.cgi?id=76762
    
    Copied from Perforce
     Change: 440020

--- WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
@@ -2175,8 +2175,12 @@ function(callback){
 
 ZmComposeController.prototype._processDataURIImages = function(imgArray, length, callback){
 
+    if (!window.atob) {
+        return;
+    }
+
     var BlobBuilder = window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder || window.BlobBuilder;
-    if(!BlobBuilder || !window.atob){
+    if (!(BlobBuilder || window.Blob)) {
         return;
     }
 
@@ -2214,13 +2218,19 @@ ZmComposeController.prototype._processDataURIImages = function(imgArray, length,
                     for (var i = 0; i < byteStringLength; i++) {
                         ia[i] = byteString.charCodeAt(i);
                     }
+                    var blob;
                     // write the ArrayBuffer to a blob, and you're done
-                    var blobbuilder = new BlobBuilder();
-                    blobbuilder.append(ab);
+                    if (window.Blob) {
+                        blob = new Blob([ab], {"type" : mimeString});
+                    }
+                    else {
+                        var blobbuilder = new BlobBuilder();
+                        blobbuilder.append(ab);
 
-                    var blob = blobbuilder.getBlob(mimeString);
-                    blob.type = mimeString;
-                    blob.name = blob.name || new Date().getTime();
+                        blob = blobbuilder.getBlob(mimeString);
+                        blob.type = mimeString;
+                        blob.name = blob.name || new Date().getTime();
+                    }
                     return blob;
                 }
             }
