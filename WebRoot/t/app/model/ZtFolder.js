b6f29f8955a73c6a644939afe4e77e6395f0e7eb
Macy Abbey <abbeym@zimbra.com>
2013-03-01 17:53:06 -0600

    Bug 60247: Web App for Touch Devices
    Missing file
    
    Copied from Perforce
     Change: 450000

---
+++ WebRoot/t/app/model/ZtFolder.js
@@ -0,0 +1,30 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2013 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+ * This class is a special organizer, so that we can create another store
+ * out of the ZtUserSession organizer data.
+
+ * @author Macy Abbey
+ */
+Ext.define('ZCS.model.ZtFolder', {
+	extend: 'ZCS.model.ZtOrganizer',
+	config: {
+		/**
+		 * This is a hack to trick ext into allowing us to create two ZtOrganizers with the same 'id' property
+		 */
+		idProperty: 'foo'
+	}
+})
