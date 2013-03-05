a5c16775639e1234661d9a98bdfc71ebaa159038
Eran Yarkon <eyarkon@zimbra.com>
2013-03-04 19:09:38 -0600

    Bug 80596
    UI freezes on move draft and selecting new folder
    disable "move" button for drafts.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=80596
    
    Copied from Perforce
     Change: 450841

--- WebRoot/js/zimbraMail/mail/controller/ZmMailListController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmMailListController.js
@@ -2077,7 +2077,7 @@ function(parent, num) {
 	parent.setItemVisible(ZmOperation.SPAM, !isDrafts);
 	parent.setItemVisible(ZmOperation.DETACH, !isDrafts);
 
-	parent.enable(ZmOperation.MOVE_MENU, !isDrafts);
+	parent.enable(ZmOperation.MOVE_MENU, !isDrafts && num > 0);
 
 	parent.enable(ZmOperation.DETACH, (appCtxt.get(ZmSetting.DETACH_MAILVIEW_ENABLED) && !isDrafts && num == 1));
 
