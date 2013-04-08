82fc3b8a49cc18b17be00c8f3bb09604d6d80f87
Ajinkya Chhatre <achhatre@zimbra.com>
2013-04-08 05:50:12 -0500

    Bug: 80423 - Problem when adding optionnal information with contact with FF 19.0
    Changeset:
    1. ZmEditContactView - Added a check for firefox and enabled a key down handler on the optional control
    
    2. DwtInputField - Exposed a function to enable the key down handler
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=80423
    
    Copied from Perforce
     Change: 461036

--- WebRoot/js/zimbraMail/abook/view/ZmEditContactView.js
+++ WebRoot/js/zimbraMail/abook/view/ZmEditContactView.js
@@ -835,6 +835,10 @@ ZmEditContactView.prototype._handleDetailCheck = function(itemId, id) {
         control.disableFocusHdlr(); //disable focus handler so hint is displayed
 		control.focus();
         control.enableFocusHdlr(); //re-enable
+        //Bug fix # 80423 - attach a onKeyDown handler for Firefox.
+        if (AjxEnv.isFirefox) {
+            control.enableKeyDownHdlr();
+        }
 	}
 };
 
