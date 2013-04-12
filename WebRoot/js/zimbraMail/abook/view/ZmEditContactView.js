330b3f62640ef0c7b5e6c1403d4281486a8f325f
Unknown Perforce User <pwaradpande>
2013-04-12 04:31:34 -0500

    bug: 79712 Javascript code uploaded as image run when view clicked - Changes integrated from bug 71976 - Performing check on corrupted file before sending out the modifyContact request - This check is performed when the preview of the image is displayed while editing the contact.
    Integration change from main to IM
    
    Reviewboard url: https://reviewboard.eng.vmware.com/r/331855/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79712
    
    Copied from Perforce
     Change: 462420

--- WebRoot/js/zimbraMail/abook/view/ZmEditContactView.js
+++ WebRoot/js/zimbraMail/abook/view/ZmEditContactView.js
@@ -1121,6 +1121,7 @@ ZmEditContactViewImage.prototype.setValue = function(value) {
         this.setToolTipContent(ZmMsg.editImg);
 	}
 	this.parent.setDirty("IMAGE", true);
+    this._imgEl.onerror = this._handleCorruptImageError.bind(this);
 };
 
 /**
@@ -1183,6 +1184,39 @@ ZmEditContactViewImage.prototype._handleImageSaved = function(folder, filenames,
  */
 ZmEditContactViewImage.prototype._createElement = function() {
 	return document.createElement("FIELDSET");
+};
+
+/**
+ * @private
+ */
+ZmEditContactViewImage.prototype._handleCorruptImageError = function() {
+    this.setValue();    // setting default contact image
+    this._popupCorruptImageErrorDialog();
+};
+
+/**
+ * @private
+ */
+ZmEditContactViewImage.prototype._popupCorruptImageErrorDialog = function() {
+    var dlg = this.corruptImageErrorDlg;
+    if(dlg){
+       dlg.popup();
+    }
+    else{
+        dlg = appCtxt.getMsgDialog();
+        this.corruptImageErrorDlg = dlg;
+	    dlg.setMessage(ZmMsg.errorCorruptImageFile, DwtMessageDialog.CRITICAL_STYLE, ZmMsg.corruptFile);
+        dlg.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._corruptImageErrorDialogOkListener));
+        dlg.popup();
+    }
+};
+
+/**
+ * @private
+ */
+ZmEditContactViewImage.prototype._corruptImageErrorDialogOkListener = function() {
+    this.corruptImageErrorDlg.popdown();
+    this._chooseImage();
 };
 
 //
