40e411ffeb4d26a3b990cb0bf0388577abce7e54
Eran Yarkon <eyarkon@zimbra.com>
2013-06-25 21:22:13 -0500

    Bug 82554
    Clicking to shared account Trash navigates to mail app tab
    
    There's something special in Contacts regarding Trash.. not sure what exactly, something I think search vs. folder... see bug 19307.
    
    This simple solution solves the problem though, so just did that.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82554
    
    Copied from Perforce
     Change: 476179

--- WebRoot/js/zimbraMail/abook/controller/ZmAddrBookTreeController.js
+++ WebRoot/js/zimbraMail/abook/controller/ZmAddrBookTreeController.js
@@ -330,7 +330,7 @@ function(folder, result) {
 	// bug fix #19307 - Trash is special when in Contacts app since it
 	// is a FOLDER type in ADDRBOOK tree. So reset selection if clicked
 	if (folder.nId == ZmFolder.ID_TRASH) {
-		this._treeView[this._app.getOverviewId()].setSelected(ZmFolder.ID_TRASH, true);
+		this._treeView[this._app.getOverviewId()].setSelected(folder, true);
 	}
 };
 
