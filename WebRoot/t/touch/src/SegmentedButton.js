0a3a20cd1d5402238e0ab603ff2a94737a7ef34c
Quanah Gibson-Mount <quanah@zimbra.com>
2013-08-08 00:19:12 -0500

    bug: 82712 Update copyright in IM
    http://bugzilla.zimbra.com/show_bug.cgi?id=82712
    
    Copied from Perforce
     Change: 479631

--- WebRoot/t/touch/src/SegmentedButton.js
+++ WebRoot/t/touch/src/SegmentedButton.js
@@ -1,3 +1,19 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2013 Zimbra Software, LLC.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 /**
  * SegmentedButton is a container for a group of {@link Ext.Button}s. Generally a SegmentedButton would be
  * a child of a {@link Ext.Toolbar} and would be used to switch between different views.
