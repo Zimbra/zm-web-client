4bb8ae544dd649661ec77329201e3a7761aca8f8
Ajinkya Chhatre <achhatre@zimbra.com>
2013-08-12 01:54:01 -0500

    Changeset:
    1. Updated the fix with comment.
    
    Copied from Perforce
     Change: 479951

--- WebRoot/t/app/view/ux/Ext.ux.TouchCalendarView.js
+++ WebRoot/t/app/view/ux/Ext.ux.TouchCalendarView.js
@@ -732,6 +732,7 @@ Ext.define('Ext.ux.TouchCalendarView', {
 	 * @return {Date}
 	 */
 	getCellDate: function(dateCell) {
+        // ZCS: Fix for JS error while tapping on the timeslot
 		var date = dateCell.dom.getAttribute('datetime') || dateCell.up('tr').down('td').getAttribute('datetime');
 		return this.stringToDate(date);
 	},
