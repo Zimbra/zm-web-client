0c70fc19270b937a1531ebf7ad105ae0f6cddb79
Eran Yarkon <eyarkon@zimbra.com>
2013-08-29 13:10:39 -0500

    Bug 83855
    New Folder dialog is corrupted
    
    problem didn't happen in Safari types (Chrome included) due to bug 28957. Fixed it generally. re-used the class ChooserDialog for that.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83855
    
    Copied from Perforce
     Change: 481839

--- WebRoot/js/zimbraMail/share/view/dialog/ZmNewOrganizerDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmNewOrganizerDialog.js
@@ -180,8 +180,7 @@ ZmNewOrganizerDialog.prototype._contentHtml =
 function() {
 	var html = [];
 	var idx = 0;
-	html[idx++] = "<table class='ZPropertySheet' cellspacing='6' ";
-	html[idx++] = (AjxEnv.isSafari) ? " width='300'>" : ">";
+	html[idx++] = "<table class='ChooserDialog ZPropertySheet' cellspacing='6' >";
 	idx = this._createStandardContentHtml(html, idx);
 	idx = this._createExtraContentHtml(html, idx);
 	html[idx++] = "</table>";
