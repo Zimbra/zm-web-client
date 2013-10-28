67cbb59f614bc65c91c435854363159e0b1a313e
Dan Christiansen (c) <dchristiansen@zimbra.com>
2013-10-28 04:00:51 -0500

    Bug 58539 - Hide task progress bar when the task is 0% complete.
    (From Lars P. Thomsen.)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=58539
    
    Copied from Perforce
     Change: 487717

--- WebRoot/js/zimbraMail/tasks/view/ZmTaskListView.js
+++ WebRoot/js/zimbraMail/tasks/view/ZmTaskListView.js
@@ -471,12 +471,14 @@ function(task, colIdx) {
 	htmlArr[idx++] = "</tr></table>";
 
     // second row
-    htmlArr[idx++] = "<table width=100% class='BottomRow'><tr>";
-    htmlArr[idx++] = "<td><div class='ZmTaskProgress'><div";
-    htmlArr[idx++] = " class='";
-    htmlArr[idx++] = this.getColorForStatus(task.status);
-    htmlArr[idx++] = "' style='width:"+ task.pComplete + "%;'></div></div></td>";
-    htmlArr[idx++] = "<td width=75 align=right><table><tr>";
+    htmlArr[idx++] = "<table width=100% class='BottomRow'><tr><td>";
+	if (task.pComplete) {
+		htmlArr[idx++] = "<div class='ZmTaskProgress'><div";
+		htmlArr[idx++] = " class='";
+		htmlArr[idx++] = this.getColorForStatus(task.status);
+		htmlArr[idx++] = "' style='width:"+ task.pComplete + "%;'></div></div>";
+	}
+    htmlArr[idx++] = "</td><td width=75 align=right><table><tr>";
 
     idx = this._getAbridgedCell(htmlArr, idx, task, ZmItem.F_TAG, colIdx, width);
     if(task.priority == ZmCalItem.PRIORITY_HIGH || task.priority == ZmCalItem.PRIORITY_LOW) {
