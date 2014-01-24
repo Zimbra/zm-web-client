799ff01207ee752e356552ba135a413146e5a6d7
Macy Abbey <abbeym@zimbra.com>
2014-01-24 16:22:38 -0600

    Bug: 85996 - Upgrading application to be able to use Sencha CMD 4.0.2
    http://bugzilla.zimbra.com/show_bug.cgi?id=85996
    
    Copied from Perforce
     Change: 497367

--- WebRoot/t/app.js
+++ WebRoot/t/app.js
@@ -1,4 +1,14 @@
 /*
+    This file is generated and updated by Sencha Cmd. You can edit this file as
+    needed for your application, but these edits will have to be merged by
+    Sencha Cmd when it performs code generation tasks such as generating new
+    models, controllers or views and when running "sencha app upgrade".
+
+    Ideally changes to this file would be limited and most work would be done
+    in other places (such as Controllers). If Sencha Cmd cannot merge your
+    changes and its generated code, it will produce a "merge conflict" that you
+    will need to resolve manually.
+
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2012, 2013 Zimbra Software, LLC.
