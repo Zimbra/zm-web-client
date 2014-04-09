506bb13f310b03fa403171a5d9649a1c3d2dba99
Komal Kakani <kkakani@zimbra.com>
2014-04-09 05:53:06 -0500

    bug:88276 - Fix styling for calendar on touch client -In day view, simplify the header to be {day of week}, {month} {date}. Remove the year.
    http://bugzilla.zimbra.com/show_bug.cgi?id=88276
    
    Copied from Perforce
     Change: 508343

--- WebRoot/t/app/view/ux/Ext.ux.TouchCalendarView.js
+++ WebRoot/t/app/view/ux/Ext.ux.TouchCalendarView.js
@@ -934,7 +934,7 @@ Ext.define('Ext.ux.TouchCalendarView', {
                                     '<th class="{[this.me.getPrevPeriodCls()]} style="display: block;">',
                                     '</th>',
                                     '<th>',
-                                        '<span>{[Ext.Date.format(values[0].date, "D jS M Y")]}</span>',
+                                        '<span>{[Ext.Date.format(values[0].date, "l, F jS")]}</span>',
                                     '</th>',
                                     '<th class="{[this.me.getNextPeriodCls()]} style="display: block;"">',
                                     '</th>',
