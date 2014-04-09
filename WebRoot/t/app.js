ffb9da90e4ea22e4e7f2ff57c4a8933ab076b23e
Dan Christiansen (c) <dchristiansen@zimbra.com>
2014-04-09 09:56:23 -0500

    bug 88278 - fix iOS home screen app icons
    As part of bug 87633, the logo icons were moved into a shared
    location, but only the login page was updated to point to it.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=88278
    
    Copied from Perforce
     Change: 508365

--- WebRoot/t/app.js
+++ WebRoot/t/app.js
@@ -106,10 +106,10 @@ Ext.application({
 	views: ['ZtMain'],
 
 	icon: {
-		'57': 'resources/icons/Icon.png',
-		'72': 'resources/icons/Icon~ipad.png',
-		'114': 'resources/icons/Icon@2x.png',
-		'144': 'resources/icons/Icon~ipad@2x.png'
+		'57': '../img/logo/Icon.png',
+		'72': '../img/logo/Icon~ipad.png',
+		'114': '../img/logo/Icon@2x.png',
+		'144': '../img/logo/Icon~ipad@2x.png'
 	},
 
 	isIconPrecomposed: true,
