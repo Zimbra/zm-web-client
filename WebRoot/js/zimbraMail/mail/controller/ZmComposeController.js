dcd8ea3dd07f5661c25245a65d98726d3efb906c
Eran Yarkon <eyarkon@zimbra.com>
2014-07-21 17:24:01 -0500

    Bug 93878
    Use prefix compose option not working
    
    
    Regression from bug 93473.
    
    USE_PREFIX is not part of "what to include" and therefor is not mapped in INC_MAP. But still we should do the same when it changes, so added it as an "or".
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=93878
    
    Copied from Perforce
     Change: 518359

--- WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmComposeController.js
@@ -1757,7 +1757,7 @@ function(ev) {
 		this._composeView._recipients._toggleBccField();
 		appCtxt.set(ZmSetting.SHOW_BCC, !appCtxt.get(ZmSetting.SHOW_BCC));
 	}
-	else if (ZmComposeController.INC_MAP[op]) {
+	else if (ZmComposeController.INC_MAP[op] || op === ZmOperation.USE_PREFIX) {
 		// user is changing include options
 		if (this._setInclude(op)) {
 			this._switchInclude(op);
