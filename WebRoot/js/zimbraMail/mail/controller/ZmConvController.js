a5314ea63fd2942a8366e3deab90fe79879528c9
Conrad Damon <cdamon@zimbra.com>
2014-11-04 22:49:53 -0600

    Bug: 96267 - Shortcut to go to next/previous conv results in JS error
    The first part of the fix was easy, since we were trying
    to check the page forward/back nav bar buttons that
    no longer exist. The second part was tougher. The conv
    controller's lack of a current query kept the conv search
    from happening, but we had loaded the conv earlier in
    setSelectedItem() when faking the double click. That had
    a side effect of mixing up the callback chain so that the
    conv did not get displayed. The fix is to make sure the
    conv controller knows its current query.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=96267
    
    Copied from Perforce
     Change: 527906

--- WebRoot/js/zimbraMail/mail/controller/ZmConvController.js
+++ WebRoot/js/zimbraMail/mail/controller/ZmConvController.js
@@ -94,6 +94,7 @@ function(conv, callback, result) {
 
 	var searchResult = result.getResponse();
 	var list = searchResult.getResults(ZmItem.MSG);
+	this._currentSearch = searchResult.search;
 	if (list && list.isZmList) {
 		this.setList(list);
 		this._activeSearch = searchResult;
@@ -324,19 +325,24 @@ ZmConvController.prototype.handleKeyAction =
 function(actionCode) {
 	DBG.println(AjxDebug.DBG3, "ZmConvController.handleKeyAction");
 
+	var navToolbar = this._navToolBar[this._currentViewId],
+		button;
+
 	switch (actionCode) {
 		case ZmKeyMap.CANCEL:
 			this._backListener();
 			break;
 
 		case ZmKeyMap.NEXT_CONV:
-			if (this._navToolBar[this._currentViewId].getButton(ZmOperation.PAGE_FORWARD).getEnabled()) {
+			button = navToolbar && navToolbar.getButton(ZmOperation.PAGE_FORWARD);
+			if (!button || button.getEnabled()) {
 				this._goToConv(true);
 			}
 			break;
 
 		case ZmKeyMap.PREV_CONV:
-			if (this._navToolBar[this._currentViewId].getButton(ZmOperation.PAGE_BACK).getEnabled()) {
+			button = navToolbar && navToolbar.getButton(ZmOperation.PAGE_BACK);
+			if (!button || button.getEnabled()) {
 				this._goToConv(false);
 			}
 			break;
