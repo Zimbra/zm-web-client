9af151ab2d11bdd35b1aed52afb363618cdf669b
Unknown Perforce User <jbryan>
2015-04-20 17:50:18 -0500

    Integrated the following from //depot/zimbra/JUDASPRIEST/...: Change 542604 by jbryan@jbryan-mbp-translate-JP on 2015/04/20 17:49:32
    bug: none
    2015-03-31: Finalized Lao functional testing (ZmMsg_lo.properties)
    2015-04-16: Files changed during linguistic testing (ZhMsg_iw.properties, ZmMsg_iw.properties, ZtMsg_iw.properties)
    
    Copied from Perforce
     Change: 542605

--- WebRoot/js/zimbraMail/mail/view/prefs/ZmMailPrefsPage.js
+++ WebRoot/js/zimbraMail/mail/view/prefs/ZmMailPrefsPage.js
@@ -335,7 +335,6 @@ function(ev) {
 	if (parentButton == this._startDateButton) {
 		this._startDateField.value = newDate;
 	} else {
-		if (ev.detail < new Date()) { return; }
 		this._endDateField.value = newDate;
 	}
 
