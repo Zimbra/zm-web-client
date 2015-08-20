8e8eadfd616ba6260b6f1c6b5e370da527acdffb
Conrad Damon <cdamon@zimbra.com>
2015-08-20 17:15:07 -0500

    Integrate change 553387 from zimbra to JUDASPRIEST
    -------------
    Bug: 100414 - Script Error: "t.getFocusMember is not a function" while click on Attachment filter from Search window
    When pushing/popping a menu's tab group, use the
    composite tab group instead of calling getTabGroupMember(),
    which will return the menu if no items have been added yet.
    Expand the check in the keyboard mgr to make sure the
    argument passed to push/pop is a DwtTabGroup.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100414
    
    Copied from Perforce
     Change: 553388

--- WebRoot/js/zimbraMail/share/model/ZmSettings.js
+++ WebRoot/js/zimbraMail/share/model/ZmSettings.js
@@ -115,8 +115,6 @@ function() {
 	this.getSetting(ZmSetting.SHORTCUTS).addChangeListener(listener);
 	this.getSetting(ZmSetting.CHILD_ACCTS_VISIBLE).addChangeListener(listener);
 	this.getSetting(ZmSetting.ATTACHMENTS_BLOCKED).addChangeListener(listener);
-	this.getSetting(ZmSetting.CHAT_PLAY_SOUND).addChangeListener(listener);
-	this.getSetting(ZmSetting.CHAT_ENABLED).addChangeListener(listener);
 
 
 	if (appCtxt.isOffline) {
@@ -837,7 +835,7 @@ function() {
     this.registerSetting("ADMIN_DELEGATED",                 {type:ZmSetting.T_CONFIG, dataType:ZmSetting.D_BOOLEAN, defaultValue:false});
 	this.registerSetting("AC_TIMER_INTERVAL",				{type:ZmSetting.T_CONFIG, dataType:ZmSetting.D_INT, defaultValue:300});
 	this.registerSetting("ASYNC_MODE",						{type:ZmSetting.T_CONFIG, dataType:ZmSetting.D_BOOLEAN, defaultValue:true});
-	this.registerSetting("BRANCH",							{type:ZmSetting.T_CONFIG, defaultValue:"JUDASPRIEST"});
+	this.registerSetting("BRANCH",							{type:ZmSetting.T_CONFIG, defaultValue:"main"});
 
 	// next 3 are replaced during deployment
 	this.registerSetting("CLIENT_DATETIME",					{type:ZmSetting.T_CONFIG, defaultValue:"@buildDateTime@"});
@@ -1056,10 +1054,6 @@ function() {
 	// zimbraFeatureAdminPreferencesEnabled set to FALSE.
 	this.registerSetting("MAIL_PREFERENCES_ENABLED",	    {type:ZmSetting.T_COS, dataType:ZmSetting.D_BOOLEAN, defaultValue:true});
 
-	//chat settings
-	this.registerSetting("CHAT_PLAY_SOUND",					{name:"zimbraPrefChatPlaySound", type: ZmSetting.T_METADATA, dataType: ZmSetting.D_BOOLEAN, defaultValue:true, isGlobal:true, section:ZmSetting.M_IMPLICIT});
-	this.registerSetting("CHAT_ENABLED",				{name:"zimbraFeatureChatEnabled", type: ZmSetting.T_METADATA, dataType: ZmSetting.D_BOOLEAN, defaultValue:true, isGlobal:true, section:ZmSetting.M_IMPLICIT});
-
 	ZmApp.runAppFunction("registerSettings", this);
 };
 
@@ -1167,10 +1161,6 @@ function(ev) {
 	}
 	else if (appCtxt.isOffline && id === ZmSetting.OFFLINE_UPDATE_NOTIFY) {
 		appCtxt.getAppController()._offlineUpdateChannelPref(ev.source.getValue());
-	} else if (id === ZmSetting.CHAT_PLAY_SOUND) {
-		appCtxt.getAppController().getApp(ZmApp.CHAT).setPlaySoundSetting(ev.source.getValue());
-	} else if (id === ZmSetting.CHAT_ENABLED) {
-		this._showConfirmDialog(ZmMsg.chatFeatureChangeRestart, this._refreshBrowserCallback.bind(this));
 	}
 };
 
