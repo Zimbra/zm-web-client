3a72d90a66c11553f39674717355c497c09005b1
Conrad Damon <cdamon@zimbra.com>
2015-09-10 17:00:15 -0500

    Integrate change 554781 from zimbra to JUDASPRIEST
    -------------
    BUG 100897 - Set setIncludeEditReply to true so message edited in plain format gets saved and sent
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=100897
    
    Copied from Perforce
     Change: 554908

--- WebRoot/js/zimbraMail/calendar/model/ZmCalItem.js
+++ WebRoot/js/zimbraMail/calendar/model/ZmCalItem.js
@@ -1419,8 +1419,8 @@ function(message) {
 		var textContent = message.getBodyContent(ZmMimeTable.TEXT_PLAIN);
 		if (!this._includeEditReply) {
 			textContent = this._trimNotesSummary(textContent);
+            this.setIncludeEditReply(true);
 		}
-
 		this.notesTopPart.setContentType(ZmMimeTable.TEXT_PLAIN);
 		this.notesTopPart.setContent(textContent);
 	}
