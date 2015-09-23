4231976984b28d0621fd74f1d301d46150d6d713
Conrad Damon <cdamon@zimbra.com>
2015-09-23 13:38:08 -0500

    Integrate change 555604 from zimbra to JUDASPRIEST
    -------------
    Bug: 101386 - Menu separator gets focus
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=101386
    
    Copied from Perforce
     Change: 555605

--- WebRoot/js/zimbraMail/mail/view/prefs/ZmMailPrefsPage.js
+++ WebRoot/js/zimbraMail/mail/view/prefs/ZmMailPrefsPage.js
@@ -361,7 +361,6 @@ function(ev) {
 	if (parentButton == this._startDateButton) {
 		this._startDateField.value = newDate;
 	} else {
-		if (ev.detail < new Date()) { return; }
 		this._endDateField.value = newDate;
 	}
 
