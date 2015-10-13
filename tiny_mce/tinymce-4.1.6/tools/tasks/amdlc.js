63a52d7e01dc9ac3295368f026ed5570269df249
Dan Christiansen (c) <dchristiansen@zimbra.com>
2015-10-13 10:23:21 -0500

    bug 101335 - upgrade TinyMCE to 4.2.6
    https://bugzilla.zimbra.com/show_bug.cgi?id=101335
    
    Copied from Perforce
     Change: 557217

--- tiny_mce/tinymce-4.1.6/tools/tasks/amdlc.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(grunt) {
-	grunt.registerMultiTask("amdlc", "Compiles AMD modules to libraries.", function() {
-		require("amdlc").compile(grunt.config([this.name, this.target]).options);
-	});
-};
