28203d60acd8eed5788cc630d6bc401bbd084a36
Conrad Damon <cdamon@zimbra.com>
2015-10-20 16:09:08 -0500

    Integrate change 557793 from zimbra to JUDASPRIEST
    -------------
    Bug: 101813 - Persistent XSS: unsafe content not filtered by defanger [CWE-79]
    Add null check when parsing attrs in DOM, since
    bad HTML may result in an empty or invalid attr.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=101813
    
    Copied from Perforce
     Change: 557794

--- WebRoot/js/zimbraMail/package/Startup1_2.js
+++ WebRoot/js/zimbraMail/package/Startup1_2.js
@@ -106,4 +106,3 @@ AjxPackage.require("zimbraMail.tasks.ZmTasksApp");
 AjxPackage.require("zimbraMail.abook.ZmContactsApp");
 AjxPackage.require("zimbraMail.briefcase.ZmBriefcaseApp");
 AjxPackage.require("zimbraMail.voicemail.ZmVoiceApp");
-AjxPackage.require("zimbraMail.chat.ZmChatApp");
