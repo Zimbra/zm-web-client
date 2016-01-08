318c18f0639b4af943368e075cf153c7b6c56676
Conrad Damon <cdamon@zimbra.com>
2016-01-08 12:56:39 -0600

    Integrate change 562773 from zimbra to JUDASPRIEST
    -------------
    Bug: 102745 - Email Fwd'ing with 'Include Original Message as an attachment' option doesn't working with web client.
    Revert part of the fix for 99703 and replace it with a simpler fix. The problem
    was that it removed the code that looks for the FORWARD_ATT action and sets
    the msg ID for attaching in the draft. That has been added back, but it now
    checks to see if the forwarded msg is a draft before attaching it.
    
    https://bugzilla.zimbra.com/show_bug.cgi?id=102745
    
    Copied from Perforce
     Change: 562774

--- WebRoot/js/zimbraMail/chat/ZmChatApp.js
+++ WebRoot/js/zimbraMail/chat/ZmChatApp.js
@@ -250,20 +250,17 @@ ZmChatApp.prototype.initChatUI = function(response) {
                 onChatStatusChanged: function(item) {
                     var chat_status = item.get('chat_status'),
                         fullname = item.get('fullname'),
-                        elChatTitle = this.$el.find('.chat-title>span[class^=icon-]'),
-                        chatBoxId = item.get('box_id'),
-                        chatBox = document.getElementById(chatBoxId), // Not using jquery selector because chatBoxId constitutes of special characters
-                        chatTextArea = $(chatBox).find('.chat-textarea'); // Don't want to add unnecessary code for jquery special character escapes or reqex
+                        elChatTitle = this.$el.find('.chat-title>span[class^=icon-]');
 
                     fullname = AjxUtil.isEmpty(fullname) ? item.get('jid'): fullname;
 					if (this.$el.is(':visible')) {
                         if (chat_status === ZmChatApp.OFFLINE || chat_status === ZmChatApp.BUSY) {
                             var chat_status_display = (chat_status === ZmChatApp.BUSY) ? ZmMsg.chatStatusBusy : ZmMsg.chatStatusOffline;
                             this.showStatusNotification(AjxMessageFormat.format(ZmMsg.chatMsgDeliveryRestricted, [fullname, chat_status_display]));
-                            chatTextArea.prop('disabled', true);
+                            $('.chat-textarea').prop('disabled',true);
                         } else {
                             this.$el.find('div.chat-event').remove();
-                            chatTextArea.prop('disabled', false);
+                            $('.chat-textarea').prop('disabled', false);
                         }
                     }
 
@@ -327,16 +324,13 @@ ZmChatApp.prototype.initChatUI = function(response) {
                 show: function (callback) {
                     var contact = converseObject.roster.get(this.model.get('jid'));
                     var chat_status = contact ? contact.get('chat_status') : "online";
-                    var fullname = this.model.get('fullname'),
-                        chatBoxId = this.model.get('box_id'),
-                        chatBox = document.getElementById(chatBoxId), // Not using jquery selector because chatBoxId constitutes of special characters
-                        chatTextArea = $(chatBox).find('.chat-textarea'); // Don't want to add unnecessary code for jquery special character escapes or reqex
+                    var fullname = this.model.get('fullname');
                     fullname = AjxUtil.isEmpty(fullname) ? item.get('jid'): fullname;
                     //ZCS change - Notify user about recipient being offline.
                     if (chat_status === ZmChatApp.OFFLINE || chat_status === ZmChatApp.BUSY) {
                         var chat_status_display = (chat_status === ZmChatApp.BUSY) ? ZmMsg.chatStatusBusy : ZmMsg.chatStatusOffline;
                         this.showStatusNotification(AjxMessageFormat.format(ZmMsg.chatMsgDeliveryRestricted, [fullname, chat_status_display]));
-                        chatTextArea.prop('disabled',true);
+                        $('.chat-textarea').prop('disabled',true);
                     }
                     var isNewChat = converseObject.newChats[this.model.get('jid')];
                     if (this.$el.is(':visible') && this.$el.css('opacity') == "1") {
