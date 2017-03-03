b16fa5e48d8db6e5b1d38af3f44ca07b7f671920
Kalpesh Patel <kalpesh.patel@synacor.com>
2017-03-03 16:36:32 +0530

    Replaced PNG icons for MailApp toolbar with SVG's
    Changeset:
    
    * Some old PNG's icons has been removed and it's corrosponding SVG icon has been added.
    * ZmOperations.js: Removed icon for `Action` menu
    * dwt.css, skin.css: Added/updated required style rules
    * skin.properties: Added/updated required properties.

--- WebRoot/js/zimbraMail/core/ZmOperation.js
+++ WebRoot/js/zimbraMail/core/ZmOperation.js
@@ -110,7 +110,7 @@ ZmOperation._operationDesc = {};
  */
 ZmOperation.initialize =
 function() {
-	ZmOperation.registerOp(ZmId.OP_ACTIONS_MENU, {textKey:"actions", tooltipKey:"", textPrecedence:40});
+	ZmOperation.registerOp(ZmId.OP_ACTIONS_MENU, {textKey:"actions", image:"MoreVertical", tooltipKey:"", textPrecedence:40, showImageInToolbar: true, showTextInToolbar: true});
 	
 	ZmOperation.registerOp(ZmId.OP_ATTACHMENT, {textKey:"addAttachment", tooltipKey:"attachmentTooltip", image:"Attachment", shortcut:ZmKeyMap.ATTACHMENT, showImageInToolbar: true});
 	ZmOperation.registerOp(ZmId.OP_CALL, {image:"Telephone"});
@@ -219,7 +219,7 @@ function() {
     ZmOperation.registerOp(ZmId.OP_UNDELETE, {textKey:"undelete", tooltipKey:"undelete", image:"MoveToFolder"});
     ZmOperation.registerOp(ZmId.OP_UNMUTE_CONV, {textKey:"unmuteConv", tooltipKey:"unmuteConvTooltip", image:"Unmute", shortcut:ZmKeyMap.MUTE_UNMUTE_CONV});
 	ZmOperation.registerOp(ZmId.OP_VIEW, {textKey:"view", image:"SplitView"});
-	ZmOperation.registerOp(ZmId.OP_VIEW_MENU, {tooltipKey:"viewTooltip", textKey:"view", image:"SplitPane", textPrecedence:80, showImageInToolbar: true, showTextInToolbar: true});
+	ZmOperation.registerOp(ZmId.OP_VIEW_MENU, {tooltipKey:"viewTooltip", textKey:"view", textPrecedence:80, showImageInToolbar: true, showTextInToolbar: true});
 	ZmOperation.registerOp(ZmId.OP_ZIMLET, {image:"ZimbraIcon"});
 
 	// invites - needed for both Mail and Calendar
