b24131582bd6d7ff55db61e0c5aaaee3d7d00b82
Saket Patel <saket.patel@synacor.com>
2017-07-10 14:06:44 +0530

    ZCS-1661 Remove duplication of AjxTimezoneData.js file - Don't package AjxTimezoneData.js file in Startup_1.1.js as it is loaded by jsp

--- WebRoot/js/zimbraMail/package/DocsPreview.js
+++ WebRoot/js/zimbraMail/package/DocsPreview.js
@@ -25,7 +25,6 @@ AjxPackage.require("ajax.util.AjxStringUtil");
 AjxPackage.require("ajax.util.AjxTimedAction");
 AjxPackage.require("ajax.util.AjxText");
 
-AjxPackage.require("ajax.util.AjxTimezoneData");
 AjxPackage.require("ajax.util.AjxTimezone");
 
 AjxPackage.require("ajax.net.AjxRpcRequest");
--- WebRoot/js/zimbraMail/package/Startup1_1.js
+++ WebRoot/js/zimbraMail/package/Startup1_1.js
@@ -49,7 +49,6 @@ AjxPackage.require("ajax.net.AjxInclude");
 AjxPackage.require("ajax.events.AjxListener");
 AjxPackage.require("ajax.util.AjxText");
 AjxPackage.require("ajax.util.AjxDateUtil");
-AjxPackage.require("ajax.util.AjxTimezoneData");
 AjxPackage.require("ajax.util.AjxTimezone");
 AjxPackage.require("ajax.util.AjxEmailAddress");
 AjxPackage.require("ajax.util.AjxHistoryMgr");
