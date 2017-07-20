ce0ec2670f0e1a9ad7e0cae489a0ffe9f2a58157
Saket Patel <saket.patel@synacor.com>
2017-07-20 14:31:55 +0530

    ZCS-1883 Firefox shows extra attribute style="" on mail editor(body area) form
    Issue:
    - Selenium has issues when it encounters empty style attributes in firefox
    
    Resolution:
    - apply styles only for IE browser for other browsers don't add any style tag
    - when removing any property from style check for empty style tags and remove it if required

--- WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
@@ -1053,8 +1053,6 @@ function(params) {
 	DBG.println(AjxDebug.DBG1, "Use IFRAME: " + this._usingIframe);
 	
 	if (this._usingIframe) {
-		// bug fix #9475 - IE isnt resolving MsgBody class in iframe so set styles explicitly
-		var inner_styles = AjxEnv.isIE ? ".MsgBody-text, .MsgBody-text * { font: 10pt monospace; }" : "";
 		var params1 = {
 			parent:					this,
 			parentElement:			params.container,
@@ -1063,13 +1061,17 @@ function(params) {
 			id:						this._msgBodyDivId,
 			hidden:					true,
 			html:					"<div>" + (this._cleanedHtml || html) + "</div>",
-			styles:					inner_styles,
 			noscroll:				!this._scrollWithIframe,
 			posStyle:				DwtControl.STATIC_STYLE,
 			processHtmlCallback:	callback,
 			useKbMgmt:				true,
 			title:                  this._getIframeTitle()
 		};
+
+		// bug fix #9475 - IE isnt resolving MsgBody class in iframe so set styles explicitly
+		if (AjxEnv.isIE) {
+			params1.styles = ".MsgBody-text, .MsgBody-text * { font: 10pt monospace; }";
+		}
 
 		// TODO: cache iframes
 		var ifw = this._ifw = new DwtIframe(params1);
