acd7e1aeae585f958fd3671b7c85f15aed23b4d2
Kalpesh Patel <kalpesh.patel@synacor.com>
2017-09-07 15:27:27 +0530

    ZCS-2416: Universal UI- Appointment create - Custom Repeat Dialog
    Summary of Changes:
    * Updated look and feel of custom repeat dialog as per mockup
    
    https://jira.corp.synacor.com/browse/ZCS-2416

--- WebRoot/js/zimbraMail/calendar/view/ZmCalItemEditView.js
+++ WebRoot/js/zimbraMail/calendar/view/ZmCalItemEditView.js
@@ -930,7 +930,7 @@ function(repeatType) {
 ZmCalItemEditView.prototype._initRecurDialog =
 function(repeatType) {
 	if (!this._recurDialog) {
-		this._recurDialog = new ZmApptRecurDialog(appCtxt.getShell(), this.uid);
+		this._recurDialog = new ZmApptRecurDialog(appCtxt.getShell(), this.uid, 'ZmApptRecurDialog');
 		this._recurDialog.addSelectionListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._recurOkListener));
 		this._recurDialog.addSelectionListener(DwtDialog.CANCEL_BUTTON, new AjxListener(this, this._recurCancelListener));
 	}
