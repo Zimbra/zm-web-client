511a12e15da8d28236cf45072453d8ff171e953c
Kalpesh Patel <kalpesh.patel@synacor.com>
2017-10-06 14:18:48 +0530

    ZCS-2432: Universal UI - Create New Folder Dialog
    Updated look and feel of Create New Folder dialog as per mockup.
    
    Changes:
    * Added/updated required styles in `dwt.css`, `zm.css` and `skin.css`
    * Added/updated required properties in `skin.properties`
    * Added/updated templates as per requirement in `Dialogs.templates` and `Widgets.template`
    * Added/updated required classes for various controls in `ZmDialog.js`, `ZmNewOrganizerDialog.js` and `ZmNewTagDialog.js`
    
    https://jira.corp.synacor.com/browse/ZCS-2432

--- WebRoot/js/zimbraMail/share/view/dialog/ZmNewOrganizerDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmNewOrganizerDialog.js
@@ -218,7 +218,7 @@ ZmNewOrganizerDialog.prototype._contentHtml =
 function() {
 	var html = [];
 	var idx = 0;
-	html[idx++] = "<table class='ChooserDialog ZPropertySheet' cellspacing='6' >";
+	html[idx++] = "<table class='ChooserDialog ZmDialogPropertySheet'>";
 	idx = this._createStandardContentHtml(html, idx);
 	idx = this._createExtraContentHtml(html, idx);
 	html[idx++] = "</table>";
