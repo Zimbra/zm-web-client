908fd2aecabafc90fc0aa0ddec2ab2be0a2fd877
Saket Patel <saket.patel@synacor.com>
2018-03-12 09:34:52 -0400

    ZCS-4389 Upgrade patches for tinymce to latest version - Commit built files as well, so webclient will be able to pick it for build creation

---
+++ tiny_mce/tinymce-4.7.9/js/tinymce/plugins/visualblocks/plugin.js
@@ -0,0 +1,135 @@
+(function () {
+var visualblocks = (function () {
+  'use strict';
+
+  var Cell = function (initial) {
+    var value = initial;
+    var get = function () {
+      return value;
+    };
+    var set = function (v) {
+      value = v;
+    };
+    var clone = function () {
+      return Cell(get());
+    };
+    return {
+      get: get,
+      set: set,
+      clone: clone
+    };
+  };
+
+  var PluginManager = tinymce.util.Tools.resolve('tinymce.PluginManager');
+
+  var fireVisualBlocks = function (editor, state) {
+    editor.fire('VisualBlocks', { state: state });
+  };
+  var $_cwfuhsr6jeo9pai0 = { fireVisualBlocks: fireVisualBlocks };
+
+  var isEnabledByDefault = function (editor) {
+    return editor.getParam('visualblocks_default_state', false);
+  };
+  var getContentCss = function (editor) {
+    return editor.settings.visualblocks_content_css;
+  };
+  var $_v1xcbr7jeo9pai1 = {
+    isEnabledByDefault: isEnabledByDefault,
+    getContentCss: getContentCss
+  };
+
+  var DOMUtils = tinymce.util.Tools.resolve('tinymce.dom.DOMUtils');
+
+  var Tools = tinymce.util.Tools.resolve('tinymce.util.Tools');
+
+  var cssId = DOMUtils.DOM.uniqueId();
+  var load = function (doc, url) {
+    var linkElements = Tools.toArray(doc.getElementsByTagName('link'));
+    var matchingLinkElms = Tools.grep(linkElements, function (head) {
+      return head.id === cssId;
+    });
+    if (matchingLinkElms.length === 0) {
+      var linkElm = DOMUtils.DOM.create('link', {
+        id: cssId,
+        rel: 'stylesheet',
+        href: url
+      });
+      doc.getElementsByTagName('head')[0].appendChild(linkElm);
+    }
+  };
+  var $_cxvc8mr8jeo9pai2 = { load: load };
+
+  var toggleVisualBlocks = function (editor, pluginUrl, enabledState) {
+    var dom = editor.dom;
+    var contentCss = $_v1xcbr7jeo9pai1.getContentCss(editor);
+    $_cxvc8mr8jeo9pai2.load(editor.getDoc(), contentCss ? contentCss : pluginUrl + '/css/visualblocks.css');
+    dom.toggleClass(editor.getBody(), 'mce-visualblocks');
+    enabledState.set(!enabledState.get());
+    $_cwfuhsr6jeo9pai0.fireVisualBlocks(editor, enabledState.get());
+  };
+  var $_fifclbr5jeo9pahz = { toggleVisualBlocks: toggleVisualBlocks };
+
+  var register = function (editor, pluginUrl, enabledState) {
+    editor.addCommand('mceVisualBlocks', function () {
+      $_fifclbr5jeo9pahz.toggleVisualBlocks(editor, pluginUrl, enabledState);
+    });
+  };
+  var $_eds14fr4jeo9pahx = { register: register };
+
+  var setup = function (editor, pluginUrl, enabledState) {
+    editor.on('PreviewFormats AfterPreviewFormats', function (e) {
+      if (enabledState.get()) {
+        editor.dom.toggleClass(editor.getBody(), 'mce-visualblocks', e.type === 'afterpreviewformats');
+      }
+    });
+    editor.on('init', function () {
+      if ($_v1xcbr7jeo9pai1.isEnabledByDefault(editor)) {
+        $_fifclbr5jeo9pahz.toggleVisualBlocks(editor, pluginUrl, enabledState);
+      }
+    });
+    editor.on('remove', function () {
+      editor.dom.removeClass(editor.getBody(), 'mce-visualblocks');
+    });
+  };
+  var $_4zoomlrbjeo9pai5 = { setup: setup };
+
+  var toggleActiveState = function (editor, enabledState) {
+    return function (e) {
+      var ctrl = e.control;
+      ctrl.active(enabledState.get());
+      editor.on('VisualBlocks', function (e) {
+        ctrl.active(e.state);
+      });
+    };
+  };
+  var register$1 = function (editor, enabledState) {
+    editor.addButton('visualblocks', {
+      active: false,
+      title: 'Show blocks',
+      cmd: 'mceVisualBlocks',
+      onPostRender: toggleActiveState(editor, enabledState)
+    });
+    editor.addMenuItem('visualblocks', {
+      text: 'Show blocks',
+      cmd: 'mceVisualBlocks',
+      onPostRender: toggleActiveState(editor, enabledState),
+      selectable: true,
+      context: 'view',
+      prependToContext: true
+    });
+  };
+  var $_g4i4ifrcjeo9paia = { register: register$1 };
+
+  PluginManager.add('visualblocks', function (editor, pluginUrl) {
+    var enabledState = Cell(false);
+    $_eds14fr4jeo9pahx.register(editor, pluginUrl, enabledState);
+    $_g4i4ifrcjeo9paia.register(editor, enabledState);
+    $_4zoomlrbjeo9pai5.setup(editor, pluginUrl, enabledState);
+  });
+  function Plugin () {
+  }
+
+  return Plugin;
+
+}());
+})();
