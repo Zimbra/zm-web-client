639b77e6bc2e147fcefa6cf779027cc60a2628f1
root <root@localhost.localdomain>
2018-07-07 22:17:12 -0400

    NICPS-197 : Login History - Front End Splash Page and Additional Columns

--- WebRoot/js/zimbraMail/core/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/core/ZmZimbraMail.js
@@ -281,7 +281,8 @@ ZmZimbraMail.killSplash =
 function() {
 	// 	Splash screen is now a part of the skin, loaded in statically via the JSP
 	//	as a well-known ID.  To hide the splash screen, just hide that div.
-	Dwt.hide("skin_container_splash_screen");
+	console.log("IN KILL SPLASH");
+	setTimeout(function () { Dwt.hide("skin_container_splash_screen") }, 15000);
 };
 
 /**
@@ -3550,7 +3551,7 @@ function() {
 			asyncMode: !appCtxt.get("FORCE_CLEAR_COOKIES"),
 			emptyResponseOkay:	true
 		};
-        var controller = appCtxt.getAppController();
+		var controller = appCtxt.getAppController();
 		controller && controller.sendRequest(args);
 	}
 };
