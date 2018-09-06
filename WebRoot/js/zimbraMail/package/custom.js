717c5839bf5f574d73b752042a057781b41b1013
Kunal Chandegaonkar <kunal.chandegaonkar@synacor.com>
2018-09-06 21:04:56 +0530

    NICPS-376 : Zimbra Login Page Redesign (#310)

--- WebRoot/js/zimbraMail/package/custom.js
+++ WebRoot/js/zimbraMail/package/custom.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    $('input[type="password"]').css("color", "white");
-    $('input[type="text"]').css("color", "white");
 
     var emailRegex = new RegExp(
         '^(([^<>()[\\]\\\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\"]+)*)|' +
