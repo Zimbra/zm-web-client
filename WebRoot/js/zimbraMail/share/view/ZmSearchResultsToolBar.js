3f5206e5b4349268fce2c0530ebdc680ec8f8b84
vishnukantjangid <44998083+vishnukantjangid@users.noreply.github.com>
2019-01-11 00:14:22 +0530

    NICPS-516: Language translation in search bar (#408)

--- WebRoot/js/zimbraMail/share/view/ZmSearchResultsToolBar.js
+++ WebRoot/js/zimbraMail/share/view/ZmSearchResultsToolBar.js
@@ -138,6 +138,9 @@ function(search) {
 			var token = tokens[i], prevToken = tokens[i - 1], nextToken = tokens[i + 1];
 			var showAnd = (prevToken && prevToken.op == ZmParsedQuery.GROUP_CLOSE) || (nextToken && nextToken.op == ZmParsedQuery.GROUP_OPEN);
 			var text = token.toString(showAnd);
+			if(langSearchArray != undefined && langSearchArray[text] != undefined) {
+				text = langSearchArray[text];
+			}
 			if (text) {
 				var bubble = this._searchField.addBubble({address:text, noParse:true});
 				this._bubbleId[text] = bubble.id;
