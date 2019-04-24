86ebb9fce35c71c454933af360046693bea72328
Vishnu kant Jangid <vishnukant.jangid@synacor.com>
2019-04-24 12:34:47 +0530

    NICPS-568: Briefcase Multiple File Upload Issue

--- WebRoot/js/zimbraMail/share/view/dialog/ZmUploadDialog.js
+++ WebRoot/js/zimbraMail/share/view/dialog/ZmUploadDialog.js
@@ -367,13 +367,13 @@ ZmUploadDialog.prototype._upload = function(){
                             setLinkTitleText();
                         }
 
-                        if ((i + 1) === fileElements.length) {
-                            postUpload();
-                        }
                     }).bind(this))
 
                 }).bind(this)(i, j);
             }
+			if ((i + 1) === fileElements.length) {
+				postUpload();
+			}
         } else {
 			var fileName = element.value.replace(/^.*[\\\/:]/, "");
             file = { name: fileName };
