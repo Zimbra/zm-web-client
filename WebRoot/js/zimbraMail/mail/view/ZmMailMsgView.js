077911ad50dc5cb6e1d796cb30a0dae34ef07de9
Suchit Gupta <suchit.gupta@synacor.com>
2019-06-29 16:33:45 +1100

    NICPS-618: Changes for print date time

--- WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
+++ WebRoot/js/zimbraMail/mail/view/ZmMailMsgView.js
@@ -1463,7 +1463,7 @@ function(msg, container, doNotClearBubbles) {
 	var subject = AjxStringUtil.htmlEncode(msg.subject || ZmMsg.noSubject);
 	var dateFormatter = AjxDateFormat.getDateTimeInstance(AjxDateFormat.LONG, AjxDateFormat.SHORT);
 	// bug fix #31512 - if no sent date then display received date
-	var date = new Date(msg.date);
+	var date = new Date(msg.date || msg.sentDate);
 	var dateString = dateFormatter.format(date);
 
 	var additionalHdrs = [];
