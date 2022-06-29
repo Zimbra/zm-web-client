fd6f70018a4bc9a62e8e3e6bf13748e02d097e27
Saket Patel <saket.patel@synacor.com>
2022-06-29 16:41:16 +0530

    ZBUG-2821 Prevent root folder sharing from webclient - There is certain possibility that user wants to share calendar folder but somehow selection changes and share folder dialog takes root folder for sharing - User is not aware about this and unintentionally share the root folder which is problematic - So have added a check to ignore action and show error message when user encounters this situation - We still need to find exact steps to reproduce this problem to reliably investigate it

--- WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseTreeController.js
+++ WebRoot/js/zimbraMail/briefcase/controller/ZmBriefcaseTreeController.js
@@ -220,6 +220,13 @@ function(ev) {
 	var briefcase = this._pendingActionData;
 	var share = null;
 
+	// This is a transient fix untill we find actual steps to reproduce problem and find exact root cause
+	if (briefcase.id === "1") {
+		var ex = new AjxException("Root folder sharing is not allowed, ignoring action.", AjxException.INVALID_PARAM);
+		appCtxt.getAppController()._handleException(ex);
+		return;
+	}
+
 	var sharePropsDialog = appCtxt.getSharePropsDialog();
 	sharePropsDialog.popup(ZmSharePropsDialog.NEW, briefcase, share);
 };
