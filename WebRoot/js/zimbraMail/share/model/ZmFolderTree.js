48e2199cde22825f127ca76c2621020ab8f252d3
miteshsavani810 <mitesh.savani@synacor.com>
2022-09-27 10:33:05 +0530

    ZCS-12159: A registered external user for shared briefcase is not able to login using classic client

--- WebRoot/js/zimbraMail/share/model/ZmFolderTree.js
+++ WebRoot/js/zimbraMail/share/model/ZmFolderTree.js
@@ -92,7 +92,7 @@ function(rootObj, elementType, account) {
 ZmFolderTree.createFromJs =
 function(parent, obj, tree, elementType, path, account) {
 	if (!(obj && obj.id)) { return; }
-	if (obj && (obj.owner || obj.id == ZmFolder.ID_FILE_SHARED_WITH_ME) && !appCtxt.get(ZmSetting.SHARING_ENABLED)) { return; }
+	if (obj && (obj.owner || obj.id == ZmFolder.ID_FILE_SHARED_WITH_ME) && (!appCtxt.isExternalAccount() && !appCtxt.get(ZmSetting.SHARING_ENABLED))) { return; }
 
 	var folder;
 	if (elementType == "search") {
