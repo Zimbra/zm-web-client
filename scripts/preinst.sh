6c039d432a03939f07d5b165f6fe312771755cb3
UmaShankar Avagadda <umagmrit@gmail.com>
2024-04-01 14:32:07 +0000

    ZCS-14844:add preinst.sh,postinst.sh for restrict write access for /opt/zimbra/jetty_base/webapps

--- scripts/preinst.sh
+++ scripts/preinst.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if lsattr -d /opt/zimbra/jetty_base/webapps/ | grep -q 'i'; then
         echo "**** remove restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-webclient-war)****"
+	lsattr -d /opt/zimbra/jetty_base/webapps/
         chattr -i -R /opt/zimbra/jetty_base/webapps/
 fi
