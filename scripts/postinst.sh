e74d97847fb895ffab29d2bd4a4ca939ead3b8d6
UmaShankar Avagadda <umagmrit@gmail.com>
2024-04-01 12:59:33 +0000

    ZCS-14844:add preinst.sh,postinst.sh for restrict write access for /opt/zimbra/jetty_base/webapps

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "**** Fixing up the permission ****"
-/opt/zimbra/libexec/zmfixperms
+#echo "**** Fixing up the permission ****"
+#/opt/zimbra/libexec/zmfixperms
 
 # This is only required in 8.8 patches, for 8.9 this code is already moved to /opt/zimbra/libexec/zmfixperms script
 if [ -d /opt/zimbra/conf/templates/ ]; then
   chmod -R 755 /opt/zimbra/conf/templates
   chown -R zimbra:zimbra /opt/zimbra/conf/templates
 fi
-echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ ****"
+echo "**** apply restrict write access for /opt/zimbra/jetty_base/webapps/ (zimbra-mbox-webclient-war)****"
 chattr +i -R /opt/zimbra/jetty_base/webapps/
