0bf4cd68e803b5fc32710099bbb716248d815b7c
miteshsavani810 <mitesh.savani@synacor.com>
2025-03-20 19:11:41 +0530

    10.1.8 ZCS-16982: Notify zimlet when user perform logout action

--- WebRoot/js/zimbraMail/core/ZmZimbraMail.js
+++ WebRoot/js/zimbraMail/core/ZmZimbraMail.js
@@ -2769,6 +2769,9 @@ function(ev, relogin) {
 
 	ZmZimbraMail._isLogOff = true;
 
+	//notify Zimlet
+	appCtxt.notifyZimlets("onLogoff", [this]);
+
 	// bug fix #36791 - reset the systray icon when returning to Account Setup
 	if (appCtxt.isOffline && AjxEnv.isWindows &&
 		appCtxt.get(ZmSetting.OFFLINE_SUPPORTS_DOCK_UPDATE))
